c1e0c543d930023e0bc22bf09b4713b91ec07a76
gatzhamm <gatzhamm@in.tum.de>
2013-11-18 10:48:51 +0100

    Added API functions to precice/adapters/fortran/ and fortran solverproxy.
    Added from C++ interface:
    - SolverInterface::initializeData()
    - constants::actionWriteInitialData()

--- src/precice/adapters/fortran/ConstantsFortran.hpp
+++ src/precice/adapters/fortran/ConstantsFortran.hpp
@@ -29,6 +29,16 @@ void precicef_action_write_iter_checkp_(
   int   lengthNameAction );
 
 /**
+ * @brief Name of action for writing initial data.
+ *
+ * FortranSyntax:
+ * precicef_action_write_initial_data( CHARACTER nameAction(*) )
+ */
+void precicef_action_write_initial_data_(
+  char* nameAction,
+  int   lengthNameAction );
+
+/**
  * @brief Name of action for reading iteration checkpoint.
  *
  * Fortran syntax:
