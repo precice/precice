2fc22bfaccf10c990fc7a74822b2670e0675c7c8
uekerman <benjamin.uekermann@gmail.com>
2014-08-11 10:20:09 +0200

    configurations for benjamin@riemann

--- tools/communication_dummies/startA.sh
+++ tools/communication_dummies/startA.sh
@@ -1,6 +1,6 @@
 export FSI_FSIDUMMYA_JAVA=off
 export FSI_FSIDUMMYA_WORKERS=20
-export FSI_FSIDUMMYA_XML=/work/atanasoa/gitlab/precice/tools/communication_dummies/FSIDummies/workspace/FSIDummyWorkbench.xml
+export FSI_FSIDUMMYA_XML=./FSIDummies/workspace/FSIDummyWorkbench.xml
 export FSI_FSIDUMMYA_BUFFER_SIZE=4096
-/home/atanasoa/intel/impi/4.1.3.049/intel64/bin/mpiexec -np 2 ./dummyA  2 1
+mpiexec -np 2 ./dummyA  2 1
 
--- tools/communication_dummies/startB.sh
+++ tools/communication_dummies/startB.sh
@@ -1,5 +1,5 @@
 export FSI_FSIDUMMYB_JAVA=off
 export FSI_FSIDUMMYB_WORKERS=20
-export FSI_FSIDUMMYB_XML=/work/atanasoa/gitlab/precice/tools/communication_dummies/FSIDummies/workspace/FSIDummyWorkbench.xml
+export FSI_FSIDUMMYB_XML=./FSIDummies/workspace/FSIDummyWorkbench.xml
 export FSI_FSIDUMMYB_BUFFER_SIZE=4096
-/home/atanasoa/intel/impi/4.1.3.049/intel64/bin/mpiexec -np 2 ./dummyB  2 1
+mpiexec -np 2 ./dummyB  2 1
