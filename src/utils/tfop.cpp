110813388912e8eb77a3fb616dfb24b5b7e0a556
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2014-11-18 15:25:10 +0100

    Add TFOP - Timings for Precice. First version.

---
+++ src/utils/tfop.cpp
@@ -0,0 +1,175 @@
+#include "tfop.hpp"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <chrono>
+
+using namespace std;
+
+
+Event::Event(std::string eventName, bool autostart)
+{
+  name = eventName;
+  if (autostart) {
+    isStarted = true;
+    starttime = Clock::now();
+  }
+}
+
+Event::~Event()
+{
+  stop();
+}
+
+void Event::start()
+{
+  if (isStarted) {
+    std::cerr << "Event " << name << " started which was already started. Restarting." << std::endl;
+  }
+  isStarted = true;
+  starttime = Clock::now();
+}
+
+void Event::stop()
+{
+  if (isStarted) {
+    stoptime = Clock::now();
+    isStarted = false;
+    duration = Clock::duration(stoptime - starttime);
+    EventRegistry::put(this);
+  }  
+}
+
+void Event::addProp(std::string property, int value)
+{
+  properties[property] += value;
+}
+
+
+Event::Clock::duration Event::getDuration()
+{
+  return duration;
+}
+
+// -----------------------------------------------------------------------
+
+
+void EventData::put(Event* event)
+{
+  count++;
+  Event::Clock::duration duration = event->getDuration();
+  total += duration;
+  if (min > duration) {
+    min = duration;
+  }
+  if (max < duration) {
+    max = duration;
+  }
+  for (auto p : event->properties) {
+    properties[p.first] += p.second;
+  }
+}
+
+int EventData::getAvg()
+{
+  return (std::chrono::duration_cast<std::chrono::milliseconds>(total) / count).count();
+  
+}
+
+int EventData::getMax()
+{
+  return std::chrono::duration_cast<std::chrono::milliseconds>(max).count();
+}
+
+int EventData::getMin()
+{
+  return std::chrono::duration_cast<std::chrono::milliseconds>(min).count();
+}
+
+int EventData::getTotal()
+{
+  return std::chrono::duration_cast<std::chrono::milliseconds>(total).count();  
+}
+
+int EventData::getCount()
+{
+  return count;
+}
+
+int EventData::getTimePercentage(Event::Clock::duration globalDuration)
+{
+  return ((double) total.count() / globalDuration.count()) * 100;
+}
+
+// -----------------------------------------------------------------------
+
+
+std::map<std::string, EventData> EventRegistry::events;
+Event::Clock::time_point EventRegistry::globalStart;
+Event::Clock::time_point EventRegistry::globalStop;
+
+void EventRegistry::put(Event* event)
+{
+  EventData data = events[event->name];
+  data.put(event);
+  events[event->name] = data;
+}
+
+void EventRegistry::print()
+{
+  using std::cout; using std::endl;
+  using std::setw; using std::setprecision;
+  using std::left; using std::right;
+  EventData::Properties allProps;
+  Event::Clock::duration globalDuration = globalStop - globalStart;
+
+  cout << "Global runtime = "
+       << std::chrono::duration_cast<std::chrono::milliseconds>(globalDuration).count() << "ms / "
+       << std::chrono::duration_cast<std::chrono::seconds>(globalDuration).count() << "s"
+       << endl << endl;
+
+  cout << "Event                Count    Total[ms]     Max[ms]     Min[ms]     Avg[ms]   T%" << endl;
+  cout << "--------------------------------------------------------------------------------" << endl;
+        
+  for (auto e : events) {
+    cout << setw(14) << left << e.first << right 
+         << setw(12) << e.second.getCount()
+         << setw(12) << e.second.getTotal()
+         << setw(12) << e.second.getMax() 
+         << setw(12) << e.second.getMin()
+         << setw(12) << e.second.getAvg()
+         << setw(6) << e.second.getTimePercentage(globalDuration)
+         << endl;
+    for (auto p : e.second.properties) {
+      allProps[p.first] += p.second;
+      
+      cout << "  " << setw(12) << left << p.first
+           << setw(12) << right << p.second
+           << endl;
+    }
+    cout << endl;
+  }
+
+  cout << "All Events, accumulated" << endl;
+  cout << "--------------------------" << endl;
+  for (auto a : allProps) {
+    cout << setw(14) << left << a.first << right
+         << setw(12) << a.second << endl;
+  }
+  
+}
+
+void TFOP_Init()
+{
+  std::cout << "Initialize TFOP" << std::endl;
+  EventRegistry::globalStart = Event::Clock::now();
+}
+
+void TFOP_Finalize()
+{
+  std::cout << "Finalize TLOP" << std::endl;
+  EventRegistry::globalStop = Event::Clock::now();
+}
---
+++ src/utils/tfop.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <chrono>
+#include <map>
+#include <string>
+
+
+class Event
+{
+public:
+  using Clock = std::chrono::high_resolution_clock;
+  std::string name;  
+  Event(std::string eventName, bool autostart = true);
+  ~Event();
+  void start();
+  void stop();
+  Clock::duration getDuration();
+
+  std::map<std::string, double> properties;
+  // void setProp(std::string property, double value);
+  // void setProp(std::string property, int value);
+
+  // void addProp(std::string property, double value);
+  void addProp(std::string property, int value);
+
+  // void incProp(std::string property);
+  // void incProp(std::string property);
+
+  
+
+private:  
+  Clock::time_point starttime;
+  Clock::time_point stoptime;
+  Clock::duration duration;
+  bool isStarted = false;
+
+};
+
+class EventData
+{
+public:
+  void put(Event* event);
+  int getAvg();
+  int getMax();
+  int getMin();
+  int getTotal();
+  int getCount();
+  int getTimePercentage(Event::Clock::duration globalDuration);
+
+  using Properties = std::map<std::string, double>;
+  Properties properties; // evtl. friend machen
+  
+private:
+  int count = 0;
+  Event::Clock::duration total = Event::Clock::duration::zero();
+  Event::Clock::duration max   = Event::Clock::duration::min();
+  Event::Clock::duration min   = Event::Clock::duration::max();
+
+};
+
+
+
+class EventRegistry
+{
+public:
+  static std::map<std::string, EventData> events;
+  static void put(Event* event);
+  static Event::Clock::time_point globalStart;
+  static Event::Clock::time_point globalStop;
+  void print();
+};
+
+
+void TFOP_Init();
+void TFOP_Finalize();
