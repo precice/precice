731d298686bada4efa80ce83cbbf758577a04878
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2016-02-23 10:46:34 +0100

    Pass a custom filename to the plot script.

--- tools/plotEventtimings.py
+++ tools/plotEventtimings.py
@@ -2,10 +2,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 import pdb
 from EventTimings import parseEventlog
 
-record = parseEventlog("EventTimings.log")[-1] # We just take the last of timing blocks
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description='Plots the timings')
+parser.add_argument('integers', metavar='filename', type=str, default="EventTimingslog", help="EventTimings.log file")
+args = parser.parse_args()
+
+record = parseEventlog(args.filename)[-1] # We just take the last of timing blocks
 timings = record["timings"]
 
 fig, ax = plt.subplots()
