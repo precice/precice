d5e8b0e2ec477a9c2e4357d2d836cc6147a61e2e
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2016-06-01 13:07:12 +0200

    Add number of procs to EventTimings output.
    Closes #53.

--- tools/EventTimings.py
+++ tools/EventTimings.py
@@ -74,14 +74,16 @@ def parseEventlog(file):
     events = []
     
     for i in readBlock(file):
-        timeStamp = datetime.datetime.strptime(i[0][18:], "%a %b %d %H:%M:%S %Y")
-        globalTimings = parseTimings(i[2])
-        timings = np.hstack( [parseTimings(t) for t in i[3:]] )
+        timeStamp = datetime.datetime.strptime(i[0][19:], "%a %b %d %H:%M:%S %Y")
+        procs = int(i[1][24:])
+        globalTimings = parseTimings(i[3])
+        timings = np.hstack( [parseTimings(t) for t in i[4:]] )
         
         events.append( {
             "timestamp" : timeStamp,
-            "global" : globalTimings,
-            "timings" : timings
+            "global"    : globalTimings,
+            "procs"     : procs,
+            "timings"   : timings
         } )
 
     return events
