d8f8b7223c1f998b6151d5ba2bb584832635269c
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2016-06-27 13:32:30 +0200

    Removed some include guards and copyright notices.

--- src/precice/impl/MeshContext.hpp
+++ src/precice/impl/MeshContext.hpp
@@ -1,8 +1,4 @@
-// Copyright (C) 2011 Technische Universitaet Muenchen
-// This file is part of the preCICE project. For conditions of distribution and
-// use, please see the license notice at http://www5.in.tum.de/wiki/index.php/PreCICE_License
-#ifndef PRECICE_IMPL_MESHCONTEXT_HPP_
-#define PRECICE_IMPL_MESHCONTEXT_HPP_
+#pragma once
 
 #include "MappingContext.hpp"
 #include "geometry/SharedPointer.hpp"
@@ -80,5 +76,3 @@ struct MeshContext
 };
 
 }} // namespace precice, impl
-
-#endif /* PRECICE_IMPL_MESHCONTEXT_HPP_ */
--- src/precice/impl/SolverInterfaceImpl.hpp
+++ src/precice/impl/SolverInterfaceImpl.hpp
@@ -1,8 +1,4 @@
-// Copyright (C) 2011 Technische Universitaet Muenchen
-// This file is part of the preCICE project. For conditions of distribution and
-// use, please see the license notice at http://www5.in.tum.de/wiki/index.php/PreCICE_License
-#ifndef PRECICE_IMPL_SOLVERINTERFACEIMPL_HPP_
-#define PRECICE_IMPL_SOLVERINTERFACEIMPL_HPP_
+#pragma once
 
 #include "precice/MeshHandle.hpp"
 #include "precice/Constants.hpp"
@@ -757,5 +753,3 @@ private:
 };
 
 }} // namespace precice, impl
-
-#endif /* PRECICE_IMPL_SOLVERINTERFACEIMPL_HPP_ */
