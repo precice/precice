8f3163f94f94f9c4a533f5701ce48ecec62e713f
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2017-01-31 14:55:23 +0100

    Update of Travis CI
    + Use caches
    + Outfactor installation of dependencies in separate script
    + Bump Eigen version.

---
+++ tools/travis-install-dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Installs dependencies of preCICE
+# boost and Eigen
+# if not already cached by Travis.
+
+set -x
+set -e
+
+LOCAL_INSTALL=$1
+
+# Don't test for $LOCAL_INSTALL, because it's created by the cacher.
+if [ ! -d $LOCAL_INSTALL/include ]; then
+    mkdir -p $LOCAL_INSTALL/include $LOCAL_INSTALL/lib
+
+    # Download and extract Eigen
+    wget http://bitbucket.org/eigen/eigen/get/3.3.2.tar.bz2 -O - | tar xj -C $LOCAL_INSTALL/include --strip-components=1 eigen-eigen-da9b4e14c255/Eigen
+
+    # Download, compile and install Boost
+    wget 'http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.bz2' -O - | tar xj
+    cd boost_1_60_0
+    ./bootstrap.sh --prefix=$LOCAL_INSTALL
+    ./b2 -j2 --with-program_options --with-test --with-filesystem --with-log install
+fi
+
