6e9721b4bd4039cb72e95c6893f9624a45cad183
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2017-03-31 16:06:24 +0200

    Fix typo in rbfShape.

--- tools/rbfShape.py
+++ tools/rbfShape.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 # parser.add_argument('basisfunction', choices=["Gaussian"], default="Gaussian", help="Basisfunction to use")
 parser.add_argument("meshwidth", type=float, help="Maximum mesh width h")
 parser.add_argument("vertices", type=int, help="Vertices to include m")
-parser.add_argument("--decay", type=float, default=10e-9, help="Allow basisfunction to decay to value within m vertices.")
+parser.add_argument("--decay", type=float, default=1e-9, help="Allow basisfunction to decay to value within m vertices.")
 args = parser.parse_args()
 
 h, m, decay = args.meshwidth, args.vertices, args.decay
