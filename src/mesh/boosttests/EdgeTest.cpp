166bcf13b8faa9c1b3db1d52ceb6e94a367a4665
Amin Totounferoush <a.totounferoush@gmail.com>
2017-05-24 16:14:26 +0200

    ported Edge test to boost tests

---
+++ src/mesh/boosttests/EdgeTest.cpp
@@ -0,0 +1,22 @@
+#include "mesh/Vertex.hpp"
+#include "testing/Testing.hpp"
+#include "mesh/Edge.hpp"
+#include <Eigen/Core>
+
+using namespace precice::mesh;
+
+BOOST_AUTO_TEST_SUITE(Mesh)
+
+BOOST_AUTO_TEST_CASE(Edges)
+{
+   Vertex v1 ( Eigen::Vector3d::Constant(0.0), 0 );
+   Vertex v2 ( Eigen::Vector3d::Constant(1.0), 1 );
+   Edge edge ( v1, v2, 0 );
+
+   Eigen::VectorXd coords1 = edge.vertex(0).getCoords();
+   Eigen::VectorXd coords2 = edge.vertex(1).getCoords();
+   BOOST_TEST ( coords1 == Eigen::Vector3d::Constant(0.0) );
+   BOOST_TEST ( coords2 == Eigen::Vector3d::Constant(1.0) );
+}
+
+BOOST_AUTO_TEST_SUITE_END() // Mesh
