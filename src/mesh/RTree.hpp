7a7f906b2bedd8d4eb938dee2d3325fc29412899
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2017-10-05 15:43:51 +0800

    Bug with rtree adapter setter and delete cache only for this mesh

--- src/mesh/RTree.hpp
+++ src/mesh/RTree.hpp
@@ -21,29 +21,13 @@ public:
   /*
    * Creates and fills the tree, if it wasn't requested before, otherwise it returns the cached tree.
    */
-  static PtrRTree getVertexRTree(PtrMesh mesh)
-  {
-    RTreeParameters params;
-    VertexIndexGetter ind(mesh->vertices());
-    
-    auto result = trees.emplace(std::piecewise_construct,
-                                std::forward_as_tuple(mesh->getID()),
-                                std::forward_as_tuple(std::make_shared<VertexRTree>(params, ind)));
-    
-    PtrRTree rtree = std::get<0>(result)->second;
-    
-    if (std::get<1>(result)) // insertion took place, fill tree
-      for (size_t i = 0; i < mesh->vertices().size(); ++i)
-        rtree->insert(i);
-    
-    return rtree;
-  }
+  static PtrRTree getVertexRTree(PtrMesh mesh);
+  
+  /// Clears the cache of all trees, e.g. when the mesh has changed
+  static void clear();
 
-  /// Clears the cache of trees, e.g. when the mesh has changed
-  static void clear()
-  {
-    trees.clear();
-  }
+  /// Only removed the tree of that specific mesh
+  static void clear(int id);
 
 private:
   static std::map<int, PtrRTree> trees;
