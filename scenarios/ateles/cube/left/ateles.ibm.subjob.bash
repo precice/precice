a29816afff25d7932561d0cd280901825557141c
Benjamin Uekermann <benjamin.uekermann@gmail.com>
2017-10-26 11:19:26 +0200

    removed scenarios folder

--- scenarios/ateles/cube/left/ateles.ibm.subjob.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-L1="$((MP_CHILD % 16))"
-L2="$((L1       + 16))"
-
-pushd 'left'
-  taskset -c "${L1},${L2}" "${ATELES}" 'ateles_left.lua' &> "${configuration}-$((LEFT_NP + RIGHT_NP))-${LEFT_NP}.log"
-popd
