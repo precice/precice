855ca711b15b226b5824e295d8e53426c50aec71
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2017-11-23 15:22:35 +0100

    Port io tests to boost tests

---
+++ src/io/boosttests/ExportConfigurationTest.cpp
@@ -0,0 +1,47 @@
+#include "io/config/ExportConfiguration.hpp"
+#include "testing/Testing.hpp"
+#include "utils/Globals.hpp"
+#include "utils/xml/XMLTag.hpp"
+
+BOOST_AUTO_TEST_SUITE(IOTests)
+
+using namespace precice;
+
+BOOST_AUTO_TEST_CASE(Configuration)
+{
+  using utils::XMLTag;
+  XMLTag tag = utils::getRootTag();
+  {
+    io::ExportConfiguration config(tag);
+    utils::configure(tag, utils::getPathToSources() + "/io/boosttests/config1.xml");
+    BOOST_TEST(config.exportContexts().size() == 1);
+    const io::ExportContext &context = config.exportContexts().front();
+    BOOST_TEST(context.type == "vtk");
+    BOOST_TEST(context.timestepInterval == 10);
+    BOOST_TEST(context.triggerSolverPlot);
+  }
+  {
+    tag.clear();
+    io::ExportConfiguration config(tag);
+    utils::configure(tag, utils::getPathToSources() + "/io/boosttests/config2.xml");
+    BOOST_TEST(config.exportContexts().size() == 1);
+    const io::ExportContext &context = config.exportContexts().front();
+    BOOST_TEST(context.type == "vtk");
+    BOOST_TEST(context.timestepInterval == 1);
+    BOOST_TEST(context.location == "somepath");
+    BOOST_TEST(not context.triggerSolverPlot);
+  }
+  {
+    tag.clear();
+    io::ExportConfiguration config(tag);
+    utils::configure(tag, utils::getPathToSources() + "/io/boosttests/config3.xml");
+    BOOST_TEST(config.exportContexts().size() == 1);
+    const io::ExportContext &context = config.exportContexts().front();
+    BOOST_TEST(context.type == "vrml");
+    BOOST_TEST(context.timestepInterval == 1);
+    BOOST_TEST(context.location == "");
+    BOOST_TEST(not context.triggerSolverPlot);
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END() // IOTests
---
+++ src/io/boosttests/TXTTableWriterTest.cpp
@@ -0,0 +1,28 @@
+#include "io/TXTTableWriter.hpp"
+#include "testing/Testing.hpp"
+
+BOOST_AUTO_TEST_SUITE(IOTests)
+
+using namespace precice::io;
+
+BOOST_AUTO_TEST_CASE(TXTTableWriterTest)
+{
+  TXTTableWriter writer("io-TXTTableWriterTest.txt");
+  writer.addData("Timestep", TXTTableWriter::INT);
+  writer.addData("Flowrate", TXTTableWriter::DOUBLE);
+  writer.addData("Force2D", TXTTableWriter::VECTOR2D);
+  writer.addData("Force3D", TXTTableWriter::VECTOR3D);
+
+  for (int t = 0; t < 10; t++) {
+    writer.writeData("Timestep", t);
+    writer.writeData("Flowrate", 0.0 + (double) t);
+    writer.writeData("Force2D", Eigen::Vector2d(0.0 + 2.0 * (double) t,
+                                                0.0 + 2.0 * (double) t));
+    writer.writeData("Force3D", Eigen::Vector3d(0.0 + 2.0 * (double) t,
+                                                0.0 + 2.0 * (double) t,
+                                                0.0 + 2.0 * (double) t));
+  }
+  writer.close();
+}
+
+BOOST_AUTO_TEST_SUITE_END() // IOTests
