756f046807d1599153ef1d696c52097fb9943d3b
Benjamin Rüth <benjamin.rueth@tum.de>
2018-02-28 13:59:22 +0100

    Update precice_config.sh

--- tools/anaconda_building/precice_config.sh
+++ tools/anaconda_building/precice_config.sh
@@ -1,5 +1,5 @@
-ANACONDA_ROOT=/work_fast/ga25zih/anaconda2/
-PRECICE_ROOT=/home/ga25zih/Programming/precice
+ANACONDA_ROOT=''
+PRECICE_ROOT=''
 
 SCONS_PARALLELJOBS=8
 PRECICE_MPI_IMPLEMENTATION=mpich
