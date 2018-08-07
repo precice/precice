6e5bbbeb25be8a5e96c4e8500dc5060e847ab85b
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-08-07 10:29:51 +0200

    EventTimings additions.
    Add link to EventTimings repo, fix division and clear stateChanges.

--- src/utils/EventTimings.cpp
+++ src/utils/EventTimings.cpp
@@ -78,6 +78,7 @@ void Event::stop(bool barrier)
     state = State::STOPPED;
     EventRegistry::instance().put(this);
     data.clear();
+    stateChanges.clear();
     duration = Clock::duration::zero();
     DEBUG("Stopped event " << name);
   }
@@ -453,7 +454,9 @@ void EventRegistry::printGlobalStats()
   auto stats = getGlobalStats(globalEvents);
   for (auto & e : stats) {
     auto & ev = e.second;
-    double rel = ev.max == Event::Clock::duration::zero() ? 0 : ev.min / ev.max; // Guard against division by zero
+    double rel = 0;
+    if (ev.max != Event::Clock::duration::zero()) // Guard against division by zero
+      rel = static_cast<double>(ev.min.count()) / ev.max.count();
     t.printLine(e.first, ev.max, ev.maxRank, ev.min, ev.minRank, rel);
   }
 }
