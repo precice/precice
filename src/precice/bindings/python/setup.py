1b89bc2d4adaf4139257843a087172af9c4b1987
Benjamin Rüth <benjamin.rueth@tum.de>
2018-08-23 16:06:47 +0200

    more robust determination of mpi implementation

--- src/precice/bindings/python/setup.py
+++ src/precice/bindings/python/setup.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import shutil
+import subprocess
+from enum import Enum
 
 from distutils.core import setup
 from distutils.extension import Extension
@@ -12,18 +14,36 @@ precice_root = os.path.join(python_bindings_path,"../../../../../precice")
 # name of Interfacing API
 appname = "PySolverInterface"
 
+
+class MpiImplementations(Enum):
+    OPENMPI = 1
+    MPICH = 2
+
+def check_mpi_implementation():
+
+    FNULL = open(os.devnull, 'w')  # used to supress output of subprocess.call
+
+    if subprocess.call(["mpic++","-showme:compile"], stdout=FNULL, stderr=FNULL) == 0:
+        PRECICE_MPI_IMPLEMENTATION = MpiImplementations.OPENMPI
+    elif subprocess.call(["mpic++","-compile-info"], stdout=FNULL, stderr=FNULL) == 0:
+        PRECICE_MPI_IMPLEMENTATION = MpiImplementations.MPICH
+    else:
+        raise Exception("unknown/no mpi++")
+
+    return PRECICE_MPI_IMPLEMENTATION
+
+
+PRECICE_MPI_IMPLEMENTATION = check_mpi_implementation()
+
 # determine which flags to use with mpic++
-if not os.getenv('PRECICE_MPI_IMPLEMENTATION') or os.getenv('PRECICE_MPI_IMPLEMENTATION') == 'openmpi':  # if PRECICE_MPI_IMPLEMENTATION is not defined try openmpi commands as default (this might break)
-    mpi_compile_args = os.popen("mpic++ -showme:compile").read().strip().split(' ')
-    mpi_link_args = os.popen("mpic++ --showme:link").read().strip().split(' ')
-elif os.getenv('PRECICE_MPI_IMPLEMENTATION') == 'mpich':  # if user provides mpich, then use mpich commands
-    mpi_compile_args = os.popen("mpic++ -compile-info").read().strip().split(' ')[1::]
-    mpi_link_args = os.popen("mpic++ -link-info").read().strip().split(' ')[1::]
+if PRECICE_MPI_IMPLEMENTATION is MpiImplementations.OPENMPI:
+    mpi_compile_args = subprocess.check_output(["mpic++","-showme:compile"]).strip().split(' ')
+    mpi_link_args = subprocess.check_output(["mpic++","-showme:link"]).strip().split(' ')
+elif PRECICE_MPI_IMPLEMENTATION is MpiImplementations.MPICH:
+    mpi_compile_args = subprocess.check_output(["mpic++","-compile-info"]).strip().split(' ')[1::]
+    mpi_link_args = subprocess.check_output(["mpic++","-link-info"]).strip().split(' ')[1::]
 else:  # if PRECICE_MPI_IMPLEMENTATION is not mpich or openmpi quit.
-    print('use either mpich or openmpi for PRECICE_MPI_IMPLEMENTATION.')
-    print('')
-    print('stopping setup...')
-    quit()
+    raise Exception("unknown/no mpi++. Could not build PySolverInterface.")
 
 # need to include libs here, because distutils messes up the order
 compile_args = ["-I"+precice_root, "-Wall", "-std=c++11"] + mpi_compile_args
