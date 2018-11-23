0bf692c0b416bedbc12f2219179c6f8ee07a26b5
Frederic Simonis <simonisfrederic@gmail.com>
2018-11-23 12:26:11 +0100

    Create Test Suite for Adaptors

--- src/mesh/tests/RTreeTests.cpp
+++ src/mesh/tests/RTreeTests.cpp
@@ -10,6 +10,94 @@ namespace bgi = boost::geometry::index;
 
 BOOST_AUTO_TEST_SUITE(MeshTests)
 BOOST_AUTO_TEST_SUITE(RTree)
+BOOST_AUTO_TEST_SUITE(BGAdapters)
+
+BOOST_AUTO_TEST_CASE(VectorAdapter)
+{
+  Eigen::VectorXd vec = Eigen::Vector2d(1, 2);
+  BOOST_TEST(bg::get<0>(vec) == 1);
+  BOOST_TEST(bg::get<1>(vec) == 2);
+  BOOST_TEST(bg::get<2>(vec) == 0);
+  bg::set<1>(vec, 5);
+  BOOST_TEST(bg::get<1>(vec) == 5);
+}
+
+BOOST_AUTO_TEST_CASE(VertexAdapter)
+{
+  precice::mesh::Mesh mesh("MyMesh", 2, false);
+  auto & v = mesh.createVertex(Eigen::Vector2d(1, 2));
+  BOOST_TEST(bg::get<0>(v) == 1);
+  BOOST_TEST(bg::get<1>(v) == 2);
+  BOOST_TEST(bg::get<2>(v) == 0);
+  bg::set<1>(v, 5);
+  BOOST_TEST(bg::get<1>(v) == 5);
+}
+
+BOOST_AUTO_TEST_CASE(EdgeAdapter)
+{
+  precice::mesh::Mesh mesh("MyMesh", 2, false);
+  auto & v1 = mesh.createVertex(Eigen::Vector2d(1, 2));
+  auto & v2 = mesh.createVertex(Eigen::Vector2d(3, 4));
+  auto & e = mesh.createEdge(v1, v2);
+  BOOST_TEST((bg::get<0,0>(e)) == 1.0);
+  BOOST_TEST((bg::get<0,1>(e)) == 2.0);
+  BOOST_TEST((bg::get<0,2>(e)) == 0.0);
+  BOOST_TEST((bg::get<1,0>(e)) == 3.0);
+  BOOST_TEST((bg::get<1,1>(e)) == 4.0);
+  BOOST_TEST((bg::get<1,2>(e)) == 0.0);
+  bg::set<1,1>(e, 5.0);
+  BOOST_TEST((bg::get<1,1>(e)) == 5.0);
+}
+
+BOOST_AUTO_TEST_CASE(TriangleAdapter)
+{
+  precice::mesh::Mesh mesh("MyMesh", 3, false);
+  auto & v1 = mesh.createVertex(Eigen::Vector3d(0, 2, 0));
+  auto & v2 = mesh.createVertex(Eigen::Vector3d(2, 1, 0));
+  auto & v3 = mesh.createVertex(Eigen::Vector3d(1, 0, 0));
+  auto & e1 = mesh.createEdge(v1, v2);
+  auto & e2 = mesh.createEdge(v2, v3);
+  auto & e3 = mesh.createEdge(v3, v1);
+  auto & t = mesh.createTriangle(e1, e2, e3);
+
+  std::vector<Eigen::VectorXd> vertices(t.begin(), t.end());
+  std::vector<Eigen::VectorXd> refs{ v1.getCoords(), v2.getCoords(), v3.getCoords()};
+  BOOST_TEST(vertices.size() == refs.size());
+  BOOST_TEST((std::is_permutation(
+                  vertices.begin(), vertices.end(),
+                  refs.begin(),
+                  []( const Eigen::VectorXd& lhs, const Eigen::VectorXd& rhs) {
+                     return precice::math::equals(lhs, rhs);
+                  }
+            )));
+}
+
+BOOST_AUTO_TEST_CASE(QuadAdapter)
+{
+  precice::mesh::Mesh mesh("MyMesh", 3, false);
+  auto & v1 = mesh.createVertex(Eigen::Vector3d(0, 2, 0));
+  auto & v2 = mesh.createVertex(Eigen::Vector3d(2, 1, 0));
+  auto & v3 = mesh.createVertex(Eigen::Vector3d(3, 0, 0));
+  auto & v4 = mesh.createVertex(Eigen::Vector3d(1, 0, 0));
+  auto & e1 = mesh.createEdge(v1, v2);
+  auto & e2 = mesh.createEdge(v2, v3);
+  auto & e3 = mesh.createEdge(v3, v4);
+  auto & e4 = mesh.createEdge(v4, v1);
+  auto & t = mesh.createQuad(e1, e2, e3, e4);
+
+  std::vector<Eigen::VectorXd> vertices(t.begin(), t.end());
+  std::vector<Eigen::VectorXd> refs{ v1.getCoords(), v2.getCoords(), v3.getCoords(), v4.getCoords()};
+  BOOST_TEST(vertices.size() == refs.size());
+  BOOST_TEST((std::is_permutation(
+                  vertices.begin(), vertices.end(),
+                  refs.begin(),
+                  []( const Eigen::VectorXd& lhs, const Eigen::VectorXd& rhs) {
+                     return precice::math::equals(lhs, rhs);
+                  }
+            )));
+}
+
+BOOST_AUTO_TEST_SUITE_END() // BG Adapters
 
 BOOST_AUTO_TEST_CASE(Query_2D)
 {
@@ -118,91 +206,6 @@ BOOST_AUTO_TEST_CASE(QueryWithBox)
     
     BOOST_TEST(results.size() == 8);
   }
-}
-
-BOOST_AUTO_TEST_CASE(VertexAdapter)
-{
-  precice::mesh::Mesh mesh("MyMesh", 2, false);
-  auto & v = mesh.createVertex(Eigen::Vector2d(1, 2));
-  BOOST_TEST(bg::get<0>(v) == 1);
-  BOOST_TEST(bg::get<1>(v) == 2);
-  BOOST_TEST(bg::get<2>(v) == 0);
-  bg::set<1>(v, 5);
-  BOOST_TEST(bg::get<1>(v) == 5);
-}
-
-BOOST_AUTO_TEST_CASE(EdgeAdapter)
-{
-  precice::mesh::Mesh mesh("MyMesh", 2, false);
-  auto & v1 = mesh.createVertex(Eigen::Vector2d(1, 2));
-  auto & v2 = mesh.createVertex(Eigen::Vector2d(3, 4));
-  auto & e = mesh.createEdge(v1, v2);
-  BOOST_TEST((bg::get<0,0>(e)) == 1.0);
-  BOOST_TEST((bg::get<0,1>(e)) == 2.0);
-  BOOST_TEST((bg::get<0,2>(e)) == 0.0);
-  BOOST_TEST((bg::get<1,0>(e)) == 3.0);
-  BOOST_TEST((bg::get<1,1>(e)) == 4.0);
-  BOOST_TEST((bg::get<1,2>(e)) == 0.0);
-  bg::set<1,1>(e, 5.0);
-  BOOST_TEST((bg::get<1,1>(e)) == 5.0);
-}
-
-BOOST_AUTO_TEST_CASE(TriangleAdapter)
-{
-  precice::mesh::Mesh mesh("MyMesh", 3, false);
-  auto & v1 = mesh.createVertex(Eigen::Vector3d(0, 2, 0));
-  auto & v2 = mesh.createVertex(Eigen::Vector3d(2, 1, 0));
-  auto & v3 = mesh.createVertex(Eigen::Vector3d(1, 0, 0));
-  auto & e1 = mesh.createEdge(v1, v2);
-  auto & e2 = mesh.createEdge(v2, v3);
-  auto & e3 = mesh.createEdge(v3, v1);
-  auto & t = mesh.createTriangle(e1, e2, e3);
-
-  std::vector<Eigen::VectorXd> vertices(t.begin(), t.end());
-  std::vector<Eigen::VectorXd> refs{ v1.getCoords(), v2.getCoords(), v3.getCoords()};
-  BOOST_TEST(vertices.size() == refs.size());
-  BOOST_TEST((std::is_permutation(
-                  vertices.begin(), vertices.end(),
-                  refs.begin(),
-                  []( const Eigen::VectorXd& lhs, const Eigen::VectorXd& rhs) {
-                     return precice::math::equals(lhs, rhs);
-                  }
-            )));
-}
-
-BOOST_AUTO_TEST_CASE(QuadAdapter)
-{
-  precice::mesh::Mesh mesh("MyMesh", 3, false);
-  auto & v1 = mesh.createVertex(Eigen::Vector3d(0, 2, 0));
-  auto & v2 = mesh.createVertex(Eigen::Vector3d(2, 1, 0));
-  auto & v3 = mesh.createVertex(Eigen::Vector3d(3, 0, 0));
-  auto & v4 = mesh.createVertex(Eigen::Vector3d(1, 0, 0));
-  auto & e1 = mesh.createEdge(v1, v2);
-  auto & e2 = mesh.createEdge(v2, v3);
-  auto & e3 = mesh.createEdge(v3, v4);
-  auto & e4 = mesh.createEdge(v4, v1);
-  auto & t = mesh.createQuad(e1, e2, e3, e4);
-
-  std::vector<Eigen::VectorXd> vertices(t.begin(), t.end());
-  std::vector<Eigen::VectorXd> refs{ v1.getCoords(), v2.getCoords(), v3.getCoords(), v4.getCoords()};
-  BOOST_TEST(vertices.size() == refs.size());
-  BOOST_TEST((std::is_permutation(
-                  vertices.begin(), vertices.end(),
-                  refs.begin(),
-                  []( const Eigen::VectorXd& lhs, const Eigen::VectorXd& rhs) {
-                     return precice::math::equals(lhs, rhs);
-                  }
-            )));
-}
-
-BOOST_AUTO_TEST_CASE(VectorAdapter)
-{
-  Eigen::VectorXd vec = Eigen::Vector2d(1, 2);
-  BOOST_TEST(bg::get<0>(vec) == 1);
-  BOOST_TEST(bg::get<1>(vec) == 2);
-  BOOST_TEST(bg::get<2>(vec) == 0);
-  bg::set<1>(vec, 5);
-  BOOST_TEST(bg::get<1>(vec) == 5);
 }
 
 BOOST_AUTO_TEST_CASE(CacheClearing)
