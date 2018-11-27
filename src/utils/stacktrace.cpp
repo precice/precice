0ff5a27a2935138fb9be6550bd8f18554092f43b
Frederic Simonis <simonisfrederic@gmail.com>
2018-11-27 11:42:47 +0100

    Migrate stacktrace to boost::stacktrace

---
+++ src/utils/stacktrace.cpp
@@ -0,0 +1,10 @@
+#include <boost/stacktrace.hpp>
+#include <sstream>
+#include <string>
+
+std::string getStacktrace()
+{
+  std::ostringstream strm;
+  strm << boost::stacktrace::stacktrace();
+  return strm.str();
+}
