a125122e206c66a77a02911e6097826ab3ed7a52
Frederic Simonis <simonisfrederic@gmail.com>
2019-01-25 16:22:29 +0100

    Fix inner dependencies

--- preciceConfig.cmake
+++ preciceConfig.cmake
@@ -1,14 +1,5 @@
 include(CMakeFindDependencyMacro)
 
-# Required
 find_dependency(Threads)
-find_dependency(Boost 1.65.1)
-find_dependency(LibXml2)
-
-# Configurable
-find_dependency(MPI)
-find_dependency(PETSc 3.6)
-find_dependency(PythonLibs 2.7)
-find_dependency(NumPy)
 
 include("${CMAKE_CURRENT_LIST_DIR}/preciceTargets.cmake")
