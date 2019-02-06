36586ae28d503438b174d592e32a6f0e6ec2fda6
David Sommer <david97sommer@gmail.com>
2019-02-06 12:07:36 +0100

    Doxygen and Formatting.

--- src/com/SocketSendQueue.hpp
+++ src/com/SocketSendQueue.hpp
@@ -11,24 +11,32 @@ namespace precice
 namespace com
 {
 
+/// This Queue is intended for SocketCommunication to push requests which should be sent onto it.
+/// It ensures that the invocations of asio::aSend are done serially.
 class SendQueue 
 {
-  using TCP           = boost::asio::ip::tcp;
-  using SocketService = boost::asio::stream_socket_service<TCP>;
-  using Socket   = boost::asio::basic_stream_socket<TCP, SocketService>;
   public:
+
+    using TCP           = boost::asio::ip::tcp;
+    using SocketService = boost::asio::stream_socket_service<TCP>;
+    using Socket   = boost::asio::basic_stream_socket<TCP, SocketService>;
+
     void push(std::shared_ptr<Socket> sock, boost::asio::const_buffers_1 data, std::function<void()> callback);
+
   private:
+
     void process();
+
     struct SendItem
     {
       std::shared_ptr<Socket> sock;
       boost::asio::const_buffers_1 data;
       std::function<void()> callback;
     };
+
     std::deque<SendItem> _itemQueue;
     std::mutex _queueMutex;
-    bool ready = true;
+    bool _ready = true;
 };
 
 }}
