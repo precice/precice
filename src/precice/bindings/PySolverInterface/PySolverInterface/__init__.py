332ebcbd97f359619bf1ad368a0adbf9f5ffbbd1
BenjaminRueth <benjamin.rueth@tum.de>
2019-02-13 16:21:50 +0100

    black magic

--- src/precice/bindings/PySolverInterface/PySolverInterface/__init__.py
+++ src/precice/bindings/PySolverInterface/PySolverInterface/__init__.py
@@ -2,7 +2,7 @@ import warnings
 
 warnings.warn("PySolverInterface is deprecated and will be removed in preCICE version 2.0.0, please use the python module precice instead", Warning, stacklevel=2)
 
-from PySolverInterface import PySolverInterface
+from PySolverInterface.PySolverInterface import PySolverInterface as PySolverInterface  # black magic, but works and will be deprecated in 2.0.0
 from precice import name_configuration as PyNameConfiguration
 from precice import data_displacements as PyDataDisplacements
 from precice import data_forces as PyDataForces
