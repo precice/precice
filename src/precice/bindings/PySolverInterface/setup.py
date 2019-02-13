f6c24a79f53546a69396fdfd0fe175c234ad6b52
BenjaminRueth <benjamin.rueth@tum.de>
2019-02-13 16:15:36 +0100

    small bugfix

--- src/precice/bindings/PySolverInterface/setup.py
+++ src/precice/bindings/PySolverInterface/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="PySolverInterface",
+    packages=['PySolverInterface'],
     description='Python language bindings for preCICE coupling library',
     python_requires='>=3'
 )
