0b981bbe8e090242976e42d9057a476eb5d0e2fd
Frederic Simonis <simonisfrederic@gmail.com>
2019-02-28 17:30:32 +0100

    Update old bindings setup

--- src/precice/bindings/PySolverInterface/setup.py
+++ src/precice/bindings/PySolverInterface/setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="PySolverInterface",
-    packages=['PySolverInterface'],
+    name='PySolverInterface',
+    version='0.1',
     description='Python language bindings for preCICE coupling library',
-    python_requires='>=3'
+    url='https://github.com/precice/precice',
+    author='the preCICE developers',
+    author_email='info@precice.org',
+    license='LGPL-3.0',
+    python_requires='>=3',
+    install_requires=[
+        'precice'
+    ]
 )
