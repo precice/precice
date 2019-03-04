ef04c27fa97cbe9e3447b7759b647c8aa45ef42d
Frederic Simonis <simonisfrederic@gmail.com>
2019-03-04 22:48:50 +0100

    Extract travis build script

---
+++ tools/travis-build-test.sh
@@ -0,0 +1,18 @@
+#! /bin/env bash
+
+if [ "$BUILD_TYPE" = "SCONS" ]; then
+    export PRECICE_BUILD_DIR=$TRAVIS_BUILD_DIR/build/last
+    cd $TRAVIS_BUILD_DIR
+    scons -j 2 petsc=$PETSC mpi=$MPI python=on compiler=$CXX staticlib bin solib tests symlink
+    cd $TRAVIS_BUILD_DIR/tests
+    if [ "$MPI" = "on" ]; then
+        mpirun.openmpi -n 4 --output-filename boost-test-output  $PRECICE_BUILD_DIR/testprecice -r detailed
+    else
+        $PRECICE_BUILD_DIR/testprecice -x -r detailed > boost-test-output
+    fi
+else
+    mkdir build && cd build
+    cmake -DPETSC=$PETSC -DMPI=$MPI -DPYTHON=on -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug ..
+    cmake --build . -- -j 2
+    ctest -V > $TRAVIS_BUILD_DIR/tests/boost-test-output
+fi
