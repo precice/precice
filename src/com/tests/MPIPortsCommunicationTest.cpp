9942ec65099eafe9b3214efbd62630babf8b92bc
uekerman <benjamin.uekermann@gmail.com>
2019-03-05 18:22:11 +0100

    removed disabled and no-longer-supported tests in com, closes #187

--- src/com/tests/MPIPortsCommunicationTest.cpp
+++ src/com/tests/MPIPortsCommunicationTest.cpp
@@ -29,15 +29,6 @@ BOOST_AUTO_TEST_CASE(SendReceiveFourProcesses,
   TestSendReceiveFourProcesses<MPIPortsCommunication>();
 }
 
-/// Disabled because acceptConnection lost ability to accept multiple connections
-BOOST_AUTO_TEST_CASE(SendReceiveFourProcessesV2,
-                     * testing::MinRanks(4)
-                     * boost::unit_test::fixture<testing::SyncProcessesFixture>()
-                     * boost::unit_test::disabled())
-{
-  TestSendReceiveFourProcessesV2<MPIPortsCommunication>();
-}
-
 BOOST_AUTO_TEST_CASE(SendReceiveTwoProcessesServerClient,
                      * testing::MinRanks(2)
                      * boost::unit_test::fixture<testing::SyncProcessesFixture>()
