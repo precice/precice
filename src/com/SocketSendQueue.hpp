cbb7308acb32bed705830bf096528c0789cf7dcb
David Sommer <david97sommer@gmail.com>
2019-03-07 16:04:54 +0100

    SendQueue Destructor

--- src/com/SocketSendQueue.hpp
+++ src/com/SocketSendQueue.hpp
@@ -6,6 +6,8 @@
 #include <boost/asio.hpp>
 #include <functional>
 #include <mutex>
+#include "logging/Logger.hpp"
+
 namespace precice
 {
 namespace com
@@ -20,10 +22,15 @@ class SendQueue
     using TCP           = boost::asio::ip::tcp;
     using SocketService = boost::asio::stream_socket_service<TCP>;
     using Socket   = boost::asio::basic_stream_socket<TCP, SocketService>;
+    SendQueue() =default;
+    SendQueue(const SendQueue&) =delete;
+    SendQueue& operator=(const SendQueue&) =delete;
 
     void push(std::shared_ptr<Socket> sock, boost::asio::const_buffers_1 data, std::function<void()> callback);
+    ~SendQueue();
 
   private:
+    logging::Logger _log{"com::SendQueue"};
 
     void dispatch();
 
