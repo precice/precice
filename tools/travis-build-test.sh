234fac76be8e9a5deaf141a458405d195d014348
Frederic Simonis <simonisfrederic@gmail.com>
2019-03-11 12:31:25 +0100

    Add ccache caching in travis

--- tools/travis-build-test.sh
+++ tools/travis-build-test.sh
@@ -17,7 +17,7 @@ if [ "$BUILD_TYPE" = "SCONS" ]; then
 else
     # CMake build
     mkdir $TRAVIS_BUILD_DIR/build && cd $TRAVIS_BUILD_DIR/build
-    cmake -DPETSC=$PETSC -DMPI=$MPI -DPYTHON=on -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug $TRAVIS_BUILD_DIR
+    cmake -DPETSC=$PETSC -DMPI=$MPI -DPYTHON=on -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER_LAUNCHER=ccache $TRAVIS_BUILD_DIR
     cmake --build . -- -j $(nproc)
     ctest --output-on-failure -O $TRAVIS_BUILD_DIR/tests/boost-test-output
 fi
