f0af4e6aa056423ba3bf8fd860cdcc9878fec72c
Frédéric Simonis <simonisfrederic@gmail.com>
2019-03-11 15:44:38 +0100

    WIP Add Travis coverage report using codecov (#327)
    * Add Travis coverage report using codecov
    * Add name to coverage job
    * Add lcov as global package

---
+++ tools/travis-coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# CMake build
+mkdir $TRAVIS_BUILD_DIR/build && cd $TRAVIS_BUILD_DIR/build
+CXX=g++
+CXXFLAGS="--coverage"
+LDFLAGS="--coverage"
+cmake -DPETSC=on -DMPI=on -DPYTHON=on -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=mpicxx $TRAVIS_BUILD_DIR
+cmake --build . -- -j $(nproc)
+ctest
