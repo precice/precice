88e682c6d9cab19316121d89b28208f05a070980
Frederic Simonis <simonisfrederic@gmail.com>
2019-03-13 17:08:43 +0100

    Add explicit flush in tests

--- src/io/tests/TXTWriterReaderTest.cpp
+++ src/io/tests/TXTWriterReaderTest.cpp
@@ -13,6 +13,7 @@ BOOST_AUTO_TEST_CASE(TXTWriterReaderTest, * testing::OnMaster())
     Eigen::Matrix<double, 1, 2> output(1, 2);
     TXTWriter                   txtWriter("io-TXTWriterReaderTest-matrix-1by2.log");
     txtWriter.write(output);
+    txtWriter.flush();
     Eigen::Matrix<double, 1, 2> input(1, 2);
     TXTReader                   txtReader("io-TXTWriterReaderTest-matrix-1by2.log");
     txtReader.read(input);
@@ -23,6 +24,7 @@ BOOST_AUTO_TEST_CASE(TXTWriterReaderTest, * testing::OnMaster())
     Eigen::Matrix<double, 2, 1> output(1, 2);
     TXTWriter                   txtWriter("io-TXTWriterReaderTest-matrix-2by1.log");
     txtWriter.write(output);
+    txtWriter.flush();
     Eigen::Matrix<double, 2, 1> input(1, 2);
     TXTReader                   txtReader("io-TXTWriterReaderTest-matrix-2by1.log");
     txtReader.read(input);
@@ -37,6 +39,7 @@ BOOST_AUTO_TEST_CASE(TXTWriterReaderTest, * testing::OnMaster())
 
     Eigen::VectorXd vecOutput = Eigen::VectorXd::Constant(1, 1);
     txtWriter.write(vecOutput);
+    txtWriter.flush();
 
     Eigen::Matrix<double, 3, 3> matInput;
     TXTReader                   txtReader("io-TXTWriterReaderTest-matrix-3by3.log");
@@ -52,6 +55,7 @@ BOOST_AUTO_TEST_CASE(TXTWriterReaderTest, * testing::OnMaster())
     Eigen::Vector3d output(1, 2, 3);
     TXTWriter       txtWriter("io-TXTWriterReaderTest-vector-3.log");
     txtWriter.write(output);
+    txtWriter.flush();
 
     Eigen::Vector3d input;
     TXTReader       txtReader("io-TXTWriterReaderTest-vector-3.log");
