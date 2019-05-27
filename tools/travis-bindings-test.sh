c42b2fa6f6a827b96033d2cc6b1b5c20caba44b0
BenjaminRueth <benjamin.rueth@tum.de>
2019-05-27 12:29:39 +0200

    tries to fix problems with python3

--- tools/travis-bindings-test.sh
+++ tools/travis-bindings-test.sh
@@ -6,5 +6,7 @@ set -e
 export PYTHON_BINDINGS_DIR=$TRAVIS_BUILD_DIR/src/precice/bindings/python_future
 cd $PYTHON_BINDINGS_DIR
 
+export PYTHONPATH=/usr/local/lib/python3/
+
 python3 setup.py test
 
