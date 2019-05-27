f819ff385ef65fca515a6c3861f9c36f3906e560
BenjaminRueth <benjamin.rueth@tum.de>
2019-05-27 12:53:12 +0200

    adds python version

--- tools/travis-bindings-test.sh
+++ tools/travis-bindings-test.sh
@@ -6,8 +6,6 @@ set -e
 export PYTHON_BINDINGS_DIR=$TRAVIS_BUILD_DIR/src/precice/bindings/python_future
 cd $PYTHON_BINDINGS_DIR
 
-export PYTHONPATH=/usr/local/lib/python3/
-
 pip3 install --user setuptools cython
 python3 setup.py test
 
