c7201c3bd74af04809e68f27cc84aacdb952fd07
Frederic Simonis <simonisfrederic@gmail.com>
2019-06-26 10:33:59 +0200

    Add Autoupdate of Commit Info

--- cmake/DetectGitCommit.cmake
+++ cmake/DetectGitCommit.cmake
@@ -1,18 +1,27 @@
+#
+# Git Commit Info
+#
+# This script integrates the git commit info into the preCICE library.
+#
+# If git is discoverable on the machine, then this script will register an
+# additional target which is added as dependency to the precice target.
+# The new target launches the script cmake_refresh_git_revision.cmake which
+# automatically generates and updates the version.cpp file.
+#
+# If git was not found, this script simply configures a dummy versions.cpp file.
+#
+
 find_package(Git QUIET)
-set(preCICE_COMMIT "no-info")
 if(GIT_FOUND)
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --tags --broken
+  add_custom_target(GitCommit
+    COMMAND ${CMAKE_COMMAND} -DSRC="${PROJECT_SOURCE_DIR}/src/versions.cpp.in" -DDST="${PROJECT_BINARY_DIR}/src/versions.cpp" -P ${CMAKE_CURRENT_LIST_DIR}/cmake_refresh_git_revision.cmake
     WORKING_DIRECTORY ${preCICE_SOURCE_DIR}
-    RESULT_VARIABLE PRECICE_COMMIT_RET
-    OUTPUT_VARIABLE preCICE_COMMIT
-    OUTPUT_STRIP_TRAILING_WHITESPACE
+    BYPRODUCTS src/versions.cpp
+    DEPENDS src/versions.cpp.in
     )
-  if("${PRECICE_COMMIT_RET}" EQUAL "0")
-    message(STATUS "Revision status: ${preCICE_COMMIT}")
-  else()
-    message(WARNING "Revision status: Detection failed!")
-  endif()
+  add_dependencies(precice GitCommit)
 else(GIT_FOUND)
-  message(STATUS "Revision status: No source version control detected.")
+  set(preCICE_COMMIT "no-info")
+  configure_file("${PROJECT_SOURCE_DIR}/src/versions.cpp.in" "${PROJECT_BINARY_DIR}/src/versions.cpp" @ONLY)
+  message(STATUS "Revision status: No git installation detected.")
 endif(GIT_FOUND)
