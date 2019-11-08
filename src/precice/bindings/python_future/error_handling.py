d8df4bce131abbffa030871c602e0af10d5d4fad
ishaandesai <ishaandesai@gmail.com>
2019-11-08 20:26:02 +0100

    Changing function arguments of error checks

--- src/precice/bindings/python_future/error_handling.py
+++ src/precice/bindings/python_future/error_handling.py
@@ -2,14 +2,15 @@
 Functions to check the type of input argument against what is expected
 and raise an Exception if mismatch found
 """
-import numpy as np
 
-def check1D(self, func_name, usr_input, true_val):
-    if (usr_input != true_val):
+
+def check1D(func_name, usr_input, true_val):
+    if usr_input != true_val:
         raise Exception("Function {} received position of type {} but expects a 1D numpy \
-                         array with {} as position input".format(func_name, usr_input, true_val))
+                         array with {} values as position input".format(func_name, usr_input, true_val))
 
-def check2D(self, func_name, usr_input, true_val):
-    if (usr_input != true_val):
-        raise Exception("Function {} received positions of dimension {}expects a numpy array \
+
+def check2D(func_name, usr_input, true_val):
+    if usr_input != true_val:
+        raise Exception("Function {} received positions of dimension {}expects a numpy array of dimensions \
                          [no. of vertices x {}] as positions input".format(func_name, usr_input, true_val))
