3e72d2d5069199a912d681a8f5da466422c752a7
Frédéric Simonis <simonisfrederic@gmail.com>
2019-11-13 12:16:38 +0100

    Add extensive error message

--- cmake/ValidateCpp11Library.cmake
+++ cmake/ValidateCpp11Library.cmake
@@ -13,7 +13,16 @@ function(check_cpp_11_library_support)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
   check_cxx_source_compiles("${CPP11CheckSource}" CPP11LibraryConforming)
   if(NOT "${CPP11LibraryConforming}")
-    message(FATAL_ERROR "The standard library used by your compiler is not C++11 compliant, as it does not support time manipulation!")
+    message(FATAL_ERROR "
+    The standard library used by your compiler ${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION} is not C++11 compliant.
+    It does not support time manipulation (N2071 and N2072)!
+    Please upgrade your compiler at least to:
+    * GCC 5
+    * LLVM 3.8
+    * MSVC 19.0
+    * Intel 15 in conjunction with GCC 5
+    * PGI 2015 in conjunction with GCC 5
+    ")
   endif()
   unset(CPP11LibraryConforming)
   unset(CPP11CheckSource)
