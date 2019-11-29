499bd6266977594c2904dd54d7464cf2dd605df2
Kyle Davis <kyle.davis@ipvs.uni-stuttgart.de>
2019-11-29 14:12:11 +0100

    Update solverdummies to use original input commands

--- tools/solverdummies/fortran/solverdummy.f90
+++ tools/solverdummies/fortran/solverdummy.f90
@@ -1,9 +1,13 @@
 PROGRAM main
   IMPLICIT NONE
-  CHARACTER*50                    :: config, participantName, meshName, writeInitialData, readItCheckp, writeItCheckp
-  INTEGER                         :: rank, commsize, ongoing, dimensions, meshID, vertexID, bool
+  CHARACTER*50                    :: config, solverParameters, participantName, meshName, writeInitialData 
+  CHARACTER*50                    :: DataOneName, DataTwoName
+  CHARACTER*50                    :: readItCheckp, writeItCheckp
+  INTEGER                         :: rank, commsize, ongoing, dimensions, meshID, bool, n, i,j
+  INTEGER			  :: DataOne_ID, DataTwo_ID
   REAL                            :: dtlimit
-  REAL, DIMENSION(:), ALLOCATABLE :: vertex
+  DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: vertex, DataOne, DataTwo
+  INTEGER, DIMENSION(:), ALLOCATABLE :: vertexID
       
   ! Constants in f90 have to be prefilled with blanks to be compatible with preCICE
   writeInitialData(1:50)='                                                  '
@@ -19,48 +23,78 @@ PROGRAM main
   CALL getarg(2, participantName)
   CALL getarg(3, meshName)
 
+  IF(participantName .eq. 'SolverOne') THEN  
+    DataOneName = 'Forces'
+    DataTwoName = 'Velocities'
+  ENDIF
+  IF(participantName .eq. 'SolverTwo') THEN  
+    DataOneName = 'Velocities'
+    DataTwoName = 'Forces'
+  ENDIF
+
   rank = 0
   commsize = 1
+  dtlimit = 1
+  n = 3
   CALL precicef_create(participantName, config, rank, commsize)
 
-  ! Allocate dummy mesh with only one vertex 
+  ! Allocate dummy mesh with only three vertices 
   CALL precicef_get_dims(dimensions)
-  ALLOCATE(vertex(dimensions))
+  ALLOCATE(vertex(n*dimensions))
+  ALLOCATE(vertexID(n))
   vertex = 0
+  vertexID = 0
+
   CALL precicef_get_mesh_id(meshName, meshID)
-  CALL precicef_set_vertex(meshID, vertex, vertexID)  
-  DEALLOCATE(vertex)    
-        
+  print*, 'meshID = ', meshID
+
+  do i = 1,n,1
+    do j = 1,dimensions,1
+      vertex((i - 1)*dimensions + j ) = i
+    enddo
+    vertexID(i) = i
+  enddo
+
+  CALL precicef_set_vertices(meshID, n*dimensions, vertex, vertexID)  
+  DEALLOCATE(vertex)     
+
+  CALL precicef_get_data_id(DataOneName,meshID,DataOne_ID)
+  CALL precicef_get_data_id(DataTwoName,meshID,DataTwo_ID)
+
+  ALLOCATE(DataOne(n))
+  ALLOCATE(DataTwo(n))
+  DataOne = 0
+  DataTwo = 0
+
   CALL precicef_initialize(dtlimit)            
 
   CALL precicef_action_required(writeInitialData, bool)
   IF (bool.EQ.1) THEN
-    WRITE (*,*) 'DUMMY: Writing initial data'
+  WRITE (*,*) 'DUMMY: Writing initial data'
   ENDIF
   CALL precicef_initialize_data()
 
   CALL precicef_ongoing(ongoing)
   DO WHILE (ongoing.NE.0)
-  
-    CALL precicef_action_required(writeItCheckp, bool)
-    IF (bool.EQ.1) THEN
-      WRITE (*,*) 'DUMMY: Writing iteration checkpoint'
-      CALL precicef_fulfilled_action(writeItCheckp)
-    ENDIF
 
+    WRITE (*,*) 'DUMMY: Reading iteration checkpoint'
+    do i = 1,n,1
+      CALL precicef_read_sdata(DataTwo_ID, vertexID(i), DataTwo(i))
+    enddo
+
+    DataOne = DataTwo + 1
+
+    WRITE (*,*) 'DUMMY: Writing iteration checkpoint'
+    do i=1,n,1
+      CALL precicef_write_sdata(DataOne_ID, vertexID(i), DataOne(i));
+    enddo
+
+    WRITE (*,*) 'DUMMY: Advancing in time'
     CALL precicef_advance(dtlimit)
     CALL precicef_ongoing(ongoing)
 
-    CALL precicef_action_required(readItCheckp, bool)
-    IF (bool.EQ.1) THEN
-      WRITE (*,*) 'DUMMY: Reading iteration checkpoint'
-      CALL precicef_fulfilled_action(readItCheckp)
-    ELSE
-      WRITE (*,*) 'DUMMY: Advancing in time'
-    ENDIF
-    
   ENDDO
-  
+
   CALL precicef_finalize()
   WRITE (*,*) 'DUMMY: Closing Fortran solver dummy...'
 
--- tools/solverdummies/python/solverdummy.py
+++ tools/solverdummies/python/solverdummy.py
@@ -1,8 +1,16 @@
-from __future__ import division
-
+#import os
+#import shutil
 import argparse
 import numpy as np
-import precice
+import precice_future as precice
+#import subprocess
+#import os.path
+
+#import re
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+#import fileinput
 
 parser = argparse.ArgumentParser()
 parser.add_argument("configurationFileName", help="Name of the xml config file.", type=str)
@@ -20,38 +28,62 @@ configuration_file_name = args.configurationFileName
 participant_name = args.participantName
 mesh_name = args.meshName
 
-n = 1
+if (participant_name == 'SolverOne'):
+  DataOne_Name='Forces'
+  DataTwo_Name='Velocities'
 
-solver_process_index = 0
-solver_process_size = 1
+if (participant_name == 'SolverTwo'):
+  DataTwo_Name='Forces'
+  DataOne_Name='Velocities'
+
+n = 3				#Number of vertices
+dt = 1				#Time step
+nodeVertexIDs = n*[0.0]		#
+solver_process_index = 0	#Rank number
+solver_process_size = 1		#Total number of ranks
 
 interface = precice.Interface(participant_name, solver_process_index, solver_process_size)
 interface.configure(configuration_file_name)
-    
+
+dimensions = interface.get_dimensions()    
 mesh_id = interface.get_mesh_id(mesh_name)
+vertices = np.zeros((n, dimensions))
 
-dimensions = interface.get_dimensions()
-vertex = np.zeros(dimensions)
-data_indices = np.zeros(n)
+i = 0
+for x in range(0,n):
+    for y in range(0,dimensions):
+        vertices[x,y] = i + 1
+    i = i + 1
 
-interface.set_mesh_vertices(mesh_id, n, vertex, data_indices)
+DataOne = np.zeros(n)
+DataTwo = np.zeros(n)
+
+for x in range(0, n):
+	DataTwo[x]=1
+	DataOne[x]=1
+
+data_indices = interface.set_mesh_vertices(mesh_id, vertices)
+
+DataOne_ID = interface.get_data_id(DataOne_Name,mesh_id)
+DataTwo_ID = interface.get_data_id(DataTwo_Name,mesh_id)
 
 dt = interface.initialize()
-    
+
 while interface.is_coupling_ongoing():
-   
-    if interface.is_action_required(precice.action_write_iteration_checkpoint()):
-        print("DUMMY: Writing iteration checkpoint")
-        interface.fulfilled_action(precice.action_write_iteration_checkpoint())
-    
-    dt = interface.advance(dt)
-    
-    if interface.is_action_required(precice.action_read_iteration_checkpoint()):
-        print("DUMMY: Reading iteration checkpoint")
-        interface.fulfilled_action(precice.action_read_iteration_checkpoint())
-    else:
-        print("DUMMY: Advancing in time")
-    
+	   
+	print("DUMMY: Reading iteration checkpoint")
+	for i in range(0,n): 
+		DataTwo[i] = interface.read_scalar_data(DataTwo_ID, data_indices[i])            
+
+	DataOne = DataTwo + 1
+
+	print("DUMMY: Writing iteration checkpoint")
+	interface.fulfilled_action(precice.action_write_iteration_checkpoint())
+	for i in range(0,n):    
+		interface.write_scalar_data(DataOne_ID, data_indices[i], DataOne[i])
+
+	print("DUMMY: Advancing in time")
+	dt = interface.advance(dt)
+
 interface.finalize()
 print("DUMMY: Closing python solver dummy...")
-
