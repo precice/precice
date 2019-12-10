c7904ae7abf9fe3dd908c335c053b2e6691eabb0
Frédéric Simonis <simonisfrederic@gmail.com>
2019-12-10 16:32:38 +0100

    Change Travis to mute mpiports tests

--- tools/travis-build-test.sh
+++ tools/travis-build-test.sh
@@ -20,5 +20,8 @@ else
     mkdir $PRECICE_BUILD_DIR && cd $PRECICE_BUILD_DIR
     cmake -DPETSC=$PETSC -DMPI=$MPI -DPYTHON=on -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER_LAUNCHER=ccache $TRAVIS_BUILD_DIR
     cmake --build . -- -j $(nproc)
-    ctest --output-on-failure -O $TRAVIS_BUILD_DIR/tests/boost-test-output
+    # Run tests with moderate console output
+    ctest -LE mpiports --output-on-failure -O $TRAVIS_BUILD_DIR/tests/boost-test-output
+    # Run tests which should be muted
+    ctest -L mpiports
 fi
