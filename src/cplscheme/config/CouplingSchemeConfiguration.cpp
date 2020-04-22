f08392219212aa6c4350153f4b4d9ab24d619b86
BenjaminRueth <benjamin.rueth@tum.de>
2020-04-22 12:58:56 +0200

    Fix PRECICE_CHECK on time window size for method=first-participant

--- src/cplscheme/config/CouplingSchemeConfiguration.cpp
+++ src/cplscheme/config/CouplingSchemeConfiguration.cpp
@@ -136,7 +136,9 @@ const PtrCouplingScheme &CouplingSchemeConfiguration::getCouplingScheme(
 {
   PRECICE_CHECK(utils::contained(participantName, _couplingSchemes),
                 "No coupling scheme defined for "
-                    << "participant \"" << participantName << "\"!");
+                    << "participant \"" << participantName << "\". "
+                    << "Please make sure to provide at least one <coupling-scheme:TYPE> in your "
+                    << "precice-config.xml that couples this participant using the <participants .../> tag.");
   return _couplingSchemes.find(participantName)->second;
 }
 
@@ -149,32 +151,74 @@ void CouplingSchemeConfiguration::xmlTagCallback(
     _config.type = tag.getName();
     _accelerationConfig->clear();
   } else if (tag.getName() == TAG_PARTICIPANTS) {
-    _config.participants.push_back(tag.getStringAttributeValue(ATTR_FIRST));
-    _config.participants.push_back(tag.getStringAttributeValue(ATTR_SECOND));
+    std::string first = tag.getStringAttributeValue(ATTR_FIRST);
+    _config.participants.push_back(first);
+    std::string second = tag.getStringAttributeValue(ATTR_SECOND);
+    PRECICE_CHECK(std::find(_config.participants.begin(), _config.participants.end(), second) == _config.participants.end(),
+                  "Provided first participant equals second participant in coupling scheme. Please correct the <participants first=\""
+                  << first
+                  << "\" second=\""
+                  << second
+                  << "\" /> tag in the <coupling-scheme:...> of your precice-config.xml");
+    _config.participants.push_back(second);
   } else if (tag.getName() == TAG_PARTICIPANT) {
     PRECICE_ASSERT(_config.type == VALUE_MULTI);
-    bool control = tag.getBooleanAttributeValue(ATTR_CONTROL);
+    bool control                = tag.getBooleanAttributeValue(ATTR_CONTROL);
+    std::string participantName = tag.getStringAttributeValue(ATTR_NAME);
+    PRECICE_CHECK(std::find(_config.participants.begin(), _config.participants.end(), participantName) == _config.participants.end()
+                      && participantName.compare(_config.controller) != 0,
+                  "Participant \""
+                      << participantName
+                      << "\" is provided multiple times to multi coupling scheme. Please make sure that you do not provide the participant multiple times via the <participant name=\""
+                      << participantName
+                      << "\" /> tag in the <coupling-scheme:...> of your precice-config.xml");
     if (control) {
       PRECICE_CHECK(not _config.setController,
-                    "Only one controller per MultiCoupling can be defined");
-      _config.controller    = tag.getStringAttributeValue(ATTR_NAME);
+                    "Only one controller per MultiCouplingScheme can be defined. Please check the <participant name=\""
+                      << participantName
+                      << "\" control=\""
+                      << control
+                      << "\" /> tag in the <coupling-scheme:...> of your precice-config.xml");
+      _config.controller = participantName;
       _config.setController = true;
     } else {
-      _config.participants.push_back(tag.getStringAttributeValue(ATTR_NAME));
+      _config.participants.push_back(participantName);
     }
 
   } else if (tag.getName() == TAG_MAX_TIME) {
     _config.maxTime = tag.getDoubleAttributeValue(ATTR_VALUE);
-    PRECICE_CHECK(_config.maxTime > 0,"Maximum time has to be larger than zero.");
+    PRECICE_CHECK(_config.maxTime > 0,"Maximum time has to be larger than zero. Please check the <max-time value=\""
+                                             << _config.maxTime << "\""
+                                             <<" /> tag in the <coupling-scheme:...> of your precice-config.xml");
   } else if (tag.getName() == TAG_MAX_TIME_WINDOWS) {
     _config.maxTimeWindows = tag.getIntAttributeValue(ATTR_VALUE);
-    PRECICE_CHECK(_config.maxTimeWindows > 0,"Maximum number of time windows has to be larger than zero.");
+    PRECICE_CHECK(_config.maxTimeWindows > 0,"Maximum number of time windows has to be larger than zero. Please check the <max-time-windows value=\""
+        << _config.maxTimeWindows << "\""
+        << "/> tag in the <coupling-scheme:...> of your precice-config.xml");
   } else if (tag.getName() == TAG_TIME_WINDOW_SIZE) {
     _config.timeWindowSize = tag.getDoubleAttributeValue(ATTR_VALUE);
-    PRECICE_CHECK(_config.timeWindowSize > 0,"Time window size has to be larger than zero.");
     _config.validDigits = tag.getIntAttributeValue(ATTR_VALID_DIGITS);
     PRECICE_CHECK((_config.validDigits >= 1) && (_config.validDigits < 17),"Valid digits of time window size has to be between 1 and 16.");
     _config.dtMethod = getTimesteppingMethod(tag.getStringAttributeValue(ATTR_METHOD));
+    if(_config.dtMethod == constants::TimesteppingMethod::FIXED_TIME_WINDOW_SIZE) {
+      PRECICE_CHECK(_config.timeWindowSize > 0, "Time window size has to be larger than zero. Please check the <time-window-size "
+          << "value=\"" << _config.timeWindowSize << "\" "
+          << "valid-digits=\"" << _config.validDigits << "\" "
+          << "method=\"" << tag.getStringAttributeValue(ATTR_METHOD) << "\" "
+          << "/> tag in the <coupling-scheme:...> of your precice-config.xml");
+    } else {
+      PRECICE_ASSERT(_config.dtMethod == constants::TimesteppingMethod::FIRST_PARTICIPANT_SETS_TIME_WINDOW_SIZE);
+      PRECICE_CHECK(_config.timeWindowSize == -1, "Time window size value has to be equal to -1 (default), if method=\"first-participant\" is used. Please check the <time-window-size "
+          << "value=\"" << _config.timeWindowSize << "\" "
+          << "valid-digits=\"" << _config.validDigits << "\" "
+          << "method=\"" << tag.getStringAttributeValue(ATTR_METHOD) << "\" "
+          << "/> tag in the <coupling-scheme:...> of your precice-config.xml");
+    }
+    PRECICE_CHECK((_config.validDigits >= 1) && (_config.validDigits < 17),"Valid digits of time window size has to be between 1 and 16. Please check the <time-window-size "
+        << "value=\"" << _config.timeWindowSize << "\" "
+        << "valid-digits=\"" << _config.validDigits << "\" "
+        << "method=\"" << tag.getStringAttributeValue(ATTR_METHOD) << "\" "
+        << "/> tag in the <coupling-scheme:...> of your precice-config.xml");
   } else if (tag.getName() == TAG_ABS_CONV_MEASURE) {
     std::string dataName = tag.getStringAttributeValue(ATTR_DATA);
     std::string meshName = tag.getStringAttributeValue(ATTR_MESH);
@@ -222,12 +266,12 @@ void CouplingSchemeConfiguration::xmlTagCallback(
         }
       }
     }
-    if (exchangeData.get() == nullptr) {
-      std::ostringstream stream;
-      stream << "Mesh \"" << nameMesh << "\" with data \"" << nameData
-             << "\" not defined at definition of coupling scheme";
-      throw std::runtime_error{stream.str()};
-    }
+    PRECICE_CHECK(exchangeData.get(), "Mesh \"" << nameMesh << "\" with data \"" << nameData
+                                   << "\" not defined. Please check the <exchange "
+                                      "data=\"" << nameData << "\" "
+                                      "mesh=\"" << nameMesh << "\" "
+                                      "from=\"" << nameParticipantFrom << "\" "
+                                      "to=\"" << nameParticipantTo << "\" /> tag in the <coupling-scheme:... /> of your precice-config.xml.");
     _meshConfig->addNeededMesh(nameParticipantFrom, nameMesh);
     _meshConfig->addNeededMesh(nameParticipantTo, nameMesh);
     _config.exchanges.push_back(std::make_tuple(exchangeData, exchangeMesh,
@@ -287,7 +331,9 @@ void CouplingSchemeConfiguration::xmlEndTagCallback(
       _config = Config();
     } else if (_config.type == VALUE_MULTI) {
       PRECICE_CHECK(_config.setController,
-                    "One controller per MultiCoupling need to be defined");
+                    "One controller per MultiCoupling needs to be defined. "
+                    "Please check the <participant name=... /> tags in the <coupling-scheme:... /> of your precice-config.xml. "
+                    "Make sure that at least one participant tag provides the attribute <participant name=... control=\"True\"/>.");
       for (std::string &accessor : _config.participants) {
         PtrCouplingScheme scheme = createMultiCouplingScheme(accessor);
         addCouplingScheme(scheme, accessor);
@@ -377,13 +423,14 @@ void CouplingSchemeConfiguration::addTypespecifcSubtags(
     addTagMaxIterations(tag);
     addTagExtrapolation(tag);
   } else {
-    PRECICE_ERROR("Unknown coupling scheme type!");
+    // If wrong coupling scheme type is provided, this is already caught by the config parser. If the assertion below is triggered, it's a bug in preCICE, not wrong usage.
+    PRECICE_ASSERT(false, "Unknown coupling scheme.");
   }
 }
 
 void CouplingSchemeConfiguration::addTransientLimitTags(
     const std::string &type,
-    xml::XMLTag &tag)
+    xml::XMLTag &      tag)
 {
   using namespace xml;
   XMLTag               tagMaxTime(*this, TAG_MAX_TIME, XMLTag::OCCUR_NOT_OR_ONCE);
@@ -821,14 +868,32 @@ void CouplingSchemeConfiguration::addDataToBeExchanged(
     const std::string &from = get<2>(tuple);
     const std::string &to   = get<3>(tuple);
 
-    PRECICE_CHECK(to != from, "You cannot define an exchange from and to the same participant");
+    PRECICE_CHECK(to != from, "You cannot define an exchange from and to the same participant. "
+                              "Please check the <exchange "
+                              "data=\"" << data->getName() << "\" "
+                              "mesh=\"" << mesh->getName() << "\" "
+                              "from=\"" << from << "\" "
+                              "to=\"" << to << "\" "
+                              "/> tag in the <coupling-scheme:... /> of your precice-config.xml.");
 
     if (not(utils::contained(from, _config.participants) || from == _config.controller)) {
-      throw std::runtime_error{"Participant \"" + from + "\" is not configured for coupling scheme"};
+      PRECICE_CHECK(false, "Participant \"" + from + "\" is not configured for coupling scheme. "
+                                          "Please check the <exchange "
+                                          "data=\"" << data->getName() << "\" "
+                                          "mesh=\"" << mesh->getName() << "\" "
+                                          "from=\"" << from << "\" "
+                                          "to=\"" << to << "\" "
+                                          "/> tag in the <coupling-scheme:... /> of your precice-config.xml.");
     }
 
     if (not(utils::contained(to, _config.participants) || to == _config.controller)) {
-      throw std::runtime_error{"Participant \"" + to + "\" is not configured for coupling scheme"};
+      PRECICE_CHECK(false, "Participant \"" + to + "\" is not configured for coupling scheme. "
+                                          "Please check the <exchange "
+                                          "data=\"" << data->getName() << "\" "
+                                          "mesh=\"" << mesh->getName() << "\" "
+                                          "from=\"" << from << "\" "
+                                          "to=\"" << to << "\" "
+                                          "/> tag in the <coupling-scheme:... /> of your precice-config.xml.");
     }
 
     bool requiresInitialization = get<4>(tuple);
