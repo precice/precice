4be91809edc3e139090ba71a6a1f3d64caf53db8
oguzziya <oguzkoseomur@gmail.com>
2020-04-24 23:14:05 +0200

    Format fix

--- src/mesh/tests/BoundingBoxTest.cpp
+++ src/mesh/tests/BoundingBoxTest.cpp
@@ -12,15 +12,15 @@ BOOST_AUTO_TEST_CASE(ExpandBy_BoundingBox)
 {
   PRECICE_TEST(1_rank);
   BoundingBox bb1({0.0, 1.0,
-                    0.0, 1.0,
-                    0.0, 1.0});
+                   0.0, 1.0,
+                   0.0, 1.0});
   BoundingBox bb2({-1.0, 0.5,
-                    2.0, 3.5,
-                    0.0, 4.0});
+                   2.0, 3.5,
+                   0.0, 4.0});
   bb1.expandBy(bb2);
   std::vector<double> compareData = {-1.0, 1.0,
-                                        0.0, 3.5,
-                                        0.0, 4.0};
+                                     0.0, 3.5,
+                                     0.0, 4.0};
   BOOST_TEST(bb1.dataVector() == compareData);
 }
 
@@ -28,13 +28,13 @@ BOOST_AUTO_TEST_CASE(ExpandBy_Vertex)
 {
   PRECICE_TEST(1_rank);
   BoundingBox bb({0.0, 1.0,
-                   0.0, 1.0,
-                   0.0, 1.0});
-  Vertex v1(Eigen::Vector3d(-1.0, 3.0, 0.5), 0);
+                  0.0, 1.0,
+                  0.0, 1.0});
+  Vertex      v1(Eigen::Vector3d(-1.0, 3.0, 0.5), 0);
   bb.expandBy(v1);
   std::vector<double> compareData = {-1.0, 1.0,
-                                      0.0, 3.0,
-                                      0.0, 1.0};
+                                     0.0, 3.0,
+                                     0.0, 1.0};
   BOOST_TEST(bb.dataVector() == compareData);
 }
 
@@ -42,9 +42,9 @@ BOOST_AUTO_TEST_CASE(ExpandBy_Radius)
 {
   PRECICE_TEST(1_rank);
   BoundingBox bb({0.0, 1.0,
-                   0.5, 2.0,
+                  0.5, 2.0,
                   -1.0, 3.0});
-  double supportRadius = 1.0;
+  double      supportRadius = 1.0;
   bb.expandBy(supportRadius);
   std::vector<double> compareData = {-1.0, 2.0,
                                      -0.5, 3.0,
@@ -58,7 +58,7 @@ BOOST_AUTO_TEST_CASE(Scaling)
   BoundingBox bb1({-1.0, 1.0,
                    0.5, 2.0,
                    1.0, 1.5});
-  double safetyFactor = 2.0;
+  double      safetyFactor = 2.0;
   bb1.scaleBy(safetyFactor);
   std::vector<double> compareData = {-5.0, 5.0,
                                      -3.5, 6.0,
@@ -70,7 +70,7 @@ BOOST_AUTO_TEST_CASE(CenterOfGravity)
 {
   BoundingBox bb({0.0, 1.0,
                   -1.0, 3.0,
-                   2.0, 4.0});
+                  2.0, 4.0});
 
   Eigen::Vector3d compareCOG(0.5, 1.0, 3.0);
   BOOST_TEST(compareCOG == bb.center());
@@ -79,21 +79,21 @@ BOOST_AUTO_TEST_CASE(CenterOfGravity)
 BOOST_AUTO_TEST_CASE(Area)
 {
   BoundingBox bb({0.0, 1.0,
-                 -1.0, 3.0,
+                  -1.0, 3.0,
                   2.0, 4.0});
   {
-    std::vector<bool> deadAxis = {false, false, true};
-    double compareArea = 4.0;
+    std::vector<bool> deadAxis    = {false, false, true};
+    double            compareArea = 4.0;
     BOOST_TEST(bb.getArea(deadAxis) == compareArea);
   }
   {
-    std::vector<bool> deadAxis = {false, true, false};
-    double compareArea = 2.0;
+    std::vector<bool> deadAxis    = {false, true, false};
+    double            compareArea = 2.0;
     BOOST_TEST(bb.getArea(deadAxis) == compareArea);
   }
   {
-    std::vector<bool> deadAxis = {true, false, false};
-    double compareArea = 8.0;
+    std::vector<bool> deadAxis    = {true, false, false};
+    double            compareArea = 8.0;
     BOOST_TEST(bb.getArea(deadAxis) == compareArea);
   }
 }
@@ -102,10 +102,10 @@ BOOST_AUTO_TEST_CASE(Overlapping)
 {
   BoundingBox bb1({0.0, 1.0,
                    -1.0, 3.0,
-                    2.0, 4.0});
+                   2.0, 4.0});
   BoundingBox bb2({-1.0, 0.5,
-                    2.0, 5.0,
-                    1.0, 3.0});
+                   2.0, 5.0,
+                   1.0, 3.0});
   BoundingBox bb3({2.0, 5.0,
                    4.0, 5.0,
                    0.0, 1.0});
@@ -118,14 +118,14 @@ BOOST_AUTO_TEST_CASE(Comparison)
 {
   BoundingBox bb1({0.0, 1.0,
                    -1.0, 3.0,
-                    2.0, 4.0});
+                   2.0, 4.0});
   BoundingBox bb2({0.0, 1.0,
                    -1.0, 3.0,
-                    2.0, 4.0});
+                   2.0, 4.0});
   BoundingBox bb3({2.0, 5.0,
                    4.0, 5.0,
                    0.0, 1.0});
-  
+
   BOOST_TEST(bb1 == bb2);
   BOOST_TEST(!(bb1 == bb3));
 }
@@ -134,9 +134,9 @@ BOOST_AUTO_TEST_CASE(Contains)
 {
   BoundingBox bb({0.0, 1.0,
                   -1.0, 3.0,
-                   2.0, 4.0});
-  Vertex v1(Eigen::Vector3d(0.2, 1.0, 3.0), 0);
-  Vertex v2(Eigen::Vector3d(1.2, -2.0, 5.0), 0);
+                  2.0, 4.0});
+  Vertex      v1(Eigen::Vector3d(0.2, 1.0, 3.0), 0);
+  Vertex      v2(Eigen::Vector3d(1.2, -2.0, 5.0), 0);
 
   BOOST_TEST(bb.contains(v1));
   BOOST_TEST(!bb.contains(v2));
@@ -145,7 +145,7 @@ BOOST_AUTO_TEST_CASE(Contains)
 BOOST_AUTO_TEST_CASE(EmptyCase)
 {
   BoundingBox bb1({0.0, 1.0,
-                  -1.0, 3.0,
+                   -1.0, 3.0,
                    2.0, 4.0});
   BoundingBox bb2(3);
 
