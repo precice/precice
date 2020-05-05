c37cdc9d427b23370b098fd250e36aaa215196f6
Frédéric Simonis <simonisfrederic@gmail.com>
2020-05-05 15:19:22 +0200

    Improve PRECICE_ROOT error message

--- src/testing/Testing.cpp
+++ src/testing/Testing.cpp
@@ -12,7 +12,7 @@ std::string getPathToSources()
   precice::logging::Logger _log("testing");
   char *                   preciceRoot = std::getenv("PRECICE_ROOT");
   PRECICE_CHECK(preciceRoot != nullptr,
-                "Environment variable PRECICE_ROOT has not been set. Please set it to the precice directory.");
+                "Environment variable PRECICE_ROOT is required to run the tests, but has not been set. Please set it to the root directory of the precice repository.");
   return std::string(preciceRoot) + "/src";
 }
 
