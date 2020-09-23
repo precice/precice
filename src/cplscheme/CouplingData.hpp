1ffa5ec0085c52d57986644bb25ee0eedd9ab883
BenjaminRueth <benjamin.rueth@tum.de>
2020-09-23 16:07:18 +0200

    Deprecate constructors and members and provide better alternatives.

--- src/cplscheme/CouplingData.hpp
+++ src/cplscheme/CouplingData.hpp
@@ -11,19 +11,42 @@ namespace cplscheme {
 struct CouplingData {
   using DataMatrix = Eigen::MatrixXd;
 
+  [[deprecated("Use &(CouplingData::dataValues()). Even better: Don't use a pointer.")]]  // @todo: remove before merge, since it is C++14
   /// Data values of current iteration.
   Eigen::VectorXd *values;
+
+  /// Returns a reference to the data values.
+  Eigen::VectorXd &dataValues()
+  {
+    return data->values();
+  }
+
+  /// Returns a const reference to the data values.
+  const Eigen::VectorXd &dataValues() const
+  {
+    return data->values();
+  }
 
   /// Data values of previous iteration (1st col) and previous time windows.
   DataMatrix oldValues;
 
   mesh::PtrMesh mesh;
 
+  mesh::PtrData data;
+
   ///  True, if the data values if this CouplingData requires to be initialized by a participant.
   bool requiresInitialization;
 
+  [[deprecated("Use CouplingData::getDimensions().")]]  // @todo: remove before merge, since it is C++14
   /// dimension of one data value (scalar=1, or vectorial=interface-dimension)
   int dimension;
+
+  int getDimensions()
+  {
+    PRECICE_ASSERT(data != nullptr);
+    PRECICE_ASSERT(dimension == data->getDimensions());
+    return data->getDimensions();
+  }
 
   /**
    * @brief Default constructor, not to be used!
@@ -35,6 +58,7 @@ struct CouplingData {
     PRECICE_ASSERT(false);
   }
 
+  [[deprecated("Use CouplingData(mesh::PtrData, mesh::PtrMesh, bool requiresInitialization).")]]  // @todo: remove before merge, since it is C++14
   CouplingData(
       Eigen::VectorXd *values,
       mesh::PtrMesh    mesh,
@@ -44,6 +68,19 @@ struct CouplingData {
         mesh(mesh),
         requiresInitialization(requiresInitialization),
         dimension(dimension)
+  {
+    PRECICE_ASSERT(values != NULL);
+    PRECICE_ASSERT(mesh.use_count() > 0);
+  }
+
+  CouplingData(
+      mesh::PtrData data,
+      mesh::PtrMesh mesh,
+      bool          requiresInitialization)
+      : values(&(data->values())),
+        mesh(mesh),
+        requiresInitialization(requiresInitialization),
+        dimension(data->getDimensions())
   {
     PRECICE_ASSERT(values != NULL);
     PRECICE_ASSERT(mesh.use_count() > 0);
