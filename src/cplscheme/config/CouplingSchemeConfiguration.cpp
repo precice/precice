5166271b045e2fde8478f9f193b365d5211522fa
Frédéric Simonis <simonisfrederic@gmail.com>
2020-12-03 10:39:58 +0100

    Update src/cplscheme/config/CouplingSchemeConfiguration.cpp
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@gmail.com>

--- src/cplscheme/config/CouplingSchemeConfiguration.cpp
+++ src/cplscheme/config/CouplingSchemeConfiguration.cpp
@@ -1147,7 +1147,7 @@ void CouplingSchemeConfiguration::checkSerialImplicitAccelerationData(
       "You configured acceleration data \"" << dataName << "\" in the serial implicit coupling scheme between participants \"" << first << "\" and \"" << second << "\". "
       "For serial implicit coupling schemes, only data exchanged from the second to the first participant can be used for acceleration. Here, from \"" << second << "\" to \"" << first "\". "
       "However, you configured data \"" << dataName << "\" for acceleration, which is exchanged from \"" << first << "\" to \"" << second << "\". "
-      "Please select an acceleration data which is exchanged from \"" << second << "\" to \"" << first << "\" using the <exchange> tag in your precice configuration.");
+      "Please remove this acceleration data tag or switch to a parallel implicit coupling scheme.");
   // clang-format on
 }
 
