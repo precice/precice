17dac7066009bffe261adfb52acff9ea438a413f
Kyle Davis <kyle.davis@ipvs.uni-stuttgart.de>
2021-01-18 15:05:16 +0100

    Run formating

--- src/acceleration/impl/ResidualSumPreconditioner.cpp
+++ src/acceleration/impl/ResidualSumPreconditioner.cpp
@@ -47,13 +47,12 @@ void ResidualSumPreconditioner::_update_(bool                   timestepComplete
     sum = std::sqrt(sum);
     //PRECICE_CHECK(not math::equals(sum, 0.0), "All residual sub-vectors in the residual-sum preconditioner are numerically zero ( sum = " << sum <<
     //                                          "). This indicates that the data values exchanged between two succesive iterations did not change."
-    //                                          " Your simulation probably got unstable, e.g. produces NAN values. Please check the data values exchanged" 
+    //                                          " Your simulation probably got unstable, e.g. produces NAN values. Please check the data values exchanged"
     //                                          " between the solvers.");
     if (math::equals(sum, 0.0)) {
-      PRECICE_WARN("All residual sub-vectors in the residual-sum preconditioner are numerically zero ( sum = " << sum <<
-                                              "). This indicates that the data values exchanged between two succesive iterations did not change."
-                                              " The simulation may be unstable, e.g. produces NAN values. Please check the data values exchanged" 
-                                              " between the solvers is not identical between iterations.");
+      PRECICE_WARN("All residual sub-vectors in the residual-sum preconditioner are numerically zero ( sum = " << sum << "). This indicates that the data values exchanged between two succesive iterations did not change."
+                                                                                                                         " The simulation may be unstable, e.g. produces NAN values. Please check the data values exchanged"
+                                                                                                                         " between the solvers is not identical between iterations.");
     }
 
     for (size_t k = 0; k < _subVectorSizes.size(); k++) {
@@ -63,19 +62,18 @@ void ResidualSumPreconditioner::_update_(bool                   timestepComplete
       //                                                      " if the coupling values of one solver is zero in the first iterations. "
       //                                                      " . Please try freezing the preconditioner weights or the value preconditioner instead.");
       if (math::equals(_residualSum[k], 0.0)) {
-        PRECICE_WARN("A sub-vector in the residual-sum preconditioner became numerically zero ( residualSum = " << _residualSum[k] <<
-                                                            ") . If this occured directly after the second iteration, check that the initial-relaxation"
-                                                            " factor is not equal to 1.0 if the coupling values of one solver"
-                                                            " is zero in the first iteration."
-                                                            " Please try freezing the preconditioner weights when using residual-sum"
-                                                            " or use the value preconditioner instead.");
+        PRECICE_WARN("A sub-vector in the residual-sum preconditioner became numerically zero ( residualSum = " << _residualSum[k] << ") . If this occured directly after the second iteration, check that the initial-relaxation"
+                                                                                                                                      " factor is not equal to 1.0 if the coupling values of one solver"
+                                                                                                                                      " is zero in the first iteration."
+                                                                                                                                      " Please try freezing the preconditioner weights when using residual-sum"
+                                                                                                                                      " or use the value preconditioner instead.");
       }
     }
 
     offset = 0;
     for (size_t k = 0; k < _subVectorSizes.size(); k++) {
-      if (!math::equals(_residualSum[k], 0.0)){
-      for (size_t i = 0; i < _subVectorSizes[k]; i++) {
+      if (!math::equals(_residualSum[k], 0.0)) {
+        for (size_t i = 0; i < _subVectorSizes[k]; i++) {
           _weights[i + offset]    = 1 / _residualSum[k];
           _invWeights[i + offset] = _residualSum[k];
         }
