24df997d460e3aaf613810a7f315148b36925e24
Kyle Davis <kyle.davis@ipvs.uni-stuttgart.de>
2021-01-18 17:30:48 +0100

    Updates wording of warnings

--- src/acceleration/impl/ResidualSumPreconditioner.cpp
+++ src/acceleration/impl/ResidualSumPreconditioner.cpp
@@ -46,30 +46,32 @@ void ResidualSumPreconditioner::_update_(bool                   timestepComplete
     }
     sum = std::sqrt(sum);
     if (math::equals(sum, 0.0)) {
-      PRECICE_WARN("All residual sub-vectors in the residual-sum preconditioner are numerically zero ( sum = " << sum << "). This indicates that the data values exchanged between two succesive iterations did not change."
-                                                                                                                         " The simulation may be unstable, e.g. produces NAN values. Please check the data values exchanged"
-                                                                                                                         " between the solvers is not identical between iterations.");
+      PRECICE_WARN("All residual sub-vectors in the residual-sum preconditioner are numerically zero ( sum = " << sum << 
+                    "). This indicates that the data values exchanged between two succesive iterations did not change."
+                    " The simulation may be unstable, e.g. produces NAN values. Please check the data values exchanged"
+                    " between the solvers is not identical between iterations. The preconditioner scaling factors were"
+                    " not updated in this iteration and the scaling factors determined in the previous iteration were used.");
     }
 
     for (size_t k = 0; k < _subVectorSizes.size(); k++) {
       _residualSum[k] += norms[k] / sum;
       if (math::equals(_residualSum[k], 0.0)) {
-        PRECICE_WARN("A sub-vector in the residual-sum preconditioner became numerically zero ( residualSum = " << _residualSum[k] << ") . If this occured directly after the second iteration, check that the initial-relaxation"
-                                                                                                                                      " factor is not equal to 1.0 if the coupling values of one solver"
-                                                                                                                                      " is zero in the first iteration."
-                                                                                                                                      " Please try freezing the preconditioner weights when using residual-sum"
-                                                                                                                                      " or use the value preconditioner instead.");
+        PRECICE_WARN("A sub-vector in the residual-sum preconditioner became numerically zero ( sub-vector = " << _residualSum[k] << 
+                    "). If this occured in the second iteration and the initial-relaxation factor is equal to 1.0,"
+                    " check if the coupling data values of one solver is zero in the first iteration."
+                    " The preconditioner scaling factors were not updated for this iteration and the scaling factors"
+                    " determined in the previous iteration were used.");
       }
     }
 
     offset = 0;
     for (size_t k = 0; k < _subVectorSizes.size(); k++) {
-      if (!math::equals(_residualSum[k], 0.0)) {
+      if (not math::equals(_residualSum[k], 0.0)) {
         for (size_t i = 0; i < _subVectorSizes[k]; i++) {
           _weights[i + offset]    = 1 / _residualSum[k];
           _invWeights[i + offset] = _residualSum[k];
         }
-        PRECICE_DEBUG("preconditioner weight[" << k << "] = " << 1 / _residualSum[k]);
+        PRECICE_DEBUG("preconditioner scaling factor[" << k << "] = " << 1 / _residualSum[k]);
       }
       offset += _subVectorSizes[k];
     }
