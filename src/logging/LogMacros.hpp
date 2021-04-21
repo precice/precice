f60523230e9edd4ff492893814b84b6c7fb8b03e
Frédéric Simonis <simonisfrederic@gmail.com>
2021-04-21 15:17:27 +0200

    Simplify tracer

--- src/logging/LogMacros.hpp
+++ src/logging/LogMacros.hpp
@@ -1,18 +1,13 @@
 #pragma once
 
-#include <boost/preprocessor/control/if.hpp>
-#include <boost/preprocessor/facilities/empty.hpp>
-#include <boost/preprocessor/seq/for_each_i.hpp>
-#include <boost/preprocessor/stringize.hpp>
-#include <boost/preprocessor/variadic/to_seq.hpp>
-#include <boost/vmd/is_empty.hpp>
-#include <string>
-
 #include "fmt/format.h"
 #include "fmt/ostream.h"
-#include "logging/Tracer.hpp"
-#include "prettyprint/prettyprint.hpp" // so that we can put std::vector et. al. on ostream
-#include "utils/String.hpp"
+
+#define PRECICE_LOG_LOCATION     \
+  precice::logging::LogLocation  \
+  {                              \
+    __FILE__, __LINE__, __func__ \
+  }
 
 #define PRECICE_WARN(...) _log.warning(PRECICE_LOG_LOCATION, fmt::format(__VA_ARGS__))
 
@@ -40,24 +35,14 @@
 
 #else // NDEBUG
 
-#define PRECICE_DEBUG(...) _log.debug(PRECICE_LOG_LOCATION, fmt::format(__VA_ARGS__))
+#include "logging/Tracer.hpp"
+#include "utils/ArgumentFormatter.hpp"
 
-/// Helper macro, used by TRACE
-#define PRECICE_LOG_ARGUMENT(r, data, i, elem) \
-  << '\n'                                      \
-  << "  Argument " << i << ": " << BOOST_PP_STRINGIZE(elem) << " == " << elem
+#define PRECICE_DEBUG(...) _log.debug(PRECICE_LOG_LOCATION, fmt::format(__VA_ARGS__))
 
 // Do not put do {...} while (false) here, it will destroy the _tracer_ right after creation
 #define PRECICE_TRACE(...)                                                                                                \
   precice::logging::Tracer _tracer_(_log, PRECICE_LOG_LOCATION);                                                          \
-  _log.trace(PRECICE_LOG_LOCATION, PRECICE_AS_STRING("Entering " << __func__ BOOST_PP_IF(BOOST_VMD_IS_EMPTY(__VA_ARGS__), \
-                                                                                         BOOST_PP_EMPTY(),                \
-                                                                                         BOOST_PP_SEQ_FOR_EACH_I(PRECICE_LOG_ARGUMENT, , BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)))));
+  _log.trace(PRECICE_LOG_LOCATION, "Entering " __func__ + PRECICE_LOG_ARGUMENTS(__VA_ARGS__)) 
 
 #endif // ! NDEBUG
-
-#define PRECICE_LOG_LOCATION     \
-  precice::logging::LogLocation  \
-  {                              \
-    __FILE__, __LINE__, __func__ \
-  }
