19dcdf781371e245d10ddc45053c3f7f3bd051a2
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-05-07 12:00:52 +0200

    Apply suggestions from code review
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@gmail.com>

--- src/time/Waveform.hpp
+++ src/time/Waveform.hpp
@@ -49,12 +49,12 @@ private:
       PRECICE_ASSERT(this->_timeWindows.cols() > 0);
       extrapolatedValue = this->_timeWindows.col(0);
     } else if ((order == 1) || (timeWindows < 3 && order > 1)) { //timesteps is increased before extrapolate is called
-      // PRECICE_INFO("Performing first order extrapolation");
+      PRECICE_DEBUG("Performing first order extrapolation");
       PRECICE_ASSERT(this->_timeWindows.cols() > 1);
       extrapolatedValue = this->_timeWindows.col(0) * 2.0; // = 2*x^t
       extrapolatedValue -= this->_timeWindows.col(1);      // = 2*x^t - x^(t-1)
     } else if (order == 2) {
-      // PRECICE_INFO("Performing second order extrapolation");
+      PRECICE_DEBUG("Performing second order extrapolation");
       PRECICE_ASSERT(this->_timeWindows.cols() > 2);
       extrapolatedValue = this->_timeWindows.col(0) * 2.5;  // = 2.5*x^t
       extrapolatedValue -= this->_timeWindows.col(1) * 2.0; // = 2.5*x^t - 2*x^(t-1)
