56d43994f5370646c7110d284a5892b8f5a5252d
BenjaminRodenberg <benjamin.rodenberg@in.tum.de>
2021-05-27 08:32:21 +0200

    fix format.

--- src/acceleration/MVQNAcceleration.cpp
+++ src/acceleration/MVQNAcceleration.cpp
@@ -122,8 +122,8 @@ void MVQNAcceleration::computeUnderrelaxationSecondaryData(
     values *= _initialRelaxation; // new * omg
     Eigen::VectorXd &secResiduals = _secondaryResiduals[id];
     secResiduals                  = data->previousIteration();
-    secResiduals *= 1.0 - _initialRelaxation;  // (1-omg) * old
-    values += secResiduals;                    // (1-omg) * old + new * omg
+    secResiduals *= 1.0 - _initialRelaxation; // (1-omg) * old
+    values += secResiduals;                   // (1-omg) * old + new * omg
   }
 }
 
