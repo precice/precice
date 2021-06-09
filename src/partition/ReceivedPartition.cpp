052977fa40a6ddbbb00b4ba9c2ec04b22b107cc6
atotoun <a.totounferoush@gmail.com>
2021-06-09 18:41:31 +0200

    Correct debug format

--- src/partition/ReceivedPartition.cpp
+++ src/partition/ReceivedPartition.cpp
@@ -550,7 +550,7 @@ void ReceivedPartition::createOwnerInformation()
 
     // #3: check vertices and keep only those that fit into the current rank's bb
     int numberOfVertices = _mesh->vertices().size();
-    PRECICE_DEBUG("Tag vertices, number of vertices " << numberOfVertices);
+    PRECICE_DEBUG("Tag vertices, number of vertices {}", numberOfVertices);
     std::vector<int> tags(numberOfVertices, -1);
     std::vector<int> globalIDs(numberOfVertices, -1);
     bool             atInterface = false;
