336b9540d79041644938dcc213075ebe88cfd1a4
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-07-21 17:03:58 +0200

    Add a types.hpp header in order to introduce named types

---
+++ src/utils/types.hpp
@@ -0,0 +1,38 @@
+#pragma once
+/**
+ * A namespace in which all relevant types used throughout the code base
+ * are decalred.
+ */
+namespace types {
+
+/**
+ * Type used for the IDs of vertices
+ */
+using vertexID = unsigned int;
+
+/**
+ * Type used for the IDs of edges
+ */
+using edgeID = unsigned int;
+
+/**
+ * Type used for the IDs of triangles
+ */
+using triangleID = unsigned int;
+
+/**
+ * Type used for the IDs of matching entities
+ */
+using matchID = unsigned int;
+
+/**
+ * Type used for the IDs of data
+ */
+using dataID = unsigned int;
+
+/**
+ * Type used for the IDs of meshes
+ */
+using meshID = int;
+
+} // namespace types
