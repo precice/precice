fe161bbb60a7ed6cbf3a82dfe8c5209e4efb04af
Frédéric Simonis <simonisfrederic@gmail.com>
2021-07-29 16:59:31 +0200

    Refactor projection match into a separate type

--- src/mapping/NearestProjectionMapping.cpp
+++ src/mapping/NearestProjectionMapping.cpp
@@ -107,9 +107,9 @@ void NearestProjectionMapping::computeMapping()
   for (const auto &fVertex : fVertices) {
     // Nearest projection element is edge for 2d if exists, if not, it is the nearest vertex
     // Nearest projection element is triangle for 3d if exists, if not the edge and at the worst case it is the nearest vertex
-    auto interpolation = indexTree.findNearestProjection(fVertex.getCoords(), nnearest);
-    _interpolations.push_back(std::move(interpolation.first));
-    distanceStatistics(interpolation.second);
+    auto match = indexTree.findNearestProjection(fVertex.getCoords(), nnearest);
+    _interpolations.push_back(std::move(match.polation));
+    distanceStatistics(match.distance);
   }
 
   if (distanceStatistics.empty()) {
