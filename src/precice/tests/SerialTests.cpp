6a84bb810415d6ac573e02e3558e458e61fcbd61
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-09-14 11:16:29 +0200

    Extend test to check for timeWindowSize != 1

--- src/precice/tests/SerialTests.cpp
+++ src/precice/tests/SerialTests.cpp
@@ -341,8 +341,8 @@ BOOST_AUTO_TEST_CASE(testExplicitReadWriteScalarDataWithSubcycling)
   double windowDt      = maxDt;
   int    timestep      = 0;
   int    timewindow    = 0;
-  double dt            = windowDt / (nSubsteps - 0.5); // Timestep length desired by solver. E.g. 4 steps with size 2/7. Fourth step will be restricted to 1/7 via preCICE steering to fit into the window.
-  double expectedDts[] = {2.0 / 7.0, 2.0 / 7.0, 2.0 / 7.0, 1.0 / 7.0};
+  double dt            = windowDt / (nSubsteps - 0.5); // Timestep length desired by solver. E.g. 4 steps with size 4/7. Fourth step will be restricted to 2/7 via preCICE steering to fit into the window.
+  double expectedDts[] = {4.0 / 7.0, 4.0 / 7.0, 4.0 / 7.0, 2.0 / 7.0};
   double currentDt     = dt; // Timestep length used by solver
   double time          = timestep * dt;
 
