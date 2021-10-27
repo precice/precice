c144bad1d8ddd44d67af52a7d985e282b85ec2c1
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-10-27 16:14:54 +0200

    Remove error.

--- src/testing/WaveformFixture.cpp
+++ src/testing/WaveformFixture.cpp
@@ -23,10 +23,5 @@ double WaveformFixture::getValue(time::Waveform &waveform, int dataID, int sampl
   return waveform._timeWindowsStorage(dataID, sampleID);
 }
 
-double WaveformFixture::getValue(time::Waveform &waveform, int dataID, int sampleID)
-{
-  return waveform._timeWindows(dataID, sampleID);
-}
-
 } // namespace testing
 } // namespace precice
