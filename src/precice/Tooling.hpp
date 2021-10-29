27bb5f113c2fff272286aa2016dec3480d0e33d2
Frédéric Simonis <simonisfrederic@gmail.com>
2021-10-29 16:38:37 +0200

    Add tooling to docs

--- src/precice/Tooling.hpp
+++ src/precice/Tooling.hpp
@@ -5,15 +5,33 @@
 
 namespace precice {
 
+/** @brief Contains the preCICE tooling API
+ *
+ * The contained methods allow to query internal information of preCICE
+ * without having to create a \ref precice::SolverInterface.
+ *
+ * @note These functions are not exposed via the bindings
+ *
+ * @see \ref precice::getVersionInformation which is exposed via the bindings
+ */
 namespace tooling {
 
-/// @brief Prints the configuration reference as Markdown.
+/** @brief Prints the configuration reference as Markdown.
+ *
+ * @param[inout] out the stream to write the result to
+ */
 void printConfigAsMD(std::ostream &out);
 
-/// @brief Prints the configuration reference as DTD.
+/** @brief Prints the configuration reference as DTD.
+ *
+ * @param[inout] out the stream to write the result to
+ */
 void printConfigAsDTD(std::ostream &out);
 
-/// @brief Prints the configuration reference as XML with inlined help.
+/** @brief Prints the configuration reference as XML with inlined help.
+ *
+ * @param[inout] out the stream to write the result to
+ */
 void printConfigAsXML(std::ostream &out);
 
 } // namespace tooling
