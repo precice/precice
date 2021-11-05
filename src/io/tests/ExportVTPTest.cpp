ac310b8a5fb68e2fb775a0ba1b81ab20fd483381
Kursat_Yurt <kursatyurt@gmail.com>
2021-11-05 04:19:23 +0100

    Add test for VTP exporting

---
+++ src/io/tests/ExportVTPTest.cpp
@@ -0,0 +1,67 @@
+#include <Eigen/Core>
+#include <algorithm>
+#include <string>
+#include "io/Export.hpp"
+#include "io/ExportVTP.hpp"
+#include "mesh/Mesh.hpp"
+#include "testing/TestContext.hpp"
+#include "testing/Testing.hpp"
+
+namespace precice {
+namespace mesh {
+class Edge;
+class Vertex;
+} // namespace mesh
+} // namespace precice
+
+BOOST_AUTO_TEST_SUITE(IOTests)
+
+BOOST_AUTO_TEST_SUITE(VTPExport)
+
+using namespace precice;
+
+BOOST_AUTO_TEST_CASE(ExportPolygonalMesh)
+{
+  PRECICE_TEST(1_rank);
+  int             dim = 2;
+  mesh::Mesh      mesh("MyMesh", dim, testing::nextMeshID());
+  mesh::Vertex &  v1      = mesh.createVertex(Eigen::VectorXd::Constant(dim, 0.0));
+  mesh::Vertex &  v2      = mesh.createVertex(Eigen::VectorXd::Constant(dim, 1.0));
+  Eigen::VectorXd coords3 = Eigen::VectorXd::Constant(dim, 0.0);
+  coords3(0)              = 1.0;
+  mesh::Vertex &v3        = mesh.createVertex(coords3);
+
+  mesh.createEdge(v1, v2);
+  mesh.createEdge(v2, v3);
+  mesh.createEdge(v3, v1);
+
+  io::ExportVTP exportVTP;
+  std::string   filename = "io-VTPExport-ExportPolygonalMesh";
+  std::string   location = "";
+  exportVTP.doExport(filename, location, mesh);
+}
+
+BOOST_AUTO_TEST_CASE(ExportTriangulatedMesh)
+{
+  PRECICE_TEST(1_rank);
+  int             dim = 3;
+  mesh::Mesh      mesh("MyMesh", dim, testing::nextMeshID());
+  mesh::Vertex &  v1      = mesh.createVertex(Eigen::VectorXd::Constant(dim, 0.0));
+  mesh::Vertex &  v2      = mesh.createVertex(Eigen::VectorXd::Constant(dim, 1.0));
+  Eigen::VectorXd coords3 = Eigen::VectorXd::Zero(dim);
+  coords3(0)              = 1.0;
+  mesh::Vertex &v3        = mesh.createVertex(coords3);
+
+  mesh::Edge &e1 = mesh.createEdge(v1, v2);
+  mesh::Edge &e2 = mesh.createEdge(v2, v3);
+  mesh::Edge &e3 = mesh.createEdge(v3, v1);
+  mesh.createTriangle(e1, e2, e3);
+
+  io::ExportVTP exportVTP;
+  std::string   filename = "io-VTPExport-ExportTriangulatedMesh";
+  std::string   location = "";
+  exportVTP.doExport(filename, location, mesh);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // ExportVTP
+BOOST_AUTO_TEST_SUITE_END() // IOTests
