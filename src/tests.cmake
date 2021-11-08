5df66be2b94e7c777e58b5e99544c34b2140627f
Frédéric Simonis <simonisfrederic@gmail.com>
2021-11-08 14:14:27 +0100

    Add tests for Tooling header

--- src/tests.cmake
+++ src/tests.cmake
@@ -56,6 +56,7 @@ target_sources(testprecice
     src/precice/tests/DataContextTest.cpp
     src/precice/tests/ParallelTests.cpp
     src/precice/tests/SerialTests.cpp
+    src/precice/tests/ToolingTests.cpp
     src/precice/tests/VersioningTests.cpp
     src/precice/tests/WatchIntegralTest.cpp
     src/precice/tests/WatchPointTest.cpp
