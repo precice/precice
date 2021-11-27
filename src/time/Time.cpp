2ebf50be4d5e3b45e0ac1fcb786e1b997cc5c13b
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-11-28 00:23:19 +0100

    Remove unneeded.

--- src/time/Time.cpp
+++ src/time/Time.cpp
@@ -3,8 +3,6 @@
 namespace precice {
 namespace time {
 
-const int Time::UNDEFINED_EXTRAPOLATION_ORDER = -1;
-
 const int Time::UNDEFINED_INTERPOLATION_ORDER = -1;
 
 } // namespace time
--- src/time/Time.hpp
+++ src/time/Time.hpp
@@ -5,9 +5,6 @@ namespace time {
 
 class Time {
 public:
-  /// To be used, when the extrapolation order is not defined.
-  static const int UNDEFINED_EXTRAPOLATION_ORDER;
-
   /// To be used, when the interpolation order is not defined.
   static const int UNDEFINED_INTERPOLATION_ORDER;
 };
