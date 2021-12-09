f521ca5cca0a858fc4bc800bf783fb72ec37d3b3
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-12-09 09:13:19 +0100

    Remove changes related to MultiCouplingFourSolvers.

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -3,8 +3,4 @@
 #
 target_sources(testprecice
     PRIVATE
-    tests/serial/multiCouplingFourSolvers/MultiCouplingFourSolvers1.cpp
-    tests/serial/multiCouplingFourSolvers/MultiCouplingFourSolvers2.cpp
-    tests/serial/multiCouplingFourSolvers/helpers.cpp
-    tests/serial/multiCouplingFourSolvers/helpers.hpp
     )
