31f4b9331c44a2a84a86716b29c6f2d100a20420
Frédéric Simonis <simonisfrederic@gmail.com>
2021-12-17 11:57:41 +0100

    Reduce map access and clarify

--- src/mesh/Filter.cpp
+++ src/mesh/Filter.cpp
@@ -15,25 +15,27 @@ void addVertexToMesh(const Vertex &vertex, boost::container::flat_map<VertexID, 
 
 void addEdgeToMesh(const Edge &edge, boost::container::flat_map<EdgeID, Edge *> &edgeMap, boost::container::flat_map<VertexID, Vertex *> &vertexMap, Mesh &destination, bool withConnection)
 {
-  int vertexIndex1 = edge.vertex(0).getID();
-  int vertexIndex2 = edge.vertex(1).getID();
+  const int  vertexIndex1  = edge.vertex(0).getID();
+  const int  vertexIndex2  = edge.vertex(1).getID();
+  const bool vertexMarked1 = vertexMap.count(vertexIndex1) == 1;
+  const bool vertexMarked2 = vertexMap.count(vertexIndex2) == 1;
+  const bool allMarked     = vertexMarked1 && vertexMarked2;
+  const bool noneMarked    = !(vertexMarked1 || vertexMarked2);
 
-  if (vertexMap.count(vertexIndex1) == 0 and
-      vertexMap.count(vertexIndex2) == 0) {
+  if (noneMarked) {
     return;
   }
 
   if (withConnection) {
-    if (vertexMap.count(vertexIndex1) == 0) {
+    if (!vertexMarked1) {
       addVertexToMesh(edge.vertex(0), vertexMap, destination);
     }
-    if (vertexMap.count(vertexIndex2) == 0) {
+    if (!vertexMarked2) {
       addVertexToMesh(edge.vertex(1), vertexMap, destination);
     }
   }
 
-  if (vertexMap.count(vertexIndex1) == 1 and
-      vertexMap.count(vertexIndex2) == 1) {
+  if (allMarked || withConnection) {
     Edge &e               = destination.createEdge(*vertexMap[vertexIndex1], *vertexMap[vertexIndex2]);
     edgeMap[edge.getID()] = &e;
   }
@@ -41,31 +43,33 @@ void addEdgeToMesh(const Edge &edge, boost::container::flat_map<EdgeID, Edge *> 
 
 void addTriangleToMesh(const Triangle &triangle, boost::container::flat_map<EdgeID, Edge *> &edgeMap, boost::container::flat_map<VertexID, Vertex *> &vertexMap, Mesh &destination, bool withConnection)
 {
-  int edgeIndex1 = triangle.edge(0).getID();
-  int edgeIndex2 = triangle.edge(1).getID();
-  int edgeIndex3 = triangle.edge(2).getID();
+  const int  edgeIndex1  = triangle.edge(0).getID();
+  const int  edgeIndex2  = triangle.edge(1).getID();
+  const int  edgeIndex3  = triangle.edge(2).getID();
+  const bool edgeMarked1 = edgeMap.count(edgeIndex1) == 1;
+  const bool edgeMarked2 = edgeMap.count(edgeIndex2) == 1;
+  const bool edgeMarked3 = edgeMap.count(edgeIndex3) == 1;
+  const bool allMarked   = edgeMarked1 && edgeMarked2 && edgeMarked3;
+  const bool noneMarked  = !(edgeMarked1 || edgeMarked2 || edgeMarked3);
 
-  if (edgeMap.count(edgeIndex1) == 0 and
-      edgeMap.count(edgeIndex2) == 0 and
-      edgeMap.count(edgeIndex3) == 0) {
+  if (noneMarked) {
     return;
   }
 
   if (withConnection) {
-    if (edgeMap.count(edgeIndex1) == 0) {
+    if (!edgeMarked1) {
       addEdgeToMesh(triangle.edge(0), edgeMap, vertexMap, destination, withConnection);
     }
-    if (edgeMap.count(edgeIndex2) == 0) {
+    if (!edgeMarked2) {
       addEdgeToMesh(triangle.edge(1), edgeMap, vertexMap, destination, withConnection);
     }
-    if (edgeMap.count(edgeIndex3) == 0) {
+    if (!edgeMarked3) {
       addEdgeToMesh(triangle.edge(2), edgeMap, vertexMap, destination, withConnection);
     }
   }
-  if (edgeMap.count(edgeIndex1) == 1 and
-      edgeMap.count(edgeIndex2) == 1 and
-      edgeMap.count(edgeIndex3) == 1) {
-    Triangle &t = destination.createTriangle(*edgeMap[edgeIndex1], *edgeMap[edgeIndex2], *edgeMap[edgeIndex3]);
+
+  if (allMarked || withConnection) {
+    destination.createTriangle(*edgeMap[edgeIndex1], *edgeMap[edgeIndex2], *edgeMap[edgeIndex3]);
   }
 }
 
