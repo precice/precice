6335db3c07fdc5b518b4f9885cd246151250774d
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-12-27 13:44:55 +0100

    Add missing file.

---
+++ src/testing/DataContextFixture.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "precice/impl/DataContext.hpp"
+
+namespace precice {
+namespace testing {
+/*
+ * @brief A fixture that is used to access private functions of the DataContext class.
+ *
+ * The fixture can be used to call private functions for individual testing. 
+ */
+class DataContextFixture {
+public:
+bool hasReadMapping(precice::impl::DataContext &dataContext);
+
+bool hasWriteMapping(precice::impl::DataContext &dataContext);
+
+void resetProvidedData(precice::impl::DataContext &dataContext);
+
+};
+
+} // namespace testing
+} // namespace precice
