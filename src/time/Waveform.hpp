5af18c2a1c54efe56cdd8c947dcf3f611bcfeddf
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-12-29 11:45:42 +0100

    Extend documentation.

--- src/time/Waveform.hpp
+++ src/time/Waveform.hpp
@@ -12,6 +12,9 @@ class WaveformFixture;
 
 namespace time {
 
+/**
+ * @brief Stores data samples in time and allows to perform interpolation to obtain new values.
+ */
 class Waveform {
   friend class testing::WaveformFixture; // Make the fixture friend of this class
 public:
@@ -20,36 +23,37 @@ public:
    *
    * Storage still needs to be initialized with Waveform::initialize, before the Waveform can be used.
    *
-   * @param interpolationOrder defines the maximum interpolation order supported by this Waveform and reserves storage correspondingly
+   * @param interpolationOrder Defines the maximum interpolation order supported by this Waveform and reserves storage correspondingly
    */
   Waveform(const int interpolationOrder);
 
   /**
    * @brief Used to initialize _timeWindowsStorage according to required size.
-   * @param valuesSize defines how many values one sample in time consists of
+   * @param valuesSize Defines how many values one sample in time consists of
    */
   void initialize(const int valuesSize);
 
   /**
-   * @brief Updates first entry in _timeWindows with given values
-   * @param values new sample for this time window
+   * @brief Updates first entry in _timeWindows with given values.
+   * @param values Sample for this time window
    */
   void storeAtFirstSample(const Eigen::VectorXd &values);
 
   /**
-   * @brief Updates all entries in _timeWindowsStorage with given values
-   * @param values new sample for this time window
+   * @brief Updates all entries in _timeWindowsStorage with given values. Often used to initialize a new Waveform with given data.
+   * @param values Sample used for all time window
    */
   void storeAtAllSamples(const Eigen::VectorXd values);
 
   /**
-   * @brief Called, when moving to the next time window. All entries in _timeWindows are shifted. The new entry is initialized as the value from the last window (= constant extrapolation)
+   * @brief Shifts all entries in _timeWindows. The new entry is initialized as the value from the last window (= constant extrapolation). Usually called, when moving to the next time window.
    */
   void moveToNextWindow();
 
   /**
-   * @brief sample Waveform. Uses interpolation with Waveform's interpolation order, if necessary.
-   * @param normalizedDt time where the sampling inside the window happens. Only allows values between 0 and 1. 0 refers to the beginning of the window and 1 to the end.
+   * @brief Evaluate Waveform at specific point in time. Uses interpolation with Waveform's interpolation order, if necessary.
+   * @param normalizedDt Time where the sampling inside the window happens. Only allows values between 0 and 1. 0 refers to the beginning of the window and 1 to the end.
+   * @return Value of Waveform at time normalizedDt.
    */
   Eigen::VectorXd sample(const double normalizedDt);
 
@@ -69,26 +73,28 @@ private:
   mutable logging::Logger _log{"time::Waveform"};
 
   /**
-   * @brief returns number of values per sample in time stored by this waveform
+   * @brief Get number of values per sample in time stored by this waveform.
+   * @return Number of values per sample.
    */
   int valuesSize();
 
   /**
-   * @brief returns maximum number of samples in time this waveform can store
+   * @brief Get maximum number of samples in time this waveform can store.
+   * @return Maximum number of samples.
    */
   int maxNumberOfStoredSamples();
 
   /**
-   * @brief Updates entry in _timeWindowsStorage corresponding to a given sampleIndex with given values
-   * @param values new sample for this time window
-   * @param sampleIndex index of sample to be updated
+   * @brief Updates entry in _timeWindowsStorage corresponding to a given sampleIndex with given values.
+   * @param values Input sample.
+   * @param sampleIndex Index of sample to be updated to given input sample.
    */
   void storeAt(const Eigen::VectorXd values, int sampleIndex);
 
   /**
    * @brief Interpolates values inside current time window using _timeWindowsStorage and an interpolation scheme of the order of this Waveform.
-   *
    * @param normalizedDt time where the sampling inside the window happens. 0 refers to the beginning of the window and 1 to the end.
+   * @return Interpolated value at time normalizedDt.
    */
   Eigen::VectorXd interpolate(const double normalizedDt);
 };
