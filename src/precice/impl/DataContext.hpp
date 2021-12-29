aa8702179568567090f26d082c80397d26688ac7
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-12-29 12:11:39 +0100

    Extend documentation.

--- src/precice/impl/DataContext.hpp
+++ src/precice/impl/DataContext.hpp
@@ -24,54 +24,141 @@ namespace impl {
 class DataContext {
   friend class testing::DataContextFixture; // Make the fixture friend of this class
 public:
+  /**
+   * @brief Get _providedData member.
+   * 
+   * @return mesh::PtrData _providedData.
+   */
   mesh::PtrData providedData();
 
+  /**
+   * @brief Get _toData member.
+   * 
+   * @return mesh::PtrData _toData.
+   */
   mesh::PtrData toData();
 
+  /**
+   * @brief Get the Name of _providedData.
+   * 
+   * @return std::string Name of _providedData.
+   */
   std::string getDataName() const;
 
+  /**
+   * @brief Get the ID of _providedData.
+   * 
+   * @return int ID of _providedData.
+   */
   int getProvidedDataID() const;
 
+  /**
+   * @brief Get the ID of _fromData. Used for performing the mapping outside of this class.
+   * 
+   * @return int ID of _fromData.
+   */
   int getFromDataID() const;
 
+  /**
+   * @brief Purpose unclear. See also https://github.com/precice/precice/issues/1156.
+   */
   void resetProvidedData();
 
+  /**
+   * @brief Resets _toData to zero. Used before mapping is performed.
+   */
   void resetToData();
 
+  /**
+   * @brief Get the ID of _toData. Used for performing the mapping outside of this class.
+   * 
+   * @return int ID of _toData.
+   */
   int getToDataID() const;
 
+  /**
+   * @brief Get the dimensions of _providedData.
+   * 
+   * @return int Dimensions of _providedData.
+   */
   int getDataDimensions() const;
 
+  /**
+   * @brief Get the name of _mesh.
+   * 
+   * @return std::string Name of _mesh.
+   */
   std::string getMeshName() const;
 
+  /**
+   * @brief Get the ID of _mesh.
+   * 
+   * @return int ID of _mesh.
+   */
   int getMeshID() const;
-
+  
+  /**
+   * @brief Informs the user whether this DataContext has a _mappingContext.
+   * 
+   * @return True, if this DataContext is associated with a mapping. False, if not. 
+   */
   bool hasMapping() const;
-
+  
+  /**
+   * @brief Get the _mappingContext associated with this DataContext.
+   * 
+   * @return const MappingContext The _mappingContext of this DataContext.
+   */
   const MappingContext mappingContext() const;
 
+  /**
+   * @brief Informs the user whether this DataContext has a read mapping.
+   * 
+   * @return True, if DataContext has a read mapping.
+   */
   bool hasReadMapping() const;
 
+  /**
+   * @brief Informs the user whether this DataContext has a write mapping.
+   * 
+   * @return True, if DataContext has a write mapping.
+   */
   bool hasWriteMapping() const;
 
 protected:
+  /**
+   * @brief Construct a new DataContext without a mapping. Protected, because only ReadDataContext and WriteDataContext should be used.
+   * 
+   * @param data Data associated with this DataContext.
+   * @param mesh Mesh associated with this DataContext.
+   */
   DataContext(mesh::PtrData data, mesh::PtrMesh mesh);
 
-  // data this participant will write to and read from
-  mesh::PtrData _providedData;
-
-  mesh::PtrData _fromData;
-
-  mesh::PtrData _toData;
-
+  /// Defines the mapping associated to this DataContext. A DataContext may also exist without a mapping.
   MappingContext _mappingContext;
 
-  /// helper function for creating read and write mappings
+  /// Data this participant will write to and read from
+  mesh::PtrData _providedData;
+  
+  /// If a mapping exists, mesh::PtrData the mapping maps from.
+  mesh::PtrData _fromData;
+
+  /// If a mapping exists, mesh::PtrData the mapping maps from.
+  mesh::PtrData _toData;
+
+  /**
+   * @brief Helper to set _mappingContext, _fromData and _toData.
+   * 
+   * @param mappingContext MappingContext this DataContext will be associated to.
+   * @param fromData Data the mapping maps from.
+   * @param toData Data the mapping maps to.
+   */
   void setMapping(MappingContext mappingContext, mesh::PtrData fromData, mesh::PtrData toData);
 
 private:
   mutable logging::Logger _log{"impl::DataContext"};
-
+  
+  /// Mesh associated with _providedData.
   mesh::PtrMesh _mesh;
 };
 
--- src/precice/impl/ReadDataContext.hpp
+++ src/precice/impl/ReadDataContext.hpp
@@ -9,21 +9,29 @@ namespace precice {
 namespace impl {
 
 /**
- * @brief Stores one Data object with related mesh.
+ * @brief Stores one Data object with related mesh. Context is used to be read from and potentially provides a read mapping.
  *
  * Derived from DataContext
  */
 class ReadDataContext : public DataContext {
 public:
+  /**
+   * @brief Construct a new ReadDataContext object without a mapping.
+   * 
+   * @param data Data associated with this ReadDataContext.
+   * @param mesh Mesh associated with this ReadDataContext.
+   */
   ReadDataContext(
       mesh::PtrData data,
       mesh::PtrMesh mesh);
 
   /**
-   * @brief links a read mapping and the mesh context the read mapping requires to this data context
+   * @brief Links a MappingContext for a read mapping and the MeshContext the read mapping requires to this DataContext.
+   * 
+   * A read mapping maps _fromData to _providedData. A ReadDataContext already has _providedData, but additionally requires _fromData.
    *
-   * @param[in] mappingContext provides context of read mapping
-   * @param[in] fromMeshContext provides context of mesh this read mapping is mapping to (_toData)
+   * @param[in] mappingContext Context of read mapping
+   * @param[in] fromMeshContext Context of mesh this read mapping is mapping from (_fromData)
    */
   void configureForReadMapping(MappingContext mappingContext, MeshContext fromMeshContext);
 
