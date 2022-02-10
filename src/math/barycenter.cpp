cfba5f15a2ff1066b7d8ba8c638d298f789f108d
Nicolas\ Geldner <ngeldner3@gmail.com>
2022-02-10 17:07:12 +0100

    Refactor: Simplify calculation of barycentric coordinates for a point and a triangle according to an algorithm by Heidrich (2005)

--- src/math/barycenter.cpp
+++ src/math/barycenter.cpp
@@ -45,64 +45,33 @@ Eigen::Vector3d calcBarycentricCoordsForTriangle(
     const Eigen::VectorXd &a,
     const Eigen::VectorXd &b,
     const Eigen::VectorXd &c,
-    const Eigen::VectorXd &normal,
     const Eigen::VectorXd &location)
 {
-  using Eigen::Vector2d;
   using Eigen::Vector3d;
 
   const int dimensions = a.size();
   PRECICE_ASSERT(dimensions == 3, dimensions);
   PRECICE_ASSERT(dimensions == b.size(), "A and B need to have the same dimensions.", dimensions, b.size());
   PRECICE_ASSERT(dimensions == c.size(), "A and C need to have the same dimensions.", dimensions, c.size());
-  PRECICE_ASSERT(dimensions == normal.size(), "A and the normal need to have the same dimensions.", dimensions, normal.size());
   PRECICE_ASSERT(dimensions == location.size(), "A and the location need to have the same dimensions.", dimensions, location.size());
 
-  // Parametric representation for triangle plane:
-  // (x, y, z) * normal = d
-  const double d = normal.dot(a);
+  Vector3d ab, ac, ap, n, barycentricCoords;
+  double scaleFactor;
 
-  // Parametric description of line from searchpoint orthogonal to triangle:
-  // location + t * normal = x     (where t is parameter)
-  // Determine t such that x lies on triangle plane:
-  const double t = d - location.dot(normal) / normal.dot(normal);
+  // constant per triangle
+  ab = b - a;
+  ac = c - a;
+  n = ab.cross(ac);
+  scaleFactor = 1.0 / n.dot(n);
 
-  // Compute projected point with parameter t:
-  Vector3d projected = normal;
-  projected *= t;
-  projected += location;
+  // varying per point
+  ap = location - a;
 
-  // Project everything to 2D
-  int iMax;
-  normal.cwiseAbs().maxCoeff(&iMax);
-  int indices[2];
-  if (iMax == 0) {
-    indices[0] = 1;
-    indices[1] = 2;
-  } else if (iMax == 1) {
-    indices[0] = 0;
-    indices[1] = 2;
-  } else {
-    PRECICE_ASSERT(iMax == 2, iMax);
-    indices[0] = 0;
-    indices[1] = 1;
-  }
-  Vector2d a2D(a[indices[0]],
-               a[indices[1]]);
-  Vector2d b2D(b[indices[0]],
-               b[indices[1]]);
-  Vector2d c2D(c[indices[0]],
-               c[indices[1]]);
-  Vector2d projected2D(projected[indices[0]], projected[indices[1]]);
-  // Compute barycentric coordinates by solving linear 3x3 system
-  Vector3d                    rhs(projected2D(0), projected2D(1), 1);
-  Eigen::Matrix<double, 3, 3> A;
-  A << a2D(0), b2D(0), c2D(0),
-      a2D(1), b2D(1), c2D(1),
-      1, 1, 1;
-  Vector3d barycentricCoords = A.colPivHouseholderQr().solve(rhs);
+  barycentricCoords(2) = n.dot(ab.cross(ap)) * scaleFactor;
+  barycentricCoords(1) = n.dot(ap.cross(ac)) * scaleFactor;
+  barycentricCoords(0) = 1 - barycentricCoords(1) - barycentricCoords(2);
 
-  return {barycentricCoords, projected};
+  return barycentricCoords;
 }
 
 } // namespace barycenter
