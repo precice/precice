4a6ba4f5c7a4352e56f099a3a5b700767edcae5f
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-03-08 18:22:27 +0100

    Use docopt for documentation and improve usability.

--- tools/community/list_collabs.py
+++ tools/community/list_collabs.py
@@ -1,57 +1,77 @@
-import argparse
+"""
+Usage: list_collabs.py [--user USER] [--token TOKEN]
+
+Create a list of all collaborators that commited to an organization. Provide user name and access token, if you need to
+send many requests.
+
+  -h --help         show this
+  -u --user USER    github user name
+  -t --token TOKEN  github token (see https://docs.github.com/en/rest/overview/other-authentication-methods#basic-authentication)
+"""
+from docopt import docopt
 import requests
 from requests.auth import HTTPBasicAuth
 
-# use authentication, if you want to get a lot of results. Github limits output for unauthenticated users. See https://docs.github.com/en/rest/overview/other-authentication-methods#basic-authentication
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--user', type=str)
-parser.add_argument('-t', '--token', type=str)
-args = parser.parse_args()
+if __name__ == '__main__':
+    organization = "precice"
+    arguments = docopt(__doc__)
 
-organization = "precice"
+    commit_authors = set()
+    skipped_commits = []
+    number_commits = 0
 
-commit_authors = set()
-skipped_commits = []
-number_commits = 0
-
-# Collect all repos under organization
-repos = set()
-response = requests.get("https://api.github.com/users/{}/repos?per_page=100".format(organization))
-
-for repo in response.json():
-    repos.add(repo["name"])
-
-print("Parsing {} repos: {}".format(len(repos), repos))
-
-# Traverse all repos
-for repo in repos:
-    print("Parsing {}...".format(repo))
-    pageno = 1
-    api_url = "https://api.github.com/repos/{}/{}/commits?per_page=100&page={}".format(organization, repo, pageno)
-    
-    # Use authentication, if provided
-    if args.user:
-        response = requests.get(api_url, auth=HTTPBasicAuth(args.user,args.token))
+    # Collect all repos under organization
+    repos = set()
+    api_url = "https://api.github.com/users/{}/repos?per_page=100".format(organization)
+    if '--user' in arguments.keys():
+        response = requests.get(api_url, auth=HTTPBasicAuth(arguments['--user'], arguments['--token']))
     else:
         response = requests.get(api_url)
-    
-    # While page has entries, continue
-    while len(response.json()) > 0:
-        print("On page {} got {} hits.".format(pageno, len(response.json())))
-        pageno += 1
-        api_url = "https://api.github.com/repos/{}/{}/commits?per_page=100&page={}".format(organization, repo, pageno)
-        response = requests.get(api_url, auth=HTTPBasicAuth(args.user,args.token))
-        # Traverse all commits on page
-        for commit in response.json():
-            number_commits += 1
-            try:
-                # extract name of author
-                author = commit['author']['login']
-                commit_authors.add(author)
-            except TypeError:
-                # need to skip many commits. Not sure why.
-                skipped_commits.append(commit)
 
-print("Parsed {} commits in total.".format(number_commits))
-print("Found {} unique authors: {}".format(len(commit_authors), commit_authors))
-print("Skipped {} commits due to parsing errors".format(len(skipped_commits)))
+    try:
+        if 'message' in response.json().keys():
+            print(response.json()['message'])
+            quit()
+    except AttributeError:
+        pass
+
+    for repo in response.json():
+        repos.add(repo["name"])
+
+    print("Parsing {} repos: {}".format(len(repos), repos))
+
+    # Traverse all repos
+    for repo in repos:
+        print("Parsing {}...".format(repo))
+        pageno = 1
+        api_url = "https://api.github.com/repos/{}/{}/commits?per_page=100&page={}".format(organization, repo, pageno)
+
+        # Use authentication, if provided
+        if '--user' in arguments.keys():
+            response = requests.get(api_url, auth=HTTPBasicAuth(arguments['--user'], arguments['--token']))
+        else:
+            response = requests.get(api_url)
+
+        # While page has entries, continue
+        while len(response.json()) > 0:
+            print("On page {} got {} hits.".format(pageno, len(response.json())))
+            pageno += 1
+            api_url = "https://api.github.com/repos/{}/{}/commits?per_page=100&page={}".format(organization, repo, pageno)
+            if '--user' in arguments.keys():
+                response = requests.get(api_url, auth=HTTPBasicAuth(arguments['--user'], arguments['--token']))
+            else:
+                response = requests.get(api_url)
+            # Traverse all commits on page
+            for commit in response.json():
+                number_commits += 1
+                try:
+                    # extract name of author
+                    author = commit['author']['login']
+                    commit_authors.add(author)
+                except TypeError:
+                    # need to skip many commits. Not sure why.
+                    skipped_commits.append(commit)
+
+    print("Parsed {} commits in total.".format(number_commits))
+    print("Found {} unique authors: {}".format(len(commit_authors), commit_authors))
+    print("Skipped {} commits due to parsing errors".format(len(skipped_commits)))
