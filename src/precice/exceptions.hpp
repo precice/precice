3d4f464da2d1cf3b794c0ca5255d1c90ebb8dcf1
Frédéric Simonis <simonisfrederic@gmail.com>
2022-03-10 18:16:35 +0100

    Add module specific exception classes

--- src/precice/exceptions.hpp
+++ src/precice/exceptions.hpp
@@ -10,4 +10,46 @@ public:
       : std::runtime_error(what_arg){};
 };
 
+class ActionError : public precice::Error {
+public:
+  ActionError(const std::string &what_arg)
+      : precice::Error(what_arg){};
+};
+
+class AccelerationError : public precice::Error {
+public:
+  AccelerationError(const std::string &what_arg)
+      : precice::Error(what_arg){};
+};
+
+class CouplingSchemeError : public precice::Error {
+public:
+  CouplingSchemeError(const std::string &what_arg)
+      : precice::Error(what_arg){};
+};
+
+class ExportError : public precice::Error {
+public:
+  ExportError(const std::string &what_arg)
+      : precice::Error(what_arg){};
+};
+
+class MappingError : public precice::Error {
+public:
+  MappingError(const std::string &what_arg)
+      : precice::Error(what_arg){};
+};
+
+class InterfaceError : public precice::Error {
+public:
+  InterfaceError(const std::string &what_arg)
+      : precice::Error(what_arg){};
+};
+
+class ConfigurationError : public precice::Error {
+public:
+  ConfigurationError(const std::string &what_arg)
+      : precice::Error(what_arg){};
+};
+
 } // namespace precice
