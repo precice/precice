dc5bfa0f1f96c534f117cc593e95f018ee26c3c8
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-04-27 17:46:08 +0200

    Fix wrong `DataContext` for multiple mappings (#1267)
    * Rename current multiple mappings tests
    
    * Refactor data context logic and allow multiple read to and write from mappings
    
    * Add changelog entry
    
    * Add failing MultipleWriteFrom mappings test
    
    * Fix comment wording
    
    * Apply suggestions from code review
    
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@gmail.com>
    
    * Extend DataContext to handle multiple mappings
    
    * Extend unit tests
    
    * Add another integration test
    
    * Fix formatting
    
    * Rename addMapping to appendMapping
    
    * Split the extension of the unit test into a seperate test
    
    * Apply suggestions from code review
    
    Co-authored-by: Frédéric Simonis <simonisfrederic@gmail.com>
    
    * Add safeguard against registering a mapping twice
    
    * Rename mappingContext to mappingContexts
    
    * Rename mappingContexts in tests
    
    * Rename fromData and toData to plural
    
    * Update documentation
    
    * Update error message wording
    
    * Apply suggestions from code review
    
    Co-authored-by: Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
    
    * CHange Datas back to singular (data)
    
    * Rename addMapping to appendMapping
    
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@gmail.com>
    Co-authored-by: Frédéric Simonis <simonisfrederic@gmail.com>
    Co-authored-by: Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>

--- src/precice/impl/WriteDataContext.cpp
+++ src/precice/impl/WriteDataContext.cpp
@@ -18,12 +18,12 @@ mesh::PtrData WriteDataContext::providedData()
   return _providedData;
 }
 
-void WriteDataContext::configureMapping(const MappingContext &mappingContext, const MeshContext &meshContext)
+void WriteDataContext::appendMappingConfiguration(const MappingContext &mappingContext, const MeshContext &meshContext)
 {
   PRECICE_ASSERT(meshContext.mesh->hasDataName(getDataName()));
   mesh::PtrData data = meshContext.mesh->data(getDataName());
   PRECICE_ASSERT(data != _providedData, "Data the write mapping is mapping to needs to be different from _providedData");
-  setMapping(mappingContext, _providedData, data);
+  appendMapping(mappingContext, _providedData, data);
   PRECICE_ASSERT(hasWriteMapping());
 }
 
