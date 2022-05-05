88d46b03833ecef40ba8abe8765f65f9c4f5207b
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-05-05 13:40:58 +0200

    Extend comment a bit

--- tests/serial/mapping-rbf-gaussian/helpers.cpp
+++ tests/serial/mapping-rbf-gaussian/helpers.cpp
@@ -94,7 +94,8 @@ void testRBFMapping(const std::string configFile, const TestContext &context)
     interface.readScalarData(dataAID, idB, valueB);
     interface.readScalarData(dataAID, idC, valueC);
 
-    BOOST_TEST(valueA == expectedValTwoA, boost::test_tools::tolerance(1e-8)); // Due to Eigen 3.3.7
+    // Due to Eigen 3.3.7 (Ubunu 2004) giving slightly different results
+    BOOST_TEST(valueA == expectedValTwoA, boost::test_tools::tolerance(1e-8));
     BOOST_TEST(valueB == expectedValTwoB);
     BOOST_TEST(valueC == expectedValTwoC);
 
