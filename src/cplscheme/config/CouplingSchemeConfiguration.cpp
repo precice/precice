2a8bdd6a103a9cd7ea9eb6c7e15dfd6466c39e05
Kyle Davis <kyle.davis3019@gmail.com>
2022-05-06 10:47:56 +0200

    Update CouplingSchemeConfiguration.cpp

--- src/cplscheme/config/CouplingSchemeConfiguration.cpp
+++ src/cplscheme/config/CouplingSchemeConfiguration.cpp
@@ -697,9 +697,9 @@ void CouplingSchemeConfiguration::addRelativeConvergenceMeasure(
                 "Please check the <relative-convergence-measure limit=\"{}\" data=\"{}\" mesh=\"{}\" /> subtag "
                 "in your <coupling-scheme ... /> in the preCICE configuration file.",
                 limit, dataName, meshName);
-  if (limit > 10*math::NUMERICAL_ZERO_DIFFERENCE){ 
+  if (limit < 10*math::NUMERICAL_ZERO_DIFFERENCE){ 
     PRECICE_WARN("The relative convergence limit=\"{}\" is close to the hard-coded numerical resolution=\"{}\" of preCICE. " 
-                 "This may lead to instabilities. The minimum relative convergence limit allowed is \"{}\"  ", limit, math::NUMERICAL_ZERO_DIFFERENCE, 10*math::NUMERICAL_ZERO_DIFFERENCE); 
+                 "This may lead to instabilities. The minimum relative convergence limit should be > \"{}\"  ", limit, math::NUMERICAL_ZERO_DIFFERENCE, 10*math::NUMERICAL_ZERO_DIFFERENCE); 
   }
 
   impl::PtrConvergenceMeasure measure(new impl::RelativeConvergenceMeasure(limit));
@@ -726,9 +726,9 @@ void CouplingSchemeConfiguration::addResidualRelativeConvergenceMeasure(
                 "Please check the <residul-relative-convergence-measure limit=\"{}\" data=\"{}\" mesh=\"{}\" /> subtag "
                 "in your <coupling-scheme ... /> in the preCICE configuration file.",
                 limit, dataName, meshName);
- if (limit > 10*math::NUMERICAL_ZERO_DIFFERENCE){ 
+ if (limit < 10*math::NUMERICAL_ZERO_DIFFERENCE){ 
     PRECICE_WARN("The relative convergence limit=\"{}\" is close to the hard-coded numerical resolution=\"{}\" of preCICE. " 
-                 "This may lead to instabilities. The minimum relative convergence limit allowed is \"{}\"  ", limit, math::NUMERICAL_ZERO_DIFFERENCE, 10*math::NUMERICAL_ZERO_DIFFERENCE); 
+                 "This may lead to instabilities. The minimum relative convergence limit should be > \"{}\"  ", limit, math::NUMERICAL_ZERO_DIFFERENCE, 10*math::NUMERICAL_ZERO_DIFFERENCE); 
   }
 
   impl::PtrConvergenceMeasure measure(new impl::ResidualRelativeConvergenceMeasure(limit));
