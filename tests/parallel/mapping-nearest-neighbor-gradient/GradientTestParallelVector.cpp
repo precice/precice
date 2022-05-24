2b719c94fb40ef6af34636e613ec157da9c6b022
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-05-24 17:01:21 +0200

    Adjust parallel tests

--- tests/parallel/mapping-nearest-neighbor-gradient/GradientTestParallelVector.cpp
+++ tests/parallel/mapping-nearest-neighbor-gradient/GradientTestParallelVector.cpp
@@ -87,7 +87,7 @@ BOOST_AUTO_TEST_CASE(GradientTestParallelVector)
       for (int i = 0; i < 36; i++) {
         gradientValues[i] = 1.0;
       }
-      interface.writeBlockVectorGradientData(dataID, 6, vertexIDs, gradientValues, true);
+      interface.writeBlockVectorGradientData(dataID, 6, vertexIDs, gradientValues);
     }
     interface.advance(1.0);
     interface.finalize();
