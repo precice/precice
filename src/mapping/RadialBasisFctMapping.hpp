0e13c2bd3e04db34fb98ecd4bc9dff57a4c1e528
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-06-08 08:28:30 +0200

    Extract RBF solver to a separate class

--- src/mapping/RadialBasisFctMapping.hpp
+++ src/mapping/RadialBasisFctMapping.hpp
@@ -3,6 +3,7 @@
 #include <Eigen/Core>
 #include <Eigen/QR>
 
+#include "RadialBasisFctSolver.h"
 #include "com/CommunicateMesh.hpp"
 #include "com/Communication.hpp"
 #include "impl/BasisFunctions.hpp"
