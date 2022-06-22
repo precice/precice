2e0de05abd4b7120d4a327296d56711862954fe3
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-06-23 00:27:34 +0200

    Add Todo for c++17 and rebase

--- src/mapping/RadialBasisFctSolver.hpp
+++ src/mapping/RadialBasisFctSolver.hpp
@@ -56,6 +56,7 @@ inline double computeSquaredDifference(
   for (unsigned int d = 0; d < v.size(); ++d) {
     v[d] = (u[d] - v[d]) * static_cast<int>(activeAxis[d]);
   }
+  // @todo: this can be replaced by std::hypot when moving to C++17
   return std::accumulate(v.begin(), v.end(), static_cast<double>(0.), [](auto &res, auto &val) { return res + val * val; });
 }
 
