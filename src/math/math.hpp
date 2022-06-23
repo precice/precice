80a18a239d971f78db930c9142c4cb3e472355cc
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-06-23 11:25:50 +0200

    Implement basis functions via recursion

--- src/math/math.hpp
+++ src/math/math.hpp
@@ -18,5 +18,18 @@ inline int sign(double number)
   return 0;
 }
 
+/// Computes the power of an integer (x^iexp) using recursion, which is much faster than std::pow(x, iexp)
+template <int iexp, typename T>
+inline constexpr T pow_int(const T x)
+{
+  static_assert(iexp >= 0, "Exponent must be an integer greater zero.");
+
+  if (iexp == 0)
+    return static_cast<T>(1.);
+  else
+    // exponentiation by squaring
+    return ((iexp % 2 == 1) ? x * pow_int<iexp / 2>(x * x) : pow_int<iexp / 2>(x * x));
+}
+
 } // namespace math
 } // namespace precice
