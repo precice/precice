2f8ef96a62ef8bfaacfa2133752da43233810d92
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-06-30 11:04:03 +0200

    Fix test names to match directory name

--- tests/serial/mapping-nearest-neighbor-gradient/GradientTestUnidirectionalReadBlockVector.cpp
+++ tests/serial/mapping-nearest-neighbor-gradient/GradientTestUnidirectionalReadBlockVector.cpp
@@ -10,7 +10,7 @@ using precice::testing::TestContext;
 
 BOOST_AUTO_TEST_SUITE(Integration)
 BOOST_AUTO_TEST_SUITE(Serial)
-BOOST_AUTO_TEST_SUITE(SerialGradientMappingTests)
+BOOST_AUTO_TEST_SUITE(MappingNearestNeighborGradient)
 
 // Unidirectional Nearest Neighbor Gradient Read Mapping
 BOOST_AUTO_TEST_CASE(GradientTestUnidirectionalReadBlockVector)
