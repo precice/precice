c0167d157a1241e02cfa508b5d52e83557f12512
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2022-07-12 12:55:40 +0200

    Port unit and export tests to new gradient handling

--- src/mapping/tests/NearestNeighborGradientMappingTest.cpp
+++ src/mapping/tests/NearestNeighborGradientMappingTest.cpp
@@ -24,8 +24,10 @@ BOOST_AUTO_TEST_CASE(ConsistentNonIncremental)
 
   // Create mesh to map from
   PtrMesh inMesh(new Mesh("InMesh", dimensions, testing::nextMeshID()));
-  PtrData inDataScalar   = inMesh->createData("InDataScalar", 1, 0_dataID, true);
-  PtrData inDataVector   = inMesh->createData("InDataVector", 2, 1_dataID, true);
+  PtrData inDataScalar = inMesh->createData("InDataScalar", 1, 0_dataID);
+  PtrData inDataVector = inMesh->createData("InDataVector", 2, 1_dataID);
+  inDataScalar->setDataGradientRequired();
+  inDataVector->setDataGradientRequired();
   int     inDataScalarID = inDataScalar->getID();
   int     inDataVectorID = inDataVector->getID();
   Vertex &inVertex0      = inMesh->createVertex(Eigen::Vector2d::Constant(0.0));
@@ -113,8 +115,10 @@ BOOST_AUTO_TEST_CASE(ConsistentGradientNotConstant)
 
   // Create mesh to map from
   PtrMesh inMesh(new Mesh("InMesh", dimensions, testing::nextMeshID()));
-  PtrData inDataScalar   = inMesh->createData("InDataScalar", 1, 0_dataID, true);
-  PtrData inDataVector   = inMesh->createData("InDataVector", 2, 1_dataID, true);
+  PtrData inDataScalar = inMesh->createData("InDataScalar", 1, 0_dataID);
+  PtrData inDataVector = inMesh->createData("InDataVector", 2, 1_dataID);
+  inDataScalar->setDataGradientRequired();
+  inDataVector->setDataGradientRequired();
   int     inDataScalarID = inDataScalar->getID();
   int     inDataVectorID = inDataVector->getID();
   Vertex &inVertex0      = inMesh->createVertex(Eigen::Vector2d::Constant(0.0));
