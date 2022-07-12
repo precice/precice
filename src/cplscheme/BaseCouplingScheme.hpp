8c025db51557b29623cdac06da976357511fdf52
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-07-12 08:56:00 +0200

    Apply suggestions from code review
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@gmail.com>

--- src/cplscheme/BaseCouplingScheme.hpp
+++ src/cplscheme/BaseCouplingScheme.hpp
@@ -186,6 +186,7 @@ public:
    */
   void initialize(double startTime, int startTimeWindow) override final;
 
+  /// Receives result of first advance, if this has to happen inside SolverInterface::initialize(), see CouplingScheme.hpp
   void receiveResultOfFirstAdvance() override final;
 
   /**
