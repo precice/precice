6def76867dba690e3913d261c702915988042759
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-08-18 09:13:31 +0200

    Add unit test for zeroth and first order piecewise interpolation.

---
+++ src/time/tests/PiecewiseWaveformTest.cpp
@@ -0,0 +1,172 @@
+#include <Eigen/Core>
+#include "testing/TestContext.hpp"
+#include "testing/Testing.hpp"
+#include "testing/WaveformFixture.hpp"
+#include "time/Time.hpp"
+#include "time/Waveform.hpp"
+
+using namespace precice;
+using namespace precice::time;
+
+BOOST_AUTO_TEST_SUITE(TimeTests)
+BOOST_AUTO_TEST_SUITE(PiecewiseWaveformTests)
+BOOST_AUTO_TEST_SUITE(InterpolationTests)
+
+BOOST_AUTO_TEST_CASE(testPiecewiseInterpolateDataZerothOrder)
+{
+  PRECICE_TEST(1_rank);
+
+  testing::WaveformFixture fixture;
+
+  // Test zeroth order interpolation
+  const int interpolationOrder = 0;
+  Waveform  waveform(interpolationOrder);
+  const int valuesSize = 1;
+  waveform.initialize(Eigen::VectorXd::Zero(valuesSize));
+
+  BOOST_TEST(fixture.valuesSize(waveform) == valuesSize);
+  BOOST_TEST(fixture.numberOfStoredSamples(waveform) == 2);
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 0.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 0.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 0.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 0.0));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 0.0));
+
+  Eigen::VectorXd value(1);
+  value(0) = 0.5;
+  waveform.store(value, 0.5);
+
+  value(0) = 1.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 0.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 0.5));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 0.5));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 1.0));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 1.0));
+
+  value(0) = 1.5;
+  waveform.store(value, 0.5);
+
+  value(0) = 2.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 0.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 1.5));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 1.5));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 2.0));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 2.0));
+
+  waveform.moveToNextWindow();
+  BOOST_TEST(fixture.numberOfStoredSamples(waveform) == 2);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 2.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 2.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 2.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 2.0));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 2.0));
+
+  value(0) = 3.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 2.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 3.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 3.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 3.0));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 3.0));
+
+
+  value(0) = 1.5;
+  waveform.store(value, 0.5);
+
+  value(0) = 4.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 2.0));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 1.5));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 1.5));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 4.0));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 4.0));
+}
+
+BOOST_AUTO_TEST_CASE(testPiecewiseInterpolateDataFirstOrder)
+{
+  PRECICE_TEST(1_rank);
+
+  testing::WaveformFixture fixture;
+
+  // Test zeroth order interpolation
+  const int interpolationOrder = 1;
+  Waveform  waveform(interpolationOrder);
+  const int valuesSize = 1;
+  waveform.initialize(Eigen::VectorXd::Zero(valuesSize));
+
+  BOOST_TEST(fixture.valuesSize(waveform) == valuesSize);
+  BOOST_TEST(fixture.numberOfStoredSamples(waveform) == 2);
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 0.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 0.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 0.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 0.00));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 0.00));
+
+  Eigen::VectorXd value(1);
+  value(0) = 0.5;
+  waveform.store(value, 0.5);
+
+  value(0) = 1.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 0.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 0.25));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 0.50));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 0.75));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 1.00));
+
+  value(0) = 1.5;
+  waveform.store(value, 0.5);
+
+  value(0) = 2.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 0.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 0.75));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 1.50));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 1.75));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 2.00));
+
+  waveform.moveToNextWindow();
+  BOOST_TEST(fixture.numberOfStoredSamples(waveform) == 2);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 2.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 2.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 2.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 2.00));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 2.00));
+
+  value(0) = 3.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 2.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 2.25));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 2.50));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 2.75));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 3.00));
+
+
+  value(0) = 1.5;
+  waveform.store(value, 0.5);
+
+  value(0) = 4.0;
+  waveform.store(value, 1.0);
+
+  BOOST_TEST(testing::equals(waveform.sample(0.00)(0), 2.00));
+  BOOST_TEST(testing::equals(waveform.sample(0.25)(0), 1.75));
+  BOOST_TEST(testing::equals(waveform.sample(0.50)(0), 1.50));
+  BOOST_TEST(testing::equals(waveform.sample(0.75)(0), 2.75));
+  BOOST_TEST(testing::equals(waveform.sample(1.00)(0), 4.00));
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
