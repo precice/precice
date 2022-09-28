77537398b3f9b6ea6bca9d6a3cf69e5256e5374b
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-09-28 13:38:04 +0200

    Simplify waveform class: Don't use data from old window, allow higher-order interpolation through BSplines  (#1422)
    * Allow to store multiple samples within one window.
    * Allow higher-order interpolation through B-Splines, if sufficiently
    many samples are available.
    * Simplifies Waveform by removing data from past windows and support for
    second-order interpolation by using data from old window.
    * Reset all samples when window is repeated - except the sample at the
    beginning of the window.
    * Update intergration tests for zeroth order interpolation for more
    consistent behavior with first order interpolation.
    * Introduce time::Storage
    
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>

---
+++ src/time/tests/StorageTest.cpp
@@ -0,0 +1,108 @@
+#include <Eigen/Core>
+#include "testing/TestContext.hpp"
+#include "testing/Testing.hpp"
+#include "time/Storage.hpp"
+
+using namespace precice;
+using namespace precice::time;
+
+BOOST_AUTO_TEST_SUITE(TimeTests)
+BOOST_AUTO_TEST_SUITE(StorageTests)
+
+// create storage and test for correct initial values.
+BOOST_AUTO_TEST_CASE(testInitialize)
+{
+  PRECICE_TEST(1_rank);
+  auto storage = Storage();
+  int  nValues = 3;
+  BOOST_TEST(storage.nTimes() == 0);
+  storage.initialize(Eigen::VectorXd::Ones(nValues));
+  BOOST_TEST(storage.nDofs() == nValues);
+  BOOST_TEST(storage.nTimes() == 2);
+  for (int i = 0; i < nValues; i++) {
+    BOOST_TEST(storage.getValueAtOrAfter(0)(i) == 1);
+    BOOST_TEST(storage.getValueAtOrAfter(0.5)(i) == 1);
+    BOOST_TEST(storage.getValueAtOrAfter(1)(i) == 1);
+  }
+}
+
+// create storage and clear it.
+BOOST_AUTO_TEST_CASE(testClear)
+{
+  PRECICE_TEST(1_rank);
+  auto storage = Storage();
+  int  nValues = 3;
+  BOOST_TEST(storage.nTimes() == 0);
+  storage.initialize(Eigen::VectorXd::Ones(nValues));
+  BOOST_TEST(storage.nDofs() == nValues);
+  BOOST_TEST(storage.nTimes() == 2);
+  BOOST_TEST(storage.maxStoredNormalizedDt() == 1.0);
+  storage.clear(true);
+  BOOST_TEST(storage.nDofs() == nValues);
+  BOOST_TEST(storage.nTimes() == 1);
+  BOOST_TEST(storage.maxStoredNormalizedDt() == 0.0);
+  storage.clear(false);
+  BOOST_TEST(storage.nTimes() == 0);
+}
+
+// create storage, add some values and then move to next window.
+BOOST_AUTO_TEST_CASE(testMove)
+{
+  PRECICE_TEST(1_rank);
+  auto storage = Storage();
+  int  nValues = 3;
+  BOOST_TEST(storage.nTimes() == 0);
+  storage.initialize(Eigen::VectorXd::Ones(nValues));
+  BOOST_TEST(storage.nDofs() == nValues);
+  BOOST_TEST(storage.nTimes() == 2);
+  BOOST_TEST(storage.maxStoredNormalizedDt() == 1.0);
+  storage.clear(true);
+  BOOST_TEST(storage.nTimes() == 1);
+  storage.setValueAtTime(0.5, Eigen::VectorXd::Ones(nValues));
+  BOOST_TEST(storage.nTimes() == 2);
+  BOOST_TEST(storage.maxStoredNormalizedDt() == 0.5);
+  storage.setValueAtTime(1.0, Eigen::VectorXd::Zero(nValues));
+  BOOST_TEST(storage.nTimes() == 3);
+  BOOST_TEST(storage.maxStoredNormalizedDt() == 1.0);
+  for (int i = 0; i < nValues; i++) {
+    BOOST_TEST(storage.getValueAtOrAfter(0)(i) == 1);
+    BOOST_TEST(storage.getValueAtOrAfter(0.5)(i) == 1);
+    BOOST_TEST(storage.getValueAtOrAfter(1)(i) == 0);
+  }
+  storage.move();
+  BOOST_TEST(storage.nDofs() == nValues);
+  BOOST_TEST(storage.nTimes() == 2);
+  BOOST_TEST(storage.maxStoredNormalizedDt() == 1.0);
+  for (int i = 0; i < nValues; i++) {
+    BOOST_TEST(storage.getValueAtOrAfter(0)(i) == 0);
+    BOOST_TEST(storage.getValueAtOrAfter(1)(i) == 0);
+  }
+}
+
+// get times and values
+BOOST_AUTO_TEST_CASE(testGetTimesAndValues)
+{
+  PRECICE_TEST(1_rank);
+  auto storage = Storage();
+  int  nValues = 3;
+  storage.initialize(Eigen::VectorXd::Ones(nValues));
+  storage.clear(true);
+  storage.setValueAtTime(0.5, Eigen::VectorXd::Ones(nValues));
+  storage.setValueAtTime(1.0, Eigen::VectorXd::Zero(nValues));
+  auto times = storage.getTimes();
+  BOOST_TEST(times[0] == 0.0);
+  BOOST_TEST(times[1] == 0.5);
+  BOOST_TEST(times[2] == 1.00);
+  auto timesAndValues = storage.getTimesAndValues();
+  BOOST_TEST(timesAndValues.first[0] == 0.0);
+  BOOST_TEST(timesAndValues.first[1] == 0.5);
+  BOOST_TEST(timesAndValues.first[2] == 1.00);
+  for (int i = 0; i < nValues; i++) {
+    BOOST_TEST(timesAndValues.second.col(0)(i) == 1);
+    BOOST_TEST(timesAndValues.second.col(1)(i) == 1);
+    BOOST_TEST(timesAndValues.second.col(2)(i) == 0);
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
