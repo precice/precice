4446fe0287218075e19c4331e9877a958281d4be
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-11-02 09:59:20 +0100

    Increase timeouts
    precice.cplscheme and precice.integration.Serial take longer with new tests introduced in https://github.com/precice/precice/pull/1414

--- cmake/CTestConfig.cmake
+++ cmake/CTestConfig.cmake
@@ -2,8 +2,8 @@
 # CTest
 #
 
-set(PRECICE_TEST_TIMEOUT_LONG 120 CACHE STRING "The timeout in seconds for longer tests.")
-set(PRECICE_TEST_TIMEOUT_SHORT 20 CACHE STRING "The timeout in seconds for shorter tests.")
+set(PRECICE_TEST_TIMEOUT_LONG 300 CACHE STRING "The timeout in seconds for longer tests.")
+set(PRECICE_TEST_TIMEOUT_SHORT 60 CACHE STRING "The timeout in seconds for shorter tests.")
 
 set(PRECICE_TEST_DIR "${preCICE_BINARY_DIR}/TestOutput")
 mark_as_advanced(PRECICE_TEST_DIR)
