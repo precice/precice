33a017e8871d54bc507bc0d3ff35732bf5f41a0f
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-11-03 17:16:48 +0100

    Update cmake/CTestConfig.cmake
    Co-authored-by: Frédéric Simonis <simonisfrederic@gmail.com>

--- cmake/CTestConfig.cmake
+++ cmake/CTestConfig.cmake
@@ -2,7 +2,7 @@
 # CTest
 #
 
-set(PRECICE_TEST_TIMEOUT_LONG 300 CACHE STRING "The timeout in seconds for longer tests.")
+set(PRECICE_TEST_TIMEOUT_LONG 180 CACHE STRING "The timeout in seconds for longer tests.")
 set(PRECICE_TEST_TIMEOUT_SHORT 20 CACHE STRING "The timeout in seconds for shorter tests.")
 
 set(PRECICE_TEST_DIR "${preCICE_BINARY_DIR}/TestOutput")
