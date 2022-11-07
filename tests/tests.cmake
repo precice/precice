f299701bc0a323925c0d48f904769e1176c8bfe8
Frédéric Simonis <simonisfrederic@gmail.com>
2022-11-07 16:21:41 +0100

    Add circular test

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -72,6 +72,8 @@ target_sources(testprecice
     tests/serial/access-received-mesh/Implicit.cpp
     tests/serial/action-timings/ActionTimingsExplicit.cpp
     tests/serial/action-timings/ActionTimingsImplicit.cpp
+    tests/serial/circular/Explicit.cpp
+    tests/serial/circular/helper.hpp
     tests/serial/convergence-measures/helpers.cpp
     tests/serial/convergence-measures/helpers.hpp
     tests/serial/convergence-measures/testConvergenceMeasures1.cpp
