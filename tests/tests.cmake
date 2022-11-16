ba64412aee935ea58c542d50c1d6cb3aaa5180af
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2022-11-16 13:21:02 +0100

    Add first muscles tests (failing)

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -74,6 +74,7 @@ target_sources(testprecice
     tests/serial/action-timings/ActionTimingsImplicit.cpp
     tests/serial/circular/Explicit.cpp
     tests/serial/circular/helper.hpp
+    tests/serial/compositional/TwoActivatedMuscles.cpp
     tests/serial/convergence-measures/helpers.cpp
     tests/serial/convergence-measures/helpers.hpp
     tests/serial/convergence-measures/testConvergenceMeasures1.cpp
