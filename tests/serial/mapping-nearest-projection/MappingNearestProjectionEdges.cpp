18ab05298f730f85258548968088bbfa90d4b209
Frédéric Simonis <simonisfrederic@gmail.com>
2022-11-17 15:41:39 +0100

    Convert to data tests

--- tests/serial/mapping-nearest-projection/MappingNearestProjectionEdges.cpp
+++ tests/serial/mapping-nearest-projection/MappingNearestProjectionEdges.cpp
@@ -2,21 +2,23 @@
 
 #include "testing/Testing.hpp"
 
+#include <boost/test/data/test_case.hpp>
 #include <precice/SolverInterface.hpp>
 #include "helpers.hpp"
 
 BOOST_AUTO_TEST_SUITE(Integration)
 BOOST_AUTO_TEST_SUITE(Serial)
 BOOST_AUTO_TEST_SUITE(MappingNearestProjection)
-BOOST_AUTO_TEST_CASE(MappingNearestProjectionEdges)
+BOOST_DATA_TEST_CASE(MappingNearestProjectionEdges,
+                     boost::unit_test::data::make({true, false}) * boost::unit_test::data::make({true, false}),
+                     defineEdgesExplicitly, useBulkFunctions)
 {
   /**
- * @brief Tests the Nearest Projection Mapping between two participants with explicit definition of edges
- *
- */
+   * @brief Tests the Nearest Projection Mapping between two participants with explicit definition of edges
+   *
+   */
   PRECICE_TEST("SolverOne"_on(1_rank), "SolverTwo"_on(1_rank));
-  bool defineEdgesExplicitly = true;
-  testMappingNearestProjection(defineEdgesExplicitly, context.config(), context);
+  testMappingNearestProjection(defineEdgesExplicitly, useBulkFunctions, context.config(), context);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // Integration
