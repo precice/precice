42573911d81727a10751d2fc741bce514c84028c
Frédéric Simonis <simonisfrederic@gmail.com>
2022-11-17 16:45:05 +0100

    Move the check for multiple implicit cplschemes to config

--- src/cplscheme/config/CouplingSchemeConfiguration.cpp
+++ src/cplscheme/config/CouplingSchemeConfiguration.cpp
@@ -377,7 +377,12 @@ void CouplingSchemeConfiguration::addCouplingScheme(
     if (utils::contained(participantName, _couplingSchemeCompositions)) {
       PRECICE_DEBUG("Coupling scheme composition exists already for participant");
       // Fetch the composition and add the new scheme.
-      PRECICE_ASSERT(_couplingSchemeCompositions[participantName] != nullptr);
+      PRECICE_ASSERT(_couplingSchemeCompositions.count(participantName) == 1);
+      auto composition = _couplingSchemeCompositions.at(participantName);
+      PRECICE_CHECK(!cplScheme->isImplicitCouplingScheme() || !composition->isImplicitCouplingScheme(),
+                    "You attempted to define a second implicit coupling-scheme for the participant \"{}\", which is not allowed. "
+                    "Please use a multi coupling-scheme for true implicit coupling of multiple participants.",
+                    participantName);
       _couplingSchemeCompositions[participantName]->addCouplingScheme(cplScheme);
     } else {
       PRECICE_DEBUG("No composition exists for the participant");
