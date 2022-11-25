12463752084ff359b5b6be041dc4563d42130817
Frédéric Simonis <simonisfrederic@gmail.com>
2022-11-25 12:55:09 +0100

    Remove migration helpers

--- tests/serial/mapping-nearest-projection/helpers.hpp
+++ tests/serial/mapping-nearest-projection/helpers.hpp
@@ -15,24 +15,4 @@ void testQuadMappingNearestProjectionTallKite(bool defineEdgesExplicitly, bool u
 
 void testQuadMappingNearestProjectionWideKite(bool defineEdgesExplicitly, bool useBulkFunctions, const std::string configFile, const TestContext &context);
 
-inline void testMappingNearestProjection(bool defineEdgesExplicitly, const std::string configFile, const TestContext &context)
-{
-  testMappingNearestProjection(defineEdgesExplicitly, false, configFile, context);
-}
-
-inline void testQuadMappingNearestProjection(bool defineEdgesExplicitly, const std::string configFile, const TestContext &context)
-{
-  testQuadMappingNearestProjection(defineEdgesExplicitly, false, configFile, context);
-}
-
-inline void testQuadMappingNearestProjectionTallKite(bool defineEdgesExplicitly, const std::string configFile, const TestContext &context)
-{
-  testQuadMappingNearestProjectionTallKite(defineEdgesExplicitly, false, configFile, context);
-}
-
-inline void testQuadMappingNearestProjectionWideKite(bool defineEdgesExplicitly, const std::string configFile, const TestContext &context)
-{
-  testQuadMappingNearestProjectionWideKite(defineEdgesExplicitly, false, configFile, context);
-}
-
 #endif
