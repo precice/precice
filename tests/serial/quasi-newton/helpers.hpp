8e1d7cc03256a7a99f7fac014ef57b78dd65985f
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-12-07 10:33:51 +0100

    Cleanup from parallel case.

--- tests/serial/quasi-newton/helpers.hpp
+++ tests/serial/quasi-newton/helpers.hpp
@@ -9,7 +9,6 @@ using precice::testing::TestContext;
 
 namespace tests::serial {
 void runTestQN(std::string const &config, TestContext const &context);
-void runTestQNEmptyPartition(std::string const &config, TestContext const &context);
 } // namespace tests::serial
 
 #endif
