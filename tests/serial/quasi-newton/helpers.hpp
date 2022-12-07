5e4afd26a60da59ab1aef47afae56ecf72f10491
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2022-12-07 10:12:58 +0100

    Add serial version of QN tests.

---
+++ tests/serial/quasi-newton/helpers.hpp
@@ -0,0 +1,15 @@
+#ifndef PRECICE_NO_MPI
+
+#pragma once
+
+#include "testing/TestContext.hpp"
+
+using namespace precice;
+using precice::testing::TestContext;
+
+namespace tests::serial {
+void runTestQN(std::string const &config, TestContext const &context);
+void runTestQNEmptyPartition(std::string const &config, TestContext const &context);
+} // namespace tests::serial
+
+#endif
