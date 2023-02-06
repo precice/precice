2a099e8ca431768b115038300c408bfced925a66
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2023-02-06 14:01:55 +0100

    Add optional for xml getters

--- src/xml/XMLTag.hpp
+++ src/xml/XMLTag.hpp
@@ -3,6 +3,7 @@
 #include <Eigen/Core>
 #include <map>
 #include <memory>
+#include <optional>
 #include <set>
 #include <string>
 #include <vector>
@@ -168,13 +169,13 @@ public:
     return _fullName;
   }
 
-  double getDoubleAttributeValue(const std::string &name) const;
+  double getDoubleAttributeValue(const std::string &name, std::optional<double> default_value = std::nullopt) const;
 
-  int getIntAttributeValue(const std::string &name) const;
+  int getIntAttributeValue(const std::string &name, std::optional<int> default_value = std::nullopt) const;
 
-  const std::string &getStringAttributeValue(const std::string &name) const;
+  const std::string &getStringAttributeValue(const std::string &name, std::optional<std::string> default_value = std::nullopt) const;
 
-  bool getBooleanAttributeValue(const std::string &name) const;
+  bool getBooleanAttributeValue(const std::string &name, std::optional<bool> default_value = std::nullopt) const;
 
   const AttributeMap<double> &getDoubleAttributes() const
   {
