a4d72b2ae0b93834b9f493af57e00caf421bb1da
timo-schrader <timo-schrader>
2023-02-12 18:40:07 +0100

    removed TODO because include problem seems to be gone

--- src/mapping/ginkgo_kernels/GinkgoRBFKernels.cpp
+++ src/mapping/ginkgo_kernels/GinkgoRBFKernels.cpp
@@ -30,7 +30,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
-#include "mapping/impl/DeviceBasisFunctions.cuh" // TODO: Fix include
+#include "mapping/impl/DeviceBasisFunctions.cuh"
 
 #include <functional>
 #include <ginkgo/ginkgo.hpp>
