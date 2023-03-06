b213572b063fecf0635ac12c8248cbcd34fe7ec2
Frédéric Simonis <simonisfrederic@gmail.com>
2023-03-06 16:05:38 +0100

    Catch exceptions in bindings

--- extras/bindings/c/src/SolverInterfaceC.cpp
+++ extras/bindings/c/src/SolverInterfaceC.cpp
@@ -6,6 +6,7 @@ extern "C" {
 #include "logging/LogMacros.hpp"
 #include "logging/Logger.hpp"
 #include "precice/SolverInterface.hpp"
+#include "precice/exceptions.hpp"
 #include "precice/impl/versions.hpp"
 #include "utils/assertion.hpp"
 
@@ -32,7 +33,7 @@ void precicec_createSolverInterface_withCommunicator(
     int         solverProcessIndex,
     int         solverProcessSize,
     void *      communicator)
-{
+try {
   std::string stringAccessorName(participantName);
   std::string stringConfigFileName(configFileName);
 
@@ -42,6 +43,8 @@ void precicec_createSolverInterface_withCommunicator(
                                           solverProcessIndex,
                                           solverProcessSize,
                                           communicator));
+} catch (...) {
+  std::abort();
 }
 
 void precicec_createSolverInterface(
@@ -49,7 +52,7 @@ void precicec_createSolverInterface(
     const char *configFileName,
     int         solverProcessIndex,
     int         solverProcessSize)
-{
+try {
   std::string stringAccessorName(participantName);
   std::string stringConfigFileName(configFileName);
 
@@ -58,115 +61,147 @@ void precicec_createSolverInterface(
                                           stringConfigFileName,
                                           solverProcessIndex,
                                           solverProcessSize));
+} catch (...) {
+  std::abort();
 }
 
 double precicec_initialize()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->initialize();
+} catch (...) {
+  std::abort();
 }
 
 double precicec_advance(double computedTimestepLength)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->advance(computedTimestepLength);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_finalize()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->finalize();
   impl.reset();
+} catch (...) {
+  std::abort();
 }
 
 int precicec_getDimensions()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->getDimensions();
+} catch (...) {
+  std::abort();
 }
 
 int precicec_isCouplingOngoing()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->isCouplingOngoing()) {
     return 1;
   }
   return 0;
+} catch (...) {
+  std::abort();
 }
 
 int precicec_isTimeWindowComplete()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->isTimeWindowComplete()) {
     return 1;
   }
   return 0;
+} catch (...) {
+  std::abort();
 }
 
 int precicec_requiresInitialData()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->requiresInitialData() ? 1 : 0;
+} catch (...) {
+  std::abort();
 }
 
 int precicec_requiresWritingCheckpoint()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->requiresWritingCheckpoint() ? 1 : 0;
+} catch (...) {
+  std::abort();
 }
 
 int precicec_requiresReadingCheckpoint()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->requiresReadingCheckpoint() ? 1 : 0;
+} catch (...) {
+  std::abort();
 }
 
 int precicec_hasMesh(const char *meshName)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   std::string stringMeshName(meshName);
   if (impl->hasMesh(stringMeshName)) {
     return 1;
   }
   return 0;
+} catch (...) {
+  std::abort();
 }
 
 int precicec_getMeshID(const char *meshName)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   std::string stringMeshName(meshName);
   return impl->getMeshID(stringMeshName);
+} catch (...) {
+  std::abort();
 }
 
 int precicec_hasData(const char *dataName, int meshID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   std::string stringDataName(dataName);
   return impl->hasData(stringDataName, meshID);
+} catch (...) {
+  std::abort();
 }
 
 int precicec_getDataID(const char *dataName, int meshID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   std::string stringDataName(dataName);
   return impl->getDataID(stringDataName, meshID);
+} catch (...) {
+  std::abort();
 }
 
 int precicec_requiresMeshConnectivityFor(int meshID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->requiresMeshConnectivityFor(meshID)) {
     return 1;
   }
   return 0;
+} catch (...) {
+  std::abort();
 }
 
 int precicec_setMeshVertex(
     int           meshID,
     const double *position)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->setMeshVertex(meshID, position);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshVertices(
@@ -174,34 +209,42 @@ void precicec_setMeshVertices(
     int           size,
     const double *positions,
     int *         ids)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshVertices(meshID, size, positions, ids);
+} catch (...) {
+  std::abort();
 }
 
 int precicec_getMeshVertexSize(
     int meshID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   return impl->getMeshVertexSize(meshID);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshEdge(
     int meshID,
     int firstVertexID,
     int secondVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshEdge(meshID, firstVertexID, secondVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshEdges(
     int        meshID,
     int        size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshEdges(meshID, size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshTriangle(
@@ -209,18 +252,22 @@ void precicec_setMeshTriangle(
     int firstVertexID,
     int secondVertexID,
     int thirdVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTriangle(meshID, firstVertexID, secondVertexID, thirdVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshTriangles(
     int        meshID,
     int        size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTriangles(meshID, size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshQuad(
@@ -229,18 +276,22 @@ void precicec_setMeshQuad(
     int secondVertexID,
     int thirdVertexID,
     int fourthVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshQuad(meshID, firstVertexID, secondVertexID, thirdVertexID, fourthVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshQuads(
     int        meshID,
     int        size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshQuads(meshID, size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshTetrahedron(
@@ -249,18 +300,22 @@ void precicec_setMeshTetrahedron(
     int secondVertexID,
     int thirdVertexID,
     int fourthVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTetrahedron(meshID, firstVertexID, secondVertexID, thirdVertexID, fourthVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshTetrahedra(
     int        meshID,
     int        size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTetrahedra(meshID, size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeBlockVectorData(
@@ -268,18 +323,22 @@ void precicec_writeBlockVectorData(
     int           size,
     const int *   valueIndices,
     const double *values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockVectorData(dataID, size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeVectorData(
     int           dataID,
     int           valueIndex,
     const double *dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeVectorData(dataID, valueIndex, dataValue);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeBlockScalarData(
@@ -287,18 +346,22 @@ void precicec_writeBlockScalarData(
     int           size,
     const int *   valueIndices,
     const double *values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockScalarData(dataID, size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeScalarData(
     int    dataID,
     int    valueIndex,
     double dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeScalarData(dataID, valueIndex, dataValue);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_readBlockVectorData(
@@ -306,18 +369,22 @@ void precicec_readBlockVectorData(
     int        size,
     const int *valueIndices,
     double *   values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readBlockVectorData(dataID, size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_readVectorData(
     int     dataID,
     int     valueIndex,
     double *dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readVectorData(dataID, valueIndex, dataValue);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_readBlockScalarData(
@@ -325,36 +392,44 @@ void precicec_readBlockScalarData(
     int        size,
     const int *valueIndices,
     double *   values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readBlockScalarData(dataID, size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_readScalarData(
     int     dataID,
     int     valueIndex,
     double *dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readScalarData(dataID, valueIndex, *dataValue);
+} catch (...) {
+  std::abort();
 }
 
 int precicec_requiresGradientDataFor(int dataID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->requiresGradientDataFor(dataID)) {
     return 1;
   }
   return 0;
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeScalarGradientData(
     int           dataID,
     int           valueIndex,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeScalarGradientData(dataID, valueIndex, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeBlockScalarGradientData(
@@ -362,18 +437,22 @@ void precicec_writeBlockScalarGradientData(
     int           size,
     const int *   valueIndices,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockScalarGradientData(dataID, size, valueIndices, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeVectorGradientData(
     int           dataID,
     int           valueIndex,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeVectorGradientData(dataID, valueIndex, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_writeBlockVectorGradientData(
@@ -381,21 +460,27 @@ void precicec_writeBlockVectorGradientData(
     int           size,
     const int *   valueIndices,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockVectorGradientData(dataID, size, valueIndices, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 const char *precicec_getVersionInformation()
-{
+try {
   return precice::versionInformation;
+} catch (...) {
+  std::abort();
 }
 
 void precicec_setMeshAccessRegion(
     const int     meshID,
     const double *boundingBox)
-{
+try {
   impl->setMeshAccessRegion(meshID, boundingBox);
+} catch (...) {
+  std::abort();
 }
 
 void precicec_getMeshVerticesAndIDs(
@@ -403,8 +488,10 @@ void precicec_getMeshVerticesAndIDs(
     const int size,
     int *     ids,
     double *  coordinates)
-{
+try {
   impl->getMeshVerticesAndIDs(meshID, size, ids, coordinates);
+} catch (...) {
+  std::abort();
 }
 
 #ifdef __GNUC__
--- extras/bindings/fortran/src/SolverInterfaceFortran.cpp
+++ extras/bindings/fortran/src/SolverInterfaceFortran.cpp
@@ -42,7 +42,7 @@ void precicef_create_(
     const int * solverProcessSize,
     int         lengthAccessorName,
     int         lengthConfigFileName)
-{
+try {
   // cout << "lengthAccessorName: " << lengthAccessorName << '\n';
   // cout << "lengthConfigFileName: " << lengthConfigFileName << '\n';
   // cout << "solverProcessIndex: " << *solverProcessIndex << '\n';
@@ -56,84 +56,104 @@ void precicef_create_(
   impl.reset(new precice::SolverInterface(stringAccessorName,
                                           stringConfigFileName,
                                           *solverProcessIndex, *solverProcessSize));
+} catch (...) {
+  std::abort();
 }
 
 void precicef_initialize_(
     double *timestepLengthLimit)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *timestepLengthLimit = impl->initialize();
+} catch (...) {
+  std::abort();
 }
 
 void precicef_advance_(
     double *timestepLengthLimit)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *timestepLengthLimit = impl->advance(*timestepLengthLimit);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_finalize_()
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->finalize();
   impl.reset();
+} catch (...) {
+  std::abort();
 }
 
 void precicef_get_dims_(
     int *dimensions)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *dimensions = impl->getDimensions();
+} catch (...) {
+  std::abort();
 }
 
 void precicef_is_coupling_ongoing_(
     int *isOngoing)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->isCouplingOngoing()) {
     *isOngoing = 1;
   } else {
     *isOngoing = 0;
   }
+} catch (...) {
+  std::abort();
 }
 
 void precicef_is_time_window_complete_(
     int *isComplete)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->isTimeWindowComplete()) {
     *isComplete = 1;
   } else {
     *isComplete = 0;
   }
+} catch (...) {
+  std::abort();
 }
 
 void precicef_requires_initial_data_(
     int *isRequired)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *isRequired = impl->requiresInitialData() ? 1 : 0;
+} catch (...) {
+  std::abort();
 }
 
 void precicef_requires_writing_checkpoint_(
     int *isRequired)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *isRequired = impl->requiresWritingCheckpoint() ? 1 : 0;
+} catch (...) {
+  std::abort();
 }
 
 void precicef_requires_reading_checkpoint_(
     int *isRequired)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *isRequired = impl->requiresReadingCheckpoint() ? 1 : 0;
+} catch (...) {
+  std::abort();
 }
 
 void precicef_has_mesh_(
     const char *meshName,
     int *       hasMesh,
     int         lengthMeshName)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   int    strippedLength = precice::impl::strippedLength(meshName, lengthMeshName);
   string stringMeshName(meshName, strippedLength);
@@ -142,17 +162,21 @@ void precicef_has_mesh_(
   } else {
     *hasMesh = 0;
   }
+} catch (...) {
+  std::abort();
 }
 
 void precicef_get_mesh_id_(
     const char *meshName,
     int *       meshID,
     int         lengthMeshName)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   int    strippedLength = precice::impl::strippedLength(meshName, lengthMeshName);
   string stringMeshName(meshName, strippedLength);
   *meshID = impl->getMeshID(stringMeshName);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_has_data_(
@@ -160,7 +184,7 @@ void precicef_has_data_(
     const int * meshID,
     int *       hasData,
     int         lengthDataName)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   int    strippedLength = precice::impl::strippedLength(dataName, lengthDataName);
   string stringDataName(dataName, strippedLength);
@@ -169,6 +193,8 @@ void precicef_has_data_(
   } else {
     *hasData = 0;
   }
+} catch (...) {
+  std::abort();
 }
 
 void precicef_get_data_id_(
@@ -176,40 +202,48 @@ void precicef_get_data_id_(
     const int * meshID,
     int *       dataID,
     int         lengthDataName)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   int    strippedLength = precice::impl::strippedLength(dataName, lengthDataName);
   string stringDataName(dataName, strippedLength);
   *dataID = impl->getDataID(stringDataName, *meshID);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_requires_mesh_connectivity_for_(
     const int *meshID,
     int *      required)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->requiresMeshConnectivityFor(*meshID)) {
     *required = 1;
   } else {
     *required = 0;
   }
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_vertex_(
     const int *   meshID,
     const double *position,
     int *         vertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *vertexID = impl->setMeshVertex(*meshID, position);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_get_mesh_vertex_size_(
     const int *meshID,
     int *      meshSize)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   *meshSize = impl->getMeshVertexSize(*meshID);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_vertices_(
@@ -217,27 +251,33 @@ void precicef_set_vertices_(
     const int *size,
     double *   positions,
     int *      positionIDs)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshVertices(*meshID, *size, positions, positionIDs);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_edge_(
     const int *meshID,
     const int *firstVertexID,
     const int *secondVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshEdge(*meshID, *firstVertexID, *secondVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_mesh_edges_(
     const int *meshID,
     const int *size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshEdges(*meshID, *size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_triangle_(
@@ -245,18 +285,22 @@ void precicef_set_triangle_(
     const int *firstVertexID,
     const int *secondVertexID,
     const int *thirdVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTriangle(*meshID, *firstVertexID, *secondVertexID, *thirdVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_mesh_triangles_(
     const int *meshID,
     const int *size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTriangles(*meshID, *size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_quad_(
@@ -265,18 +309,22 @@ void precicef_set_quad_(
     const int *secondVertexID,
     const int *thirdVertexID,
     const int *fourthVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshQuad(*meshID, *firstVertexID, *secondVertexID, *thirdVertexID, *fourthVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_mesh_quads_(
     const int *meshID,
     const int *size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshQuads(*meshID, *size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_tetrahedron(
@@ -285,18 +333,22 @@ void precicef_set_tetrahedron(
     const int *secondVertexID,
     const int *thirdVertexID,
     const int *fourthVertexID)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTetrahedron(*meshID, *firstVertexID, *secondVertexID, *thirdVertexID, *fourthVertexID);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_mesh_tetrahedra_(
     const int *meshID,
     const int *size,
     const int *vertices)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshTetrahedra(*meshID, *size, vertices);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_bvdata_(
@@ -304,18 +356,22 @@ void precicef_write_bvdata_(
     const int *size,
     int *      valueIndices,
     double *   values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockVectorData(*dataID, *size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_vdata_(
     const int *   dataID,
     const int *   valueIndex,
     const double *dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeVectorData(*dataID, *valueIndex, dataValue);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_bsdata_(
@@ -323,18 +379,22 @@ void precicef_write_bsdata_(
     const int *size,
     int *      valueIndices,
     double *   values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockScalarData(*dataID, *size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_sdata_(
     const int *   dataID,
     const int *   valueIndex,
     const double *dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeScalarData(*dataID, *valueIndex, *dataValue);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_read_bvdata_(
@@ -342,18 +402,22 @@ void precicef_read_bvdata_(
     const int *size,
     int *      valueIndices,
     double *   values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readBlockVectorData(*dataID, *size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_read_vdata_(
     const int *dataID,
     const int *valueIndex,
     double *   dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readVectorData(*dataID, *valueIndex, dataValue);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_read_bsdata_(
@@ -361,48 +425,58 @@ void precicef_read_bsdata_(
     const int *size,
     int *      valueIndices,
     double *   values)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readBlockScalarData(*dataID, *size, valueIndices, values);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_read_sdata_(
     const int *dataID,
     const int *valueIndex,
     double *   dataValue)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->readScalarData(*dataID, *valueIndex, *dataValue);
+} catch (...) {
+  std::abort();
 }
 
 int precice::impl::strippedLength(
     const char *string,
     int         length)
-{
+try {
   int i = length - 1;
   while (((string[i] == ' ') || (string[i] == 0)) && (i >= 0)) {
     i--;
   }
   return i + 1;
+} catch (...) {
+  std::abort();
 }
 
 void precicef_get_version_information_(
     char *versionInfo,
     int   lengthVersionInfo)
-{
+try {
   const std::string &versionInformation = precice::versionInformation;
   PRECICE_ASSERT(versionInformation.size() < (size_t) lengthVersionInfo, versionInformation.size(), lengthVersionInfo);
   for (size_t i = 0; i < versionInformation.size(); i++) {
     versionInfo[i] = versionInformation[i];
   }
+} catch (...) {
+  std::abort();
 }
 
 void precicef_set_mesh_access_region_(
     const int     meshID,
     const double *boundingBox)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->setMeshAccessRegion(meshID, boundingBox);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_get_mesh_vertices_and_IDs_(
@@ -410,28 +484,34 @@ void precicef_get_mesh_vertices_and_IDs_(
     const int size,
     int *     ids,
     double *  coordinates)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->getMeshVerticesAndIDs(meshID, size, ids, coordinates);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_requires_gradient_data_for_(const int *dataID, int *required)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   if (impl->requiresGradientDataFor(*dataID)) {
     *required = 1;
   } else {
     *required = 0;
   }
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_sgradient_data_(
     const int *   dataID,
     const int *   valueIndex,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeScalarGradientData(*dataID, *valueIndex, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_bsgradient_data_(
@@ -439,18 +519,22 @@ void precicef_write_bsgradient_data_(
     const int *   size,
     const int *   valueIndices,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockScalarGradientData(*dataID, *size, valueIndices, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_vgradient_data_(
     const int *   dataID,
     const int *   valueIndex,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeVectorGradientData(*dataID, *valueIndex, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 void precicef_write_bvgradient_data_(
@@ -458,9 +542,11 @@ void precicef_write_bvgradient_data_(
     const int *   size,
     const int *   valueIndices,
     const double *gradientValues)
-{
+try {
   PRECICE_CHECK(impl != nullptr, Err, errormsg);
   impl->writeBlockVectorGradientData(*dataID, *size, valueIndices, gradientValues);
+} catch (...) {
+  std::abort();
 }
 
 #ifdef __GNUC__
