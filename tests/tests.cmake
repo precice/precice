bbee79654a6131f642b90aa15bc06640a4a6078e
nille <niklas.kotarsky@math.lu.se>
2023-03-15 15:32:12 +0100

    Add cmake for test

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -189,6 +189,7 @@ target_sources(testprecice
     tests/serial/time/implicit/serial-coupling/ReadWriteScalarDataWithWaveformSubcyclingMixed.cpp
     tests/serial/time/implicit/serial-coupling/ReadWriteScalarDataWithWaveformSubcyclingThird.cpp
     tests/serial/time/implicit/serial-coupling/ReadWriteScalarDataWithWaveformSubcyclingZero.cpp
+    tests/serial/time/implicit/serial-coupling/WaveformSubsyclingWithConstantAcceleration.cpp
     tests/serial/watch-integral/WatchIntegralScaleAndNoScale.cpp
     tests/serial/watch-integral/helpers.cpp
     tests/serial/watch-integral/helpers.hpp
