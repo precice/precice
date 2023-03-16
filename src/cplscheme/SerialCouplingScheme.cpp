55b9906db9e209dc763d88b75956f141b2df85c4
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-03-16 17:24:20 +0100

    Add missing global data receive in ReceiveOfFirstAdvance()

--- src/cplscheme/SerialCouplingScheme.cpp
+++ src/cplscheme/SerialCouplingScheme.cpp
@@ -106,6 +106,7 @@ void SerialCouplingScheme::exchangeInitialData()
     receiveAndSetTimeWindowSize();
     PRECICE_DEBUG("Receiving data...");
     receiveData(getM2N(), getReceiveData());
+    receiveGlobalData(getM2N(), getReceiveGlobalData());
     checkDataHasBeenReceived();
   }
 }
