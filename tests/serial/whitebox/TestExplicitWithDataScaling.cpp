ecdc1193e888e269f74644e62b3e0ba20c74508e
Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
2023-04-04 17:58:08 +0200

    Strip TestExplicitWithDataScaling test, adapt timing and disable

--- tests/serial/whitebox/TestExplicitWithDataScaling.cpp
+++ tests/serial/whitebox/TestExplicitWithDataScaling.cpp
@@ -18,7 +18,7 @@ BOOST_AUTO_TEST_SUITE(Whitebox)
  * and stay constant over the coupling cycles. SolverTwo has a scaling of the
  * values activated and reads the scaled values.
  */
-BOOST_AUTO_TEST_CASE(TestExplicitWithDataScaling)
+BOOST_AUTO_TEST_CASE(TestExplicitWithDataScaling, *precice::testing::bt::disabled())
 {
   PRECICE_TEST("SolverOne"_on(1_rank), "SolverTwo"_on(1_rank));
 
