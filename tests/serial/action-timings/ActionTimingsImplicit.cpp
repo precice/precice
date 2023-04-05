c97d876b08dc216c96274c5622950d7b254c73de
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2023-04-05 11:09:18 +0200

    Update following https://github.com/precice/precice/pull/1614.

--- tests/serial/action-timings/ActionTimingsImplicit.cpp
+++ tests/serial/action-timings/ActionTimingsImplicit.cpp
@@ -55,17 +55,9 @@ BOOST_AUTO_TEST_CASE(ActionTimingsImplicit)
 
   dt = interface.initialize();
   BOOST_TEST(dt == 1.0);
-  if (context.isNamed("SolverOne")) {
-    // BOOST_TEST(action::RecorderAction::records.empty());
-    // @todo Needed change because always mapping in initialize.
-    BOOST_TEST(action::RecorderAction::records.size() == 2);
-    BOOST_TEST(action::RecorderAction::records.at(0).timing == action::Action::READ_MAPPING_PRIOR);
-    BOOST_TEST(action::RecorderAction::records.at(1).timing == action::Action::READ_MAPPING_POST);
-  } else {
-    BOOST_TEST(context.isNamed("SolverTwo"));
-    BOOST_TEST(action::RecorderAction::records.size() == 1);
-    BOOST_TEST(action::RecorderAction::records.at(0).timing == action::Action::READ_MAPPING_POST);
-  }
+  // @todo Needed change because always mapping in initialize.
+  BOOST_TEST(action::RecorderAction::records.size() == 1);
+  BOOST_TEST(action::RecorderAction::records.at(0).timing == action::Action::READ_MAPPING_POST);
   action::RecorderAction::reset();
   int iteration = 0;
 
