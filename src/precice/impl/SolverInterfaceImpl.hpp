ef843aac0032f5604894937e9d972625426e6d14
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2023-04-26 11:22:53 +0200

    Fix doxygen.

--- src/precice/impl/SolverInterfaceImpl.hpp
+++ src/precice/impl/SolverInterfaceImpl.hpp
@@ -53,7 +53,7 @@ public:
   ///@{
 
   /**
-   * @copydoc SolverInterface::SolverInterface(const std::string&, const std::string&, int, int)
+   * @copydoc SolverInterface::SolverInterface(std::string_view, std::string_view, int, int)
    */
   SolverInterfaceImpl(
       std::string_view participantName,
@@ -62,12 +62,12 @@ public:
       int              solverProcessSize);
 
   /**
-   * @copybrief SolverInterface::SolverInterface(const std::string&, const std::string&, int, int, void*)
+   * @copybrief SolverInterface::SolverInterface(std::string_view, std::string_view, int, int, void*)
    *
    * Use the parameter communicator to specify a custom global MPI communicator.
    * Pass a null pointer to signal preCICE to use MPI_COMM_WORLD.
    *
-   * @copydetails SolverInterface::SolverInterface(const std::string&, const std::string&, int, int, void*)
+   * @copydetails SolverInterface::SolverInterface(std::string_view, std::string_view, int, int, void*)
    */
   SolverInterfaceImpl(
       std::string_view participantName,
@@ -232,7 +232,7 @@ public:
       const int *      valueIndices,
       const double *   values);
 
-  /// @copydoc precice::SolverInterface::writeBlockVectorGradientData
+  /// @copydoc SolverInterface::writeBlockVectorGradientData
   void writeBlockVectorGradientData(
       std::string_view meshName,
       std::string_view dataName,
@@ -247,7 +247,7 @@ public:
       int              valueIndex,
       const double *   value);
 
-  /// @copydoc precice::SolverInterface::writeVectorGradientData
+  /// @copydoc SolverInterface::writeVectorGradientData
   void writeVectorGradientData(
       std::string_view meshName,
       std::string_view dataName,
@@ -262,7 +262,7 @@ public:
       const int *      valueIndices,
       const double *   values);
 
-  /// @copydoc precice::SolverInterface::writeBlockScalarGradientData
+  /// @copydoc SolverInterface::writeBlockScalarGradientData
   void writeBlockScalarGradientData(
       std::string_view meshName,
       std::string_view dataName,
@@ -277,14 +277,14 @@ public:
       int              valueIndex,
       double           value);
 
-  /// @copydoc precice::SolverInterface::writeScalarGradientData
+  /// @copydoc SolverInterface::writeScalarGradientData
   void writeScalarGradientData(
       std::string_view meshName,
       std::string_view dataName,
       int              valueIndex,
       const double *   gradientValues);
 
-  /// @copydoc SolverInterface::readBlockVectorData(int, int, const int*, double, double*) const
+  /// @copydoc SolverInterface::readBlockVectorData
   void readBlockVectorData(
       std::string_view meshName,
       std::string_view dataName,
@@ -293,7 +293,7 @@ public:
       double           relativeReadTime,
       double *         values) const;
 
-  /// @copydoc SolverInterface::readVectorData(int, int, double, double*) const
+  /// @copydoc SolverInterface::readVectorData
   void readVectorData(
       std::string_view meshName,
       std::string_view dataName,
@@ -301,7 +301,7 @@ public:
       double           relativeReadTime,
       double *         value) const;
 
-  /// @copydoc SolverInterface::readBlockScalarData(int, int, const int*, double, double*) const
+  /// @copydoc SolverInterface::readBlockScalarData
   void readBlockScalarData(
       std::string_view meshName,
       std::string_view dataName,
@@ -310,7 +310,7 @@ public:
       double           relativeReadTime,
       double *         values) const;
 
-  /// @copydoc SolverInterface::readScalarData(int, int, double, double&) const
+  /// @copydoc SolverInterface::readScalarData
   void readScalarData(
       std::string_view meshName,
       std::string_view dataName,
