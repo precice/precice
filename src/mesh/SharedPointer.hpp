c4aa4f756c5b6ac155420f9713119054ef24d842
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-05-09 15:57:03 +0200

    Old cleanup

--- src/mesh/SharedPointer.hpp
+++ src/mesh/SharedPointer.hpp
@@ -6,14 +6,12 @@ namespace precice {
 namespace mesh {
 
 class Data;
-// class GlobalData;
 class Group;
 class Mesh;
 class DataConfiguration;
 class MeshConfiguration;
 
-using PtrData = std::shared_ptr<Data>;
-// using PtrGlobalData        = std::shared_ptr<GlobalData>;
+using PtrData              = std::shared_ptr<Data>;
 using PtrGroup             = std::shared_ptr<Group>;
 using PtrMesh              = std::shared_ptr<Mesh>;
 using PtrDataConfiguration = std::shared_ptr<DataConfiguration>;
