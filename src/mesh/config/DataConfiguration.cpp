590865f1a8f726200dc2e19139034ce66577eb4a
Gerasimos Chourdakis <makishourdakis@gmail.com>
2023-05-17 16:12:19 +0300

    Remove XML tag solver-interface (#1662)
    Merge Configuration and SolverInterfaceConfiguration classes
    Remove SolverInterfaceConfiguration files
    Remove solver-interface tag from all XML files
    Update formatter to version 3 and reformat everything
    
    Co-authored-by: Frédéric Simonis <simonisfrederic@gmail.com>

--- src/mesh/config/DataConfiguration.cpp
+++ src/mesh/config/DataConfiguration.cpp
@@ -22,7 +22,7 @@ DataConfiguration::DataConfiguration(xml::XMLTag &parent)
   XMLTag tagVector(*this, VALUE_VECTOR, XMLTag::OCCUR_ARBITRARY, TAG);
   tagVector.setDocumentation("Defines a vector data set to be assigned to meshes. The number of "
                              "components of each data entry depends on the spatial dimensions set "
-                             "in tag <solver-interface>.");
+                             "in tag <precice-configuration>.");
   tagVector.addAttribute(attrName);
   parent.addSubtag(tagVector);
 }
