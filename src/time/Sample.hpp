23543cb01525397ab22abd96801da6599b53f5ec
Frédéric Simonis <simonisfrederic@gmail.com>
2023-05-25 10:31:05 +0200

    Add documentation to Sample

--- src/time/Sample.hpp
+++ src/time/Sample.hpp
@@ -5,21 +5,34 @@
 
 namespace precice::time {
 
-/// @brief Sample containing data values
+/// @brief Sample of a \ref mesh::Data on a \ref mesh::Mesh
 struct Sample {
+  /// Constructs an invalid empty Sample
   Sample() = default;
+
+  /// Constructs an empty Sample of a given data dimensionality
   explicit Sample(int dims) noexcept
       : dataDims(dims) {}
+
+  /// Constructs a Sample of given data dimensionality and size without gradients
   Sample(int dims, int dataCount)
       : dataDims(dims), values(dims * dataCount) {}
+
+  /// Constructs a Sample of given data dimensionality and data values
   Sample(int dims, Eigen::VectorXd inValues)
       : dataDims(dims), values(inValues) {}
+
+  /// Constructs a Sample of given data dimensionality, data values, and data gradients
   Sample(int dims, Eigen::VectorXd inValues, Eigen::MatrixXd inGradients)
       : dataDims(dims), values(inValues), gradients(inGradients) {}
 
   Sample(const Sample &) = default;
   Sample(Sample &&)      = default;
 
+  Sample &operator=(const Sample &) = default;
+  Sample &operator=(Sample &&) = default;
+
+  /// Sets values and gradients to zero
   Sample &setZero()
   {
     values.setZero();
@@ -27,11 +40,14 @@ struct Sample {
     return *this;
   }
 
-  Sample &operator=(const Sample &) = default;
-  Sample &operator=(Sample &&) = default;
+  /// The dimensionality of the data
+  int dataDims{-1};
 
-  int             dataDims{-1};
+  /// The data values linearised
+  /// @todo Change to matrix so that values.col(i) gets the value at vertex i
   Eigen::VectorXd values;
+
+  /// The gradients of the data. Use gradients.col(i) to get the gradient at vertex i
   Eigen::MatrixXd gradients;
 };
 
