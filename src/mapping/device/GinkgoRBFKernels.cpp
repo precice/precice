882dac4ce07548cf3f4a630f400510c5ea075f06
Marcel Koch <marcel.koch@kit.edu>
2023-06-12 16:31:05 +0200

    fixes transposed access to device arrays

--- src/mapping/device/GinkgoRBFKernels.cpp
+++ src/mapping/device/GinkgoRBFKernels.cpp
@@ -16,38 +16,61 @@ using gko::ext::kokkos::map_data;
 
 namespace precice::mapping::kernel {
 
+auto create_row_major_acc()
+{
+  return [](const int &i, const int &j, auto view) {
+    return view(i, j);
+  };
+}
+
+auto create_col_major_acc()
+{
+  return [](const int &i, const int &j, auto view) {
+    return view(j, i);
+  };
+};
+
 template <typename EvalFunctionType>
 void create_rbf_system_matrix(std::shared_ptr<const gko::Executor> exec,
                               gko::ptr_param<GinkgoMatrix> mtx, const std::array<bool, 3> activeAxis,
                               gko::ptr_param<GinkgoMatrix> supportPoints, gko::ptr_param<GinkgoMatrix> targetPoints,
                               EvalFunctionType f, ::precice::mapping::RadialBasisParameters rbf_params, bool addPolynomial, unsigned int extraDims)
 {
-  exec->run(
-      gko::ext::kokkos::parallel_for(
-          "create_rbf_system_matrix",
-          gko::ext::kokkos::make_policy_top<Kokkos::MDRangePolicy, Kokkos::Rank<2>>(Kokkos::Array<int64_t, 2>{0, 0},
-                                                                                    Kokkos::Array<int64_t, 2>{static_cast<int64_t>(mtx->get_size()[0]),
-                                                                                                              static_cast<int64_t>(mtx->get_size()[1])}),
-          [=] GKO_KOKKOS_FN(const int &i, const int &j, auto k_mtx, auto k_supportPoints, auto k_targetPoints) {
-            double dist = 0;
+  auto run_kernel = [=](auto acc) {
+    exec->run(
+        gko::ext::kokkos::parallel_for(
+            "create_rbf_system_matrix",
+            gko::ext::kokkos::make_policy_top<Kokkos::MDRangePolicy, Kokkos::Rank<2>>(Kokkos::Array<int64_t, 2>{0, 0},
+                                                                                      Kokkos::Array<int64_t, 2>{static_cast<int64_t>(mtx->get_size()[0]),
+                                                                                                                static_cast<int64_t>(mtx->get_size()[1])}),
+            [=] GKO_KOKKOS_FN(const int &i, const int &j, auto k_mtx, auto k_supportPoints, auto k_targetPoints) {
+              double dist = 0;
 
-            // Make each entry zero if polynomial is on since not every entry will be adjusted below
-            if (addPolynomial) {
-              k_mtx(i, j) = 0;
-            }
-
-            // Loop over each dimension and calculate euclidean distance
-            for (size_t k = 0; k < activeAxis.size(); ++k) {
-              if(activeAxis[k]   ) {
-                dist += pow_int<2>(k_supportPoints(j, k) - k_targetPoints(i, k));
+              // Make each entry zero if polynomial is on since not every entry will be adjusted below
+              if (addPolynomial) {
+                k_mtx(i, j) = 0;
               }
-            }
 
-            dist = Kokkos::sqrt(dist);
+              // Loop over each dimension and calculate euclidean distance
+              for (size_t k = 0; k < activeAxis.size(); ++k) {
+                if (activeAxis[k]) {
+                  dist += pow_int<2>(acc(j, k, k_supportPoints) - acc(i, k, k_targetPoints));
+                }
+              }
 
-            k_mtx(i, j) = f(dist, rbf_params);
-          },
-          mtx.get(), supportPoints.get(), targetPoints.get()));
+              dist = Kokkos::sqrt(dist);
+
+              k_mtx(i, j) = f(dist, rbf_params);
+            },
+            mtx.get(), supportPoints.get(), targetPoints.get()));
+  };
+
+  if (dynamic_cast<const gko::ReferenceExecutor *>(exec.get()) ||
+      dynamic_cast<const gko::OmpExecutor *>(exec.get())) {
+    run_kernel(create_row_major_acc());
+  } else {
+    run_kernel(create_col_major_acc());
+  }
 }
 
 #define INSTATIATE_CREATE_RBF_SYSTEM_MATRIX(_function_type)                                                                                     \
@@ -67,18 +90,10 @@ INSTATIATE_CREATE_RBF_SYSTEM_MATRIX(CompactPolynomialC2);
 INSTATIATE_CREATE_RBF_SYSTEM_MATRIX(CompactPolynomialC4);
 INSTATIATE_CREATE_RBF_SYSTEM_MATRIX(CompactPolynomialC6);
 
-
 void fill_polynomial_matrix(std::shared_ptr<const gko::Executor> exec,
                             gko::ptr_param<GinkgoMatrix> mtx, gko::ptr_param<const GinkgoMatrix> x, const unsigned int dims)
 {
-  auto row_major_acc = [=] GKO_KOKKOS_FN(const int &i, const int &j, auto k_x) {
-    return k_x(i, j);
-  };
-  auto col_major_acc = [=] GKO_KOKKOS_FN(const int &i, const int &j, auto k_x) {
-    return k_x(j, i);
-  };
-
-  auto run_kernel = [=](auto acc){
+  auto run_kernel = [=](auto acc) {
     exec->run(
         gko::ext::kokkos::parallel_for(
             "create_rbf_system_matrix",
@@ -95,11 +110,11 @@ void fill_polynomial_matrix(std::shared_ptr<const gko::Executor> exec,
             mtx.get(), x.get()));
   };
 
-    if(dynamic_cast<const gko::ReferenceExecutor*>(exec.get()) ||
-       dynamic_cast<const gko::OmpExecutor*>(exec.get())){
-    run_kernel(row_major_acc);
+  if (dynamic_cast<const gko::ReferenceExecutor *>(exec.get()) ||
+      dynamic_cast<const gko::OmpExecutor *>(exec.get())) {
+    run_kernel(create_row_major_acc());
   } else {
-    run_kernel(col_major_acc);
+    run_kernel(create_col_major_acc());
   }
 }
 
