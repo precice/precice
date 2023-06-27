36d17d72957d6e51deb735a8258d091ccaa5f31e
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2023-06-27 12:06:57 +0200

    Provide implementation draft for waveform acceleration.

--- src/acceleration/Acceleration.cpp
+++ src/acceleration/Acceleration.cpp
@@ -17,16 +17,20 @@ void Acceleration::checkDataIDs(const DataMap &cplData) const
 void Acceleration::applyRelaxation(double omega, const DataMap &cplData) const
 {
   for (const DataMap::value_type &pair : cplData) {
-    const auto  couplingData = pair.second;
-    auto &      values       = couplingData->values();
-    const auto &oldValues    = couplingData->previousIteration();
-    values *= omega;
-    values += oldValues * (1 - omega);
-    if (couplingData->hasGradient()) {
-      auto &      gradients    = couplingData->gradients();
-      const auto &oldGradients = couplingData->previousIterationGradients();
-      gradients *= omega;
-      gradients += oldGradients * (1 - omega);
+    const auto couplingData = pair.second;
+    for (auto &stample : couplingData->stamples()) {
+      couplingData->sample() = stample.sample;
+      auto &      values     = couplingData->values();
+      const auto &oldValues  = couplingData->previousIteration(); // @todo need to move previousIteration inside stample? How do deal with changing time grid from one iteration to the next? (Adaptivity?)
+      values *= omega;
+      values += oldValues * (1 - omega);
+      if (couplingData->hasGradient()) {
+        auto &      gradients    = couplingData->gradients();
+        const auto &oldGradients = couplingData->previousIterationGradients(); // @todo need to move previousIterationGradients inside stample? How do deal with changing time grid from one iteration to the next? (Adaptivity?)
+        gradients *= omega;
+        gradients += oldGradients * (1 - omega);
+      }
+      couplingData->setSampleAtTime(stample.timestamp, couplingData->sample());
     }
   }
 }
