3bfc9d9015c15e4ef8967269ccc67e92a9884086
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2023-07-05 10:26:58 +0200

    Fix bug that prevents requiring checkpoint in last window (#1704)
    Previous implementation leads to bug with checkpointing: When entering
    the last window, isCouplingOngoing() evaluates false, if we increase the
    time before checking isCouplingOngoing() and don't reset the current 
    window time to zero. This will avoid writing a checkpoint for the beginning
    of the last window, meaning we jump back to the second to last window
    when reading the checkpoint.

--- src/cplscheme/BaseCouplingScheme.cpp
+++ src/cplscheme/BaseCouplingScheme.cpp
@@ -348,12 +348,14 @@ void BaseCouplingScheme::secondExchange()
         // coupling iteration.
         PRECICE_ASSERT(math::greater(_computedTimeWindowPart, 0.0));
         _timeWindows -= 1;
-      } else { // write output, prepare for next window
+        _computedTimeWindowPart = 0.0; // reset window
+      } else {                         // write output, prepare for next window
         PRECICE_DEBUG("Convergence achieved");
         advanceTXTWriters();
         PRECICE_INFO("Time window completed");
         _isTimeWindowComplete = true;
         _timeWindowStartTime += _computedTimeWindowPart;
+        _computedTimeWindowPart = 0.0; // reset window
         if (isCouplingOngoing()) {
           PRECICE_DEBUG("Setting require create checkpoint");
           requireAction(CouplingScheme::Action::WriteCheckpoint);
@@ -370,11 +372,11 @@ void BaseCouplingScheme::secondExchange()
       PRECICE_INFO("Time window completed");
       _isTimeWindowComplete = true;
       _timeWindowStartTime += _computedTimeWindowPart;
+      _computedTimeWindowPart = 0.0; // reset window
     }
     if (isCouplingOngoing()) {
       PRECICE_ASSERT(_hasDataBeenReceived);
     }
-    _computedTimeWindowPart = 0.0; // reset window
   }
 }
 
