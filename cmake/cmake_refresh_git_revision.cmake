19578a09757450a0759c938cdf45f6f150a15b61
Frédéric Simonis <simonisfrederic@gmail.com>
2023-08-03 14:36:40 +0200

    Fix git detection not working in worktrees

--- cmake/cmake_refresh_git_revision.cmake
+++ cmake/cmake_refresh_git_revision.cmake
@@ -13,32 +13,17 @@
 find_package(Git REQUIRED)
 
 execute_process(
-  COMMAND ${GIT_EXECUTABLE} rev-parse --show-toplevel
-  RESULT_VARIABLE PRECICE_REPO_RET
-  OUTPUT_VARIABLE PRECICE_REPO_OUT
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  ERROR_QUIET
-  )
-
-execute_process(
   COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
   RESULT_VARIABLE PRECICE_REVISION_RET
   OUTPUT_VARIABLE PRECICE_REVISION_OUT
   OUTPUT_STRIP_TRAILING_WHITESPACE
-  ERROR_QUIET
   )
 
 set(preCICE_REVISION "no-info [git failed to run]")
 
-if(("${PRECICE_REVISION_RET}" EQUAL "0") AND ("${PRECICE_REPO_RET}" EQUAL "0"))
-  file(TO_CMAKE_PATH "${PRECICE_REPO_OUT}" _detected_path)
-  if("${PROJECT_SOURCE_DIR}" STREQUAL "${_detected_path}")
-    set(preCICE_REVISION "${PRECICE_REVISION_OUT}")
-    message(STATUS "Revision status: ${preCICE_REVISION}")
-  else()
-    message(STATUS "Revision status: directory is not a git repository")
-    set(preCICE_REVISION "no-info [not a repository]")
-  endif()
+if("${PRECICE_REVISION_RET}" EQUAL "0")
+  set(preCICE_REVISION "${PRECICE_REVISION_OUT}")
+  message(STATUS "Revision status: ${preCICE_REVISION}")
 else()
   message(STATUS "Revision status: Detection failed")
 endif()
