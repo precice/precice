0f99c516b20ff71f57d6ddb1b486c4ecc3a2c945
Gerasimos Chourdakis <chourdak@in.tum.de>
2023-08-15 10:27:43 +0200

    Update src/precice/impl/ParticipantImpl.cpp
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@gmail.com>

--- src/precice/impl/ParticipantImpl.cpp
+++ src/precice/impl/ParticipantImpl.cpp
@@ -773,7 +773,7 @@ void ParticipantImpl::setMeshQuad(
                 secondVertexID, thirdVertexID, fourthVertexID);
   PRECICE_REQUIRE_MESH_MODIFY(meshName);
   MeshContext &context = _accessor->usedMeshContext(meshName);
-  PRECICE_CHECK(context.mesh->getDimensions() == 3, "setMeshQuad is only possible for 3D cases."
+  PRECICE_CHECK(context.mesh->getDimensions() == 3, "setMeshQuad is only possible for 3D meshes."
                                                     " Please set the mesh dimension to 3 in the preCICE configuration file.");
   if (context.meshRequirement == mapping::Mapping::MeshRequirement::FULL) {
     PRECICE_ASSERT(context.mesh);
