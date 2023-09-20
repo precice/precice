8efc8c489ef3736a58fc13221daa2d0753b7b366
Frédéric Simonis <simonisfrederic@gmail.com>
2023-09-20 12:24:49 +0200

    Rename events to profiling (#1787)
    * Rename events to profiling
    
    * Add changelog
    
    * Rename more
    
    * Rename arguments and functions

--- tools/profiling/tests/simple-test.cmake
+++ tools/profiling/tests/simple-test.cmake
@@ -6,7 +6,7 @@
 # Inputs:
 #   TEST_FOLDER - the folder of the tests
 #   FOLDER_ARG - the folder argument passed to merge
-#   EVENTS_SCRIPT - the path to the precice-events script
+#   PROFILING_SCRIPT - the path to the precice-profiling script
 
 # Force using a virtual env if it is set in the environment
 if(DEFINED ENV{VIRTUAL_ENV})
@@ -15,22 +15,22 @@ if(DEFINED ENV{VIRTUAL_ENV})
 endif()
 find_package(Python3 COMPONENTS Interpreter REQUIRED)
 
-if(NOT FOLDER_ARG OR NOT TEST_FOLDER OR NOT EVENTS_SCRIPT)
+if(NOT FOLDER_ARG OR NOT TEST_FOLDER OR NOT PROFILING_SCRIPT)
   message(FATAL_ERROR "Missing arguments")
 endif()
 
 message(STATUS "Removing previous files")
-file(REMOVE events.json trace.json exports.csv)
+file(REMOVE profiling.json trace.json exports.csv)
 
 message(STATUS "Testing: merge")
-execute_process(COMMAND ${Python3_EXECUTABLE} ${EVENTS_SCRIPT} merge ${FOLDER_ARG}
+execute_process(COMMAND ${Python3_EXECUTABLE} ${PROFILING_SCRIPT} merge ${FOLDER_ARG}
   COMMAND_ECHO STDOUT)
-if(NOT EXISTS "events.json")
-  message(FATAL_ERROR "No events.json file found")
+if(NOT EXISTS "profiling.json")
+  message(FATAL_ERROR "No profiling.json file found")
 endif()
 
 message(STATUS "Testing: trace")
-execute_process(COMMAND ${Python3_EXECUTABLE} ${EVENTS_SCRIPT} trace
+execute_process(COMMAND ${Python3_EXECUTABLE} ${PROFILING_SCRIPT} trace
   COMMAND_ECHO STDOUT
   )
 if(NOT EXISTS "trace.json")
@@ -38,10 +38,10 @@ if(NOT EXISTS "trace.json")
 endif()
 
 message(STATUS "Testing: export")
-execute_process(COMMAND ${Python3_EXECUTABLE} ${EVENTS_SCRIPT} export
+execute_process(COMMAND ${Python3_EXECUTABLE} ${PROFILING_SCRIPT} export
   COMMAND_ECHO STDOUT)
-if(NOT EXISTS "events.csv")
-  message(FATAL_ERROR "No events.csv file found")
+if(NOT EXISTS "profiling.csv")
+  message(FATAL_ERROR "No profiling.csv file found")
 endif()
 
 execute_process(COMMAND ${Python3_EXECUTABLE} -c "import polars"
@@ -51,7 +51,7 @@ if(NOT ${PYTHON_NO_POLARS})
   foreach(_participant IN LISTS _participants)
     message(STATUS "Testing: analyze ${_participant}")
     set(_outfile "analyze-${_participant}.csv")
-    execute_process(COMMAND ${Python3_EXECUTABLE} ${EVENTS_SCRIPT} analyze --output ${_outfile} ${_participant} events.json
+    execute_process(COMMAND ${Python3_EXECUTABLE} ${PROFILING_SCRIPT} analyze --output ${_outfile} ${_participant} profiling.json
     COMMAND_ECHO STDOUT)
     if(NOT EXISTS "${_outfile}")
       message(FATAL_ERROR "No ${_outfile} file found")
