1c2f7f43bf930e9c9c4ab23a2cdf4b40e51ffa23
Jun Chen <jun.chen@ipvs.uni-stuttgart.de>
2023-10-01 15:21:26 +0200

    Add assertion to warn false large ranks

--- src/acceleration/impl/SVDFactorization.hpp
+++ src/acceleration/impl/SVDFactorization.hpp
@@ -167,6 +167,7 @@ public:
     _psi.conservativeResize(_rows, _cols);
     _phi.conservativeResize(_rows, _cols);
     _sigma.conservativeResize(_cols);
+    PRECICE_ASSERT(_sigma.size() == std::min(_psi.cols(), _psi.rows()));
     PRECICE_DEBUG("SVD factorization of Jacobian is truncated to {} DOFs. Cut off {} DOFs", _cols, waste);
 
     _initialSVD = true;
