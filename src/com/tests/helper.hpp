dbceba3cef0eb64827e7924ce1343614b0b69b28
Frédéric Simonis <simonisfrederic@gmail.com>
2023-11-14 17:09:23 +0100

    Fix helper format

--- src/com/tests/helper.hpp
+++ src/com/tests/helper.hpp
@@ -10,7 +10,7 @@ namespace precice::testing {
 inline cplscheme::PtrCouplingData makeCouplingData(mesh::PtrData data, mesh::PtrMesh mesh)
 {
   bool requiresInitialization = false;
-  bool exchangeSubsteps = true;
+  bool exchangeSubsteps       = true;
   return std::make_shared<cplscheme::CouplingData>(std::move(data), std::move(mesh), requiresInitialization, exchangeSubsteps, cplscheme::CouplingData::Direction::Send);
 }
 } // namespace precice::testing
