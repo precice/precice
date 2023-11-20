dc973f029f102ed45dd3d25e2342ba96a6c21a03
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2023-11-20 18:02:22 +0100

    Rewrite error message in RBF solver (#1873)

--- src/mapping/RadialBasisFctSolver.hpp
+++ src/mapping/RadialBasisFctSolver.hpp
@@ -246,8 +246,8 @@ RadialBasisFctSolver<RADIAL_BASIS_FUNCTION_T>::RadialBasisFctSolver(RADIAL_BASIS
   PRECICE_CHECK(decompositionSuccessful,
                 "The interpolation matrix of the RBF mapping from mesh \"{}\" to mesh \"{}\" is not invertable. "
                 "This means that the mapping problem is not well-posed. "
-                "Please check if your coupling meshes are correct. Maybe you need to fix axis-aligned mapping setups "
-                "by marking perpendicular axes as dead?",
+                "Please check if your coupling meshes are correct (e.g. no vertices are duplicated) or reconfigure "
+                "your basis-function (e.g. reduce the support-radius).",
                 inputMesh.getName(), outputMesh.getName());
 
   // Second, assemble evaluation matrix
