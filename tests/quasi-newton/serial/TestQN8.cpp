01b48e93d62d42ff606bbca3fea40f253fe6e64e
Jun Chen <jun.chen@ipvs.uni-stuttgart.de>
2023-11-20 11:13:44 +0100

    Improve comments to explain what the added tests do

--- tests/quasi-newton/serial/TestQN8.cpp
+++ tests/quasi-newton/serial/TestQN8.cpp
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_SUITE(Serial)
 BOOST_AUTO_TEST_CASE(TestQN8)
 {
   PRECICE_TEST("SolverOne"_on(1_rank), "SolverTwo"_on(1_rank));
-  // serial coupling, IQN-IMVJ (which is identical to IQN-ILS as only first timestep is considered), relaxed QR2 filter
+  // serial coupling, IQN-IMVJ (which is identical to IQN-ILS as only first timestep is considered), to test `always-build-jacobian` option;
   runTestQN(context.config(), context);
 }
 
