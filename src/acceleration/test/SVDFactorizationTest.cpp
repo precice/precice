dc3f1781fdb6a6c72136301c031c0a78efb341ef
Jun Chen <jun.chen@ipvs.uni-stuttgart.de>
2023-11-29 17:09:18 +0100

    add test for singular values

--- src/acceleration/test/SVDFactorizationTest.cpp
+++ src/acceleration/test/SVDFactorizationTest.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#ifndef PRECICE_NO_MPI
+
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <Eigen/src/Core/Matrix.h>
@@ -17,12 +20,15 @@ using namespace precice::acceleration::impl;
 BOOST_AUTO_TEST_CASE(testSVDFactorization)
 {
   PRECICE_TEST(1_rank);
-  int                 m                   = 8;
-  double              eps                 = 1.2e-3;
-  const bool          cyclicCommunication = false;
-  std::vector<double> factors(1.0, 1.0);
-  Eigen::VectorXd     a(m);
-  Eigen::VectorXd     b(m);
+  int                      m                   = 8;
+  double                   eps                 = 1.2e-3;
+  const bool               cyclicCommunication = false;
+  std::vector<double>      factors(1.0, 1.0);
+  Eigen::VectorXd          a(m);
+  Eigen::VectorXd          b(m);
+  SVDFactorization::Vector singleValues = SVDFactorization::Vector::Zero(2);
+  singleValues(0)                       = 0.000578589774323541;
+  singleValues(1)                       = 8.13011257500996e-05;
 
   // prepare preConditioner to be used to construct a SVD factorization class
   auto prec(std::make_shared<impl::ConstantPreconditioner>(factors));
@@ -32,7 +38,7 @@ BOOST_AUTO_TEST_CASE(testSVDFactorization)
   matOperation.initialize(cyclicCommunication);
   auto ptrParMatrixOp = std::make_shared<ParallelMatrixOperations>(matOperation);
 
-  // construct a SVD factorization odject
+  // construct a SVD factorization object
   SVDFactorization svd_1(eps, prec);
 
   svd_1.initialize(ptrParMatrixOp, m);
@@ -46,6 +52,9 @@ BOOST_AUTO_TEST_CASE(testSVDFactorization)
   }
   // check if the over small single values have been correctly truncated
   BOOST_TEST(svd_1.rank() == 2);
+  BOOST_TEST(testing::equals(svd_1.singularValues(), singleValues, 1e-10));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+#endif
