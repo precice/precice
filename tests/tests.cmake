28890f5fcda7c346de8c68d9d06f2f65858feed9
Frédéric Simonis <simonisfrederic@gmail.com>
2023-12-01 13:32:42 +0100

    Add muscle tests (#1514)
    TwoActivatedMuscles is a simplified version due to bug #1907 
    
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>
    Co-authored-by: carme-hp <homspons@gmail.com>

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -101,6 +101,8 @@ target_sources(testprecice
     tests/serial/action-timings/ActionTimingsSerialImplicit.cpp
     tests/serial/circular/Explicit.cpp
     tests/serial/circular/helper.hpp
+    tests/serial/compositional/OneActivatedMuscle.cpp
+    tests/serial/compositional/TwoActivatedMuscles.cpp
     tests/serial/convergence-measures/helpers.cpp
     tests/serial/convergence-measures/helpers.hpp
     tests/serial/convergence-measures/testConvergenceMeasures1.cpp
