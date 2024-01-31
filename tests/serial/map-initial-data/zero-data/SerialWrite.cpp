9dd607e5417e6f86dd7d501f142d437b607474bf
Frédéric Simonis <simonisfrederic@gmail.com>
2024-01-31 15:16:15 +0100

    Add tests for mapping initialized data

---
+++ tests/serial/map-initial-data/zero-data/SerialWrite.cpp
@@ -0,0 +1,22 @@
+#ifndef PRECICE_NO_MPI
+
+#include "../helper.hpp"
+#include "testing/Testing.hpp"
+
+BOOST_AUTO_TEST_SUITE(Integration)
+BOOST_AUTO_TEST_SUITE(Serial)
+BOOST_AUTO_TEST_SUITE(MapInitialData)
+BOOST_AUTO_TEST_SUITE(ZeroData)
+BOOST_AUTO_TEST_CASE(SerialWrite)
+{
+  PRECICE_TEST("One"_on(1_rank), "Two"_on(1_rank));
+
+  testMapInitialData(context, 0.0, 0.0, 0, 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // NonZeroData
+BOOST_AUTO_TEST_SUITE_END() // MapInitialData
+BOOST_AUTO_TEST_SUITE_END() // Serial
+BOOST_AUTO_TEST_SUITE_END() // Integration
+
+#endif // PRECICE_NO_MPI
