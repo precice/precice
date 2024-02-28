e79f610afee1f884ee5e217a4ff753347f179a97
Frédéric Simonis <simonisfrederic@gmail.com>
2024-02-28 15:14:54 +0100

    Precompute areas in scalebyarea action

--- src/action/ScaleByAreaAction.cpp
+++ src/action/ScaleByAreaAction.cpp
@@ -28,28 +28,31 @@ void ScaleByAreaAction::performAction()
   PRECICE_TRACE();
   const int meshDimensions = getMesh()->getDimensions();
 
+  // precompute areas
+  Eigen::VectorXd areas = Eigen::VectorXd::Zero(getMesh()->vertices().size());
+  if (meshDimensions == 2) {
+    PRECICE_CHECK(!getMesh()->edges().empty(),
+                  "The multiply/divide-by-area actions require meshes with connectivity information. In 2D, please ensure that the mesh {} contains edges.", getMesh()->getName());
+    for (mesh::Edge &edge : getMesh()->edges()) {
+      areas[edge.vertex(0).getID()] += edge.getEnclosingRadius();
+      areas[edge.vertex(1).getID()] += edge.getEnclosingRadius();
+    }
+  } else {
+    PRECICE_CHECK(!getMesh()->triangles().empty(),
+                  "The multiply/divide-by-area actions require meshes with connectivity information. In 3D, please ensure that the mesh {} contains triangles.", getMesh()->getName());
+    for (mesh::Triangle &face : getMesh()->triangles()) {
+      areas[face.vertex(0).getID()] += face.getArea() / 3.0;
+      areas[face.vertex(1).getID()] += face.getArea() / 3.0;
+      areas[face.vertex(2).getID()] += face.getArea() / 3.0;
+    }
+  }
+
+  // Process samples
   for (auto &targetStample : _targetData->timeStepsStorage().stamples()) {
-    auto &          targetValues    = targetStample.sample.values;
-    const int       valueDimensions = _targetData->getDimensions();
-    Eigen::VectorXd areas           = Eigen::VectorXd::Zero(getMesh()->vertices().size());
+    auto &    targetValues    = targetStample.sample.values;
+    const int valueDimensions = _targetData->getDimensions();
     PRECICE_ASSERT(targetValues.size() / valueDimensions == areas.size());
 
-    if (meshDimensions == 2) {
-      PRECICE_CHECK(getMesh()->edges().size() != 0,
-                    "The multiply/divide-by-area actions require meshes with connectivity information. In 2D, please ensure that the mesh {} contains edges.", getMesh()->getName());
-      for (mesh::Edge &edge : getMesh()->edges()) {
-        areas[edge.vertex(0).getID()] += edge.getEnclosingRadius();
-        areas[edge.vertex(1).getID()] += edge.getEnclosingRadius();
-      }
-    } else {
-      PRECICE_CHECK(getMesh()->triangles().size() != 0,
-                    "The multiply/divide-by-area actions require meshes with connectivity information. In 3D, please ensure that the mesh {} contains triangles.", getMesh()->getName());
-      for (mesh::Triangle &face : getMesh()->triangles()) {
-        areas[face.vertex(0).getID()] += face.getArea() / 3.0;
-        areas[face.vertex(1).getID()] += face.getArea() / 3.0;
-        areas[face.vertex(2).getID()] += face.getArea() / 3.0;
-      }
-    }
     if (_scaling == SCALING_DIVIDE_BY_AREA) {
       for (int i = 0; i < areas.size(); i++) {
         for (int dim = 0; dim < valueDimensions; dim++) {
