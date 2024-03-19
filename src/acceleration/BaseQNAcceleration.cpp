422c0fab35d5378f5452933c728132d8852fd69a
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-03-19 13:20:26 +0100

    Simplify QN update. (#1979)

--- src/acceleration/BaseQNAcceleration.cpp
+++ src/acceleration/BaseQNAcceleration.cpp
@@ -371,7 +371,7 @@ void BaseQNAcceleration::performAcceleration(
     /**
      * apply quasiNewton update
      */
-    _values = _oldValues + xUpdate + _residuals; // = x^k + delta_x + r^k - q^k
+    _values += xUpdate;
 
     // pending deletion: delete old V, W matrices if timeWindowsReused = 0
     // those were only needed for the first iteration (instead of underrelax.)
