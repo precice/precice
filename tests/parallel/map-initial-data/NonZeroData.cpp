e62f54bc3bc78d60f5cd98bb00fab871fb48c614
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2024-04-11 10:12:24 +0200

    Provide failing test for skip initial mapping in parallel

---
+++ tests/parallel/map-initial-data/NonZeroData.cpp
@@ -0,0 +1,20 @@
+#ifndef PRECICE_NO_MPI
+
+#include "helper.hpp"
+#include "testing/Testing.hpp"
+
+BOOST_AUTO_TEST_SUITE(Integration)
+BOOST_AUTO_TEST_SUITE(Parallel)
+BOOST_AUTO_TEST_SUITE(MapInitialData)
+BOOST_AUTO_TEST_CASE(NonZeroData)
+{
+  PRECICE_TEST("One"_on(1_rank), "Two"_on(2_ranks));
+
+  testMapInitialData(context, 1.0, 1.0, 2, 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // MapInitialData
+BOOST_AUTO_TEST_SUITE_END() // Parallel
+BOOST_AUTO_TEST_SUITE_END() // Integration
+
+#endif // PRECICE_NO_MPI
