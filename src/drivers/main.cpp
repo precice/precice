095ded6abd3c661b5504239cbb54d5081f31233c
Frédéric Simonis <simonisfrederic@gmail.com>
2024-09-24 15:51:24 +0200

    Catch Error in check

--- src/drivers/main.cpp
+++ src/drivers/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <precice/Exceptions.hpp>
 #include <precice/Tooling.hpp>
 #include <stdexcept>
 #include <string>
@@ -56,7 +57,10 @@ int main(int argc, char **argv)
         return 1;
       }
     }
-    precice::tooling::checkConfiguration(file, participant, size);
+    try {
+      precice::tooling::checkConfiguration(file, participant, size);
+    } catch (const ::precice::Error &) {
+    }
     return 0;
   }
 
