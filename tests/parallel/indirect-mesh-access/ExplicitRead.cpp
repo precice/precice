7fd6f7252fed3fa6eea8aca6ee16087589de9f0a
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2024-10-29 08:48:01 +0100

    Provide a failing distributed test

---
+++ tests/parallel/indirect-mesh-access/ExplicitRead.cpp
@@ -0,0 +1,115 @@
+#ifndef PRECICE_NO_MPI
+
+#include "testing/Testing.hpp"
+
+#include <precice/precice.hpp>
+#include <vector>
+
+BOOST_AUTO_TEST_SUITE(Integration)
+BOOST_AUTO_TEST_SUITE(Parallel)
+BOOST_AUTO_TEST_SUITE(IndirectMeshAccess)
+// Test case for a indirect mesh access on one participant to a mesh defined
+// by another participant. The region of interest is defined through a
+// boundingBox. SolverTwo defines the mesh (as usual) whereas SolverOne reads
+// indirectly from this mesh.
+// parallel-access-nearest-neighbor-consistent-read
+BOOST_AUTO_TEST_CASE(ExplicitRead)
+{
+  PRECICE_TEST("SolverOne"_on(2_ranks), "SolverTwo"_on(1_rank));
+
+  // Set up Participant
+  precice::Participant couplingInterface(context.name, context.config(), context.rank, context.size);
+
+  constexpr int dim = 2;
+
+  if (context.isNamed("SolverOne")) {
+
+    std::array<double, dim * 2> boundingBox;
+    if (context.isPrimary()) {
+      boundingBox = {0.0, 1.0, 0.0, 1.0};
+    }
+    if (!context.isPrimary()) {
+      boundingBox = {1.0, 2.0, 1.0, 2.0};
+    }
+    auto otherMeshName = "MeshTwo";
+    auto dataName      = "Velocities";
+    BOOST_REQUIRE(couplingInterface.getMeshDimensions(otherMeshName) == 2);
+
+    // Define region of interest, where we could obtain direct write access
+    couplingInterface.setMeshAccessRegion(otherMeshName, boundingBox);
+
+    couplingInterface.initialize();
+
+    double time = 0;
+    while (couplingInterface.isCouplingOngoing()) {
+      double dt = couplingInterface.getMaxTimeStepSize();
+      time += dt;
+
+      // read data:
+      std::vector<double> expectedData1({1, 2, 3, 4, 2, 4});
+      std::vector<double> expectedData2({-10, -11, -12, -13, -11, -13});
+      std::vector<double> tmpPositions = {0.0, -0.01, 0.01, 0.05, 0.1, 0.1, 0.1, 0.0, 0, 0.05, 0.1, 0.0};
+
+      for (std::size_t i = 0; i < expectedData1.size(); ++i) {
+        std::vector<double> solverTwoCoord(dim);
+        double              value;
+        for (int d = 0; d < dim; ++d) {
+          solverTwoCoord[d] = tmpPositions[i * dim + d];
+        }
+
+        couplingInterface.mapAndreadData(otherMeshName, dataName, solverTwoCoord, dt, {&value, 1});
+        // Expected data according to the writeData
+        if (time == 1) {
+          BOOST_TEST(expectedData1[i] == value);
+        } else if (time == 2) {
+          BOOST_TEST(expectedData2[i] == value);
+        } else {
+          PRECICE_ASSERT(false);
+        }
+      }
+      // solve time step
+      // write data (not necessary here)
+      couplingInterface.advance(dt);
+    }
+  } else {
+    BOOST_TEST(context.isNamed("SolverTwo"));
+    // Query IDs
+    auto meshName = "MeshTwo";
+    auto dataName = "Velocities";
+    BOOST_REQUIRE(couplingInterface.getMeshDimensions(meshName));
+
+    std::vector<double> positions = {0.0, 0.0, 0.0, 0.05, 0.1, 0.1, 0.1, 0.0};
+    std::vector<int>    ids(4, -1);
+
+    // Define the mesh
+    couplingInterface.setMeshVertices(meshName, positions, ids);
+    // Some dummy readData
+    std::array<double, 4> writeData1({1, 2, 3, 4});
+    std::array<double, 4> writeData2({-10, -11, -12, -13});
+
+    // Initialize
+    couplingInterface.initialize();
+    double time = 0;
+    while (couplingInterface.isCouplingOngoing()) {
+      double dt = couplingInterface.getMaxTimeStepSize();
+      time += dt;
+      // read data (not necessary here)
+      // solve time step
+      // write data:
+      if (time == 1) {
+        couplingInterface.writeData(meshName, dataName, ids, writeData1);
+      } else if (time == 2) {
+        couplingInterface.writeData(meshName, dataName, ids, writeData2);
+      } else {
+        PRECICE_ASSERT(false);
+      }
+      couplingInterface.advance(dt);
+    }
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif // PRECICE_NO_MPI
