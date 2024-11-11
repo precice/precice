e161a3283408d613e2efc067da7bf81d8a51f13a
Frédéric Simonis <simonisfrederic@gmail.com>
2024-11-11 17:37:51 +0100

    Add default initial relaxation to Aitken (#2112)
    * Add default initial relaxation to Aitken
    
    * Fix Aitken config and add tests
    
    ---------
    
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@ipvs.uni-stuttgart.de>

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -168,7 +168,6 @@ target_sources(testprecice
     tests/remeshing/parallel-implicit/noop/RemeshSecond.cpp
     tests/remeshing/parallel-implicit/noop/RemeshSecond2LI.cpp
     tests/remeshing/parallel-implicit/noop/RemeshSecondSerial.cpp
-    tests/serial/AitkenAcceleration.cpp
     tests/serial/ImplicitCheckpointing.cpp
     tests/serial/PreconditionerBug.cpp
     tests/serial/SendMeshToMultipleParticipants.cpp
@@ -182,6 +181,8 @@ target_sources(testprecice
     tests/serial/action-timings/ActionTimingsParallelImplicit.cpp
     tests/serial/action-timings/ActionTimingsSerialExplicit.cpp
     tests/serial/action-timings/ActionTimingsSerialImplicit.cpp
+    tests/serial/aitken/DefaultInitialRelaxation.cpp
+    tests/serial/aitken/DefinedInitialRelaxation.cpp
     tests/serial/circular/Explicit.cpp
     tests/serial/circular/helper.hpp
     tests/serial/compositional/OneActivatedMuscle.cpp
