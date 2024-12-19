ef06031abdb829a02dbfe30e54f3d86ae9442b84
Frédéric Simonis <simonisfrederic@gmail.com>
2024-12-19 13:05:36 +0100

    Fix benchmark without MPI

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,9 +1,13 @@
 #include <benchmark/benchmark.h>
+
+#ifndef PRECICE_NO_MPI
 #include <iostream>
 #include <mpi.h>
+#endif
 
 int main(int argc, char **argv)
 {
+#ifndef PRECICE_NO_MPI
   // Setup MPI on one rank
   MPI_Init(nullptr, nullptr);
   int size;
@@ -12,6 +16,7 @@ int main(int argc, char **argv)
     std::cerr << "ERROR: running on multiple ranks is not supported!\n";
     return 1;
   }
+#endif
 
   ::benchmark::Initialize(&argc, argv);
   if (::benchmark::ReportUnrecognizedArguments(argc, argv))
@@ -19,6 +24,8 @@ int main(int argc, char **argv)
   ::benchmark::RunSpecifiedBenchmarks();
   ::benchmark::Shutdown();
 
+#ifndef PRECICE_NO_MPI
   MPI_Finalize();
+#endif
   return 0;
 }
