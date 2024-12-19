129ebd951761c5347f30e1f58ef16a3181e44663
Frédéric Simonis <simonisfrederic@gmail.com>
2024-12-19 12:34:06 +0100

    Add support for MPI

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,3 +1,24 @@
 #include <benchmark/benchmark.h>
+#include <iostream>
+#include <mpi.h>
 
-BENCHMARK_MAIN();
+int main(int argc, char **argv)
+{
+  // Setup MPI on one rank
+  MPI_Init(nullptr, nullptr);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (size > 1) {
+    std::cerr << "ERROR: running on multiple ranks is not supported!\n";
+    return 1;
+  }
+
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv))
+    return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+
+  MPI_Finalize();
+  return 0;
+}
