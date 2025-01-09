33ef6b559ead506c1af04c7ea0f899430edd2a2d
Frédéric Simonis <simonisfrederic@gmail.com>
2025-01-09 16:14:46 +0100

    Overhaul assemble benchmark

--- benchmarks/rbf-assembly-kernels.cpp
+++ benchmarks/rbf-assembly-kernels.cpp
@@ -6,51 +6,31 @@
 
 using namespace precice;
 
-// Benchmark of our assemble routines
-
-class SetupMeshes : public benchmark::Fixture {
-public:
-  void SetUp(const ::benchmark::State &state)
-  {
-    std::cout << "Setup" << std::endl;
-    polynomial    = mapping::Polynomial::OFF;
-    activeAxis    = {{true, true, true}};
-    int dimension = 3;
-
-    inMesh = std::make_unique<mesh::Mesh>("inMesh", dimension, -1);
-
-    for (int i = 0; i < 10; ++i)
-      for (int j = 0; j < 10; ++j)
-        inMesh->createVertex(Eigen::Vector3d(0, i * 3.1, j * 1.4315));
-
-    outMesh = std::make_unique<mesh::Mesh>("outMesh", dimension, -1);
-
-    for (int i = 0; i < 10; ++i)
-      for (int j = 0; j < 10; ++j)
-        outMesh->createVertex(Eigen::Vector3d(1, i * 1.2111, j * 5.4315));
-
-    std::cout << "end" << std::endl;
-  }
-
-  void TearDown(const ::benchmark::State &state)
-  {
-    inMesh.reset();
-    outMesh.reset();
-  }
-
-  std::unique_ptr<mesh::Mesh> inMesh;
-  std::unique_ptr<mesh::Mesh> outMesh;
-  std::array<bool, 3>         activeAxis;
-  mapping::Polynomial         polynomial{};
-};
-
-BENCHMARK_F(SetupMeshes, assembleA)
-(benchmark::State &state)
+template <typename RBF>
+void assembleA(benchmark::State &state, RBF f, mapping::Polynomial polynomial)
 {
-  mapping::CompactPolynomialC6 f(1);
-  Eigen::MatrixXd              mat;
+  std::array<bool, 3> activeAxis{true, true, true};
+  const int           dimension = 3;
+
+  mesh::Mesh inMesh("inMesh", dimension, -1);
+
+  for (int i = 0; i < 10; ++i)
+    for (int j = 0; j < 10; ++j)
+      inMesh.createVertex(Eigen::Vector3d(0, i * 3.1, j * 1.4315));
+
+  mesh::Mesh outMesh("outMesh", dimension, -1);
+
+  for (int i = 0; i < 10; ++i)
+    for (int j = 0; j < 10; ++j)
+      outMesh.createVertex(Eigen::Vector3d(1, i * 1.2111, j * 5.4315));
+
   for (auto _ : state) {
-    mat = mapping::buildMatrixA(f, *inMesh.get(), boost::irange<Eigen::Index>(0, inMesh->vertices().size()), *outMesh.get(), boost::irange<Eigen::Index>(0, outMesh->vertices().size()), activeAxis, polynomial);
+    Eigen::MatrixXd mat = mapping::buildMatrixA(f, inMesh, boost::irange<Eigen::Index>(0, inMesh.nVertices()), outMesh, boost::irange<Eigen::Index>(0, outMesh.nVertices()), activeAxis, polynomial);
+    benchmark::DoNotOptimize(mat);
   }
-  benchmark::DoNotOptimize(mat);
 }
+
+BENCHMARK_CAPTURE(assembleA, 1, mapping::CompactPolynomialC6{1}, mapping::Polynomial::ON)->Name("Assemble RBF matrix A for C6 with polynomials");
+BENCHMARK_CAPTURE(assembleA, 2, mapping::CompactPolynomialC6{1}, mapping::Polynomial::OFF)->Name("Assemble RBF matrix A for C6");
+BENCHMARK_CAPTURE(assembleA, 3, mapping::ThinPlateSplines{}, mapping::Polynomial::ON)->Name("Assemble RBF matrix A for TPS with polynomials");
+BENCHMARK_CAPTURE(assembleA, 4, mapping::ThinPlateSplines{}, mapping::Polynomial::OFF)->Name("Assemble RBF matrix A for TPS");
