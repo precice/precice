fd9d0f9b626b3045e273753a9d0383432a315210
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-02-20 15:27:48 +0100

    Avoid write access to stamples. Use setSampleAtTime.

--- src/acceleration/Acceleration.cpp
+++ src/acceleration/Acceleration.cpp
@@ -25,20 +25,24 @@ void Acceleration::applyRelaxation(double omega, DataMap &cplData, double window
     // @todo: This will apply the scaling to the sample at t=0 and t=1 when
     // calling performAcceleration the first time. Computations at the
     // previousValuesAtTime/oldValues don't change anything and are unneeded
-    for (auto &stample : couplingData.timeStepsStorage().stamples()) {
-      if (!math::greater(stample.timestamp, windowStart)) {
+    for (auto &time : couplingData.timeStepsStorage().getTimes()) {
+      if (!math::greater(time, windowStart)) {
         // skip stamples at beginning of this window or earlier since this is either initial data or already converged data from previous windows
         continue;
       }
 
-      auto &values = stample.sample.values;
-      auto  old    = couplingData.getPreviousValuesAtTime(stample.timestamp); // IMPORTANT DETAIL: The interpolation that we use for resampling does not necessarily have to be the same interpolation as the interpolation the user accesses via read-data. (But probably it is easier to just use the same)
-      values       = values * omega + old.values() * (1.0 - omega);
+      auto sample = couplingData.timeStepsStorage().getSampleAtOrAfter(time);
+      auto values = sample.values;
+      auto old    = couplingData.getPreviousValuesAtTime(time); // IMPORTANT DETAIL: The interpolation that we use for resampling does not necessarily have to be the same interpolation as the interpolation the user accesses via read-data. (But probably it is easier to just use the same)
+      values      = values * omega + old.values() * (1.0 - omega);
 
-      if (couplingData.hasGradient()) {
-        auto &gradients    = stample.sample.gradients;
-        auto  oldGradients = couplingData.getPreviousGradientsAtTime(stample.timestamp); // IMPORTANT DETAIL: The interpolation that we use for resampling does not necessarily have to be the same interpolation as the interpolation the user accesses via read-data. (But probably it is easier to just use the same)
-        gradients          = gradients * omega + oldGradients * (1.0 - omega);
+      if (!couplingData.hasGradient()) {
+        couplingData.setSampleAtTime(time, time::Sample(couplingData.getDimensions(), values));
+      } else {
+        auto gradients    = sample.gradients;
+        auto oldGradients = couplingData.getPreviousGradientsAtTime(time); // IMPORTANT DETAIL: The interpolation that we use for resampling does not necessarily have to be the same interpolation as the interpolation the user accesses via read-data. (But probably it is easier to just use the same)
+        gradients         = gradients * omega + oldGradients * (1.0 - omega);
+        couplingData.setSampleAtTime(time, time::Sample(couplingData.getDimensions(), values, gradients));
       }
     }
     // @todo remove
