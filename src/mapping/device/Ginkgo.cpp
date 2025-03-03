f4d01789c9961027cebfa2ebd3377294a480d094
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2025-03-03 11:11:50 +0100

    Do not initialize GPU with invalid defaults (#2219)

--- src/mapping/device/Ginkgo.cpp
+++ src/mapping/device/Ginkgo.cpp
@@ -22,7 +22,13 @@ void Ginkgo::initialize(int nThreads, int deviceId)
 {
   // We initialize Ginkgo internally through Kokkos
   if (!Kokkos::is_initialized() && !Kokkos::is_finalized()) {
-    Kokkos::initialize(Kokkos::InitializationSettings().set_num_threads(nThreads).set_device_id(deviceId).set_disable_warnings(true));
+    const int defaultDeviceID = -1;
+    // don't use and initialize the GPU if we don't use it
+    if (deviceId == defaultDeviceID)
+      Kokkos::initialize(Kokkos::InitializationSettings().set_num_threads(nThreads).set_disable_warnings(true));
+    else {
+      Kokkos::initialize(Kokkos::InitializationSettings().set_num_threads(nThreads).set_device_id(deviceId).set_disable_warnings(true));
+    }
   }
   weInitialized = true;
 }
