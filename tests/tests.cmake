764fe081fec7be89d67cf0060321d01953c894bd
Frédéric Simonis <simonisfrederic@gmail.com>
2025-03-19 09:08:32 +0100

    Add tests for interleaved mesh creation with gradients

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -5,6 +5,7 @@ target_sources(testprecice
     PRIVATE
     tests/fundamental/DifferentConfigs.cpp
     tests/fundamental/initial-data/InterleavedCreation.cpp
+    tests/fundamental/initial-data/InterleavedCreationWithGradients.cpp
     tests/fundamental/profiling/NotAllStopped.cpp
     tests/fundamental/profiling/NotStoppedAtFinalize.cpp
     tests/fundamental/profiling/UserProfiling.cpp
