4c94153d3395d02b8a004bd9685444ac18872699
Frédéric Simonis <simonisfrederic@gmail.com>
2025-08-12 18:06:17 +0200

    Add test using random participant-first timesteps

---
+++ tests/serial/time/explicit/serial-coupling/FirstParticipantRandomTimeWindows.cpp
@@ -0,0 +1,59 @@
+#ifndef PRECICE_NO_MPI
+
+#include "testing/Testing.hpp"
+
+#include <precice/precice.hpp>
+#include <random>
+
+using namespace precice;
+
+BOOST_AUTO_TEST_SUITE(Integration)
+BOOST_AUTO_TEST_SUITE(Serial)
+BOOST_AUTO_TEST_SUITE(Time)
+BOOST_AUTO_TEST_SUITE(Explicit)
+BOOST_AUTO_TEST_SUITE(SerialCoupling)
+
+PRECICE_TEST_SETUP("SolverOne"_on(1_rank), "SolverTwo"_on(1_rank))
+BOOST_AUTO_TEST_CASE(FirstParticipantRandomTimeWindows)
+{
+  PRECICE_TEST();
+
+  Participant precice(context.name, context.config(), 0, 1);
+
+  std::mt19937                        gen(101); // seed for reproducibility
+  std::lognormal_distribution<double> dist(-2.0, 0.5);
+
+  std::array<double, 3> value{1, 1, 1}; // value doesn't matter
+
+  std::string meshName, writeDataName, readDataName;
+  if (context.isNamed("SolverOne")) {
+    meshName = "SolverOne-Mesh";
+  } else {
+    BOOST_TEST(context.isNamed("SolverTwo"));
+    meshName = "SolverTwo-Mesh";
+  }
+
+  double   v0[]     = {0, 0, 0};
+  VertexID vertexID = precice.setMeshVertex(meshName, v0);
+  precice.initialize();
+  while (precice.isCouplingOngoing()) {
+    // SolverOne takes random steps
+    double dt = context.isNamed("SolverOne") ? dist(gen) : precice.getMaxTimeStepSize();
+
+    if (context.isNamed("SolverTwo")) {
+      // Bug #2160, read fails in tw=7 t=60.06
+      precice.readData(meshName, "Data-One", {&vertexID, 1}, dt, value);
+    } else {
+      precice.writeData(meshName, "Data-One", {&vertexID, 1}, value);
+    }
+    precice.advance(dt);
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END() // Integration
+BOOST_AUTO_TEST_SUITE_END() // Serial
+BOOST_AUTO_TEST_SUITE_END() // Time
+BOOST_AUTO_TEST_SUITE_END() // Explicit
+BOOST_AUTO_TEST_SUITE_END() // SerialCoupling
+
+#endif // PRECICE_NO_MPI
