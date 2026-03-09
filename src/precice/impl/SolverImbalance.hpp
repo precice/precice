c588b473efc7f496521e88025012dce92252e1b5
Valentina Schüller <valentina.schuller@math.lu.se>
2026-03-09 10:43:22 +0100

    Add SolverImbalance object to Participant
    - records solver advance time and participant dt
    - at the end of a time window, compute solver time to advance

---
+++ src/precice/impl/SolverImbalance.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <chrono>
+#include <vector>
+
+namespace precice::impl {
+
+class SolverImbalance {
+  using Clock = std::chrono::steady_clock;
+
+public:
+  SolverImbalance() = default;
+  double getSolverTimeToAdvance();
+  void   startSolver();
+  void   stopSolver(double solverTimeStepSize);
+
+private:
+  std::vector<double>     _participant_dt;
+  mutable logging::Logger _log{"impl::SolverImbalance"};
+  std::vector<double>     _solver_advance_time;
+  Clock::time_point       _start_time;
+
+  enum class State : bool {
+    STOPPED = false,
+    RUNNING = true
+  };
+
+  State _state = State::STOPPED;
+};
+
+} // namespace precice::impl
