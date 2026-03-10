d4ca8e8177b74d36c163d6df9c828124b15f88f0
Ankit Vashisth <ankitvashisth06@gmail.com>
2026-03-10 22:42:01 +0530

    Allow convergence measures using written but non-exchanged data

--- src/cplscheme/config/CouplingSchemeConfiguration.cpp
+++ src/cplscheme/config/CouplingSchemeConfiguration.cpp
@@ -1120,10 +1120,42 @@ void CouplingSchemeConfiguration::checkIfDataIsExchanged(
     dataName = dataptr->getName();
   }
 
-  PRECICE_ERROR("You need to exchange every data that you use for convergence measures and/or the iteration acceleration. "
-                "Data \"{}\" is currently not exchanged over the respective mesh of participant \"{}\" on which it is used for convergence measures and/or iteration acceleration. "
-                "Please check the <exchange ... /> and <...-convergence-measure ... /> tags in the <coupling-scheme:... /> of your precice-config.xml.",
+  PRECICE_ERROR("You need to exchange every data that you use for the iteration acceleration. "
+                "Data \"{}\" is currently not exchanged over the respective mesh of participant \"{}\" on which it is used for iteration acceleration. "
+                "Please check the <exchange ... /> tags in the <coupling-scheme:... /> of your precice-config.xml.",
                 dataName, participant);
+}
+
+void CouplingSchemeConfiguration::checkConvergenceMeasureDataAvailable(
+    DataID dataID, std::string_view participant, const std::string &dataName, const std::string &meshName) const
+{
+  // First, check if the data is exchanged (existing behavior)
+  const auto match = std::find_if(_config.exchanges.begin(),
+                                  _config.exchanges.end(),
+                                  [dataID, participant](const Config::Exchange &exchange) {
+                                    if (exchange.from != participant && exchange.to != participant) {
+                                      return false;
+                                    } else {
+                                      return exchange.data->getID() == dataID;
+                                    }
+                                  });
+  if (match != _config.exchanges.end()) {
+    return;
+  }
+
+  // Data is not exchanged. Check if the convergence-checking participant
+  // writes this data on a mesh it provides
+  const auto &participantPtr = _participantConfig->getParticipant(participant);
+  if (participantPtr->isMeshProvided(meshName) && participantPtr->isDataWrite(meshName, dataName)) {
+    return;
+  }
+
+  PRECICE_ERROR("Data \"{}\" of mesh \"{}\" is used in a convergence measure but is neither exchanged "
+                "nor written by participant \"{}\" on a provided mesh. "
+                "Please either add an <exchange ... /> tag for this data, "
+                "or make sure participant \"{}\" provides the mesh and writes this data. "
+                "See the <coupling-scheme:... /> of your precice-config.xml.",
+                dataName, meshName, participant, participant);
 }
 
 void CouplingSchemeConfiguration::checkSerialImplicitAccelerationData(
@@ -1165,7 +1197,7 @@ void CouplingSchemeConfiguration::addConvergenceMeasures(
 {
   for (auto &elem : convergenceMeasureDefinitions) {
     _meshConfig->addNeededMesh(participant, elem.meshName);
-    checkIfDataIsExchanged(elem.data->getID(), participant);
+    checkConvergenceMeasureDataAvailable(elem.data->getID(), participant, elem.data->getName(), elem.meshName);
     scheme->addConvergenceMeasure(elem.data->getID(), elem.suffices, elem.strict, elem.measure);
   }
 }
---
+++ tests/serial/convergence-measures/testConvergenceMeasures5.cpp
@@ -0,0 +1,64 @@
+#ifndef PRECICE_NO_MPI
+
+#include "testing/Testing.hpp"
+
+#include <precice/precice.hpp>
+
+BOOST_AUTO_TEST_SUITE(Integration)
+BOOST_AUTO_TEST_SUITE(Serial)
+BOOST_AUTO_TEST_SUITE(ConvergenceMeasures)
+PRECICE_TEST_SETUP("SolverOne"_on(1_rank), "SolverTwo"_on(1_rank))
+BOOST_AUTO_TEST_CASE(testConvergenceMeasures5)
+{
+  PRECICE_TEST();
+  using Eigen::Vector2d;
+
+  std::string meshName = context.isNamed("SolverOne") ? "MeshOne" : "MeshTwo";
+
+  precice::Participant interface(context.name, context.config(), 0, 1);
+
+  Vector2d vertex{0.0, 0.0};
+
+  VertexID vertexID = interface.setMeshVertex(meshName, vertex);
+
+  interface.initialize();
+  int numberOfAdvanceCalls = 0;
+  int numberOfIterations   = -1;
+  int timestep             = 0;
+
+  // Data3 converges on iteration 2 for time window 1, and iteration 3 for time window 2.
+  // These values are written by SolverTwo on its provided MeshTwo.
+  // Data3 is NOT exchanged, but used in the convergence measure.
+  std::vector<double> writeValuesData3 = {1.0, 1.01, 2.0, 2.5, 2.81};
+
+  // Expected iterations per time window based on the convergence of Data3
+  std::vector<int> expectedIterations = {2, 3};
+
+  while (interface.isCouplingOngoing()) {
+    if (interface.requiresWritingCheckpoint()) {
+      numberOfIterations = 0;
+    }
+
+    if (context.isNamed("SolverTwo")) {
+      // Write Data3 (non-exchanged, used for convergence measure)
+      interface.writeData(meshName, "Data3", {&vertexID, 1}, {&writeValuesData3.at(numberOfAdvanceCalls), 1});
+    }
+
+    interface.advance(1.0);
+    ++numberOfAdvanceCalls;
+    ++numberOfIterations;
+
+    if (interface.requiresReadingCheckpoint()) {
+    } else { // converged
+      BOOST_TEST(numberOfIterations == expectedIterations.at(timestep));
+      ++timestep;
+    }
+  }
+  interface.finalize();
+}
+
+BOOST_AUTO_TEST_SUITE_END() // Integration
+BOOST_AUTO_TEST_SUITE_END() // Serial
+BOOST_AUTO_TEST_SUITE_END() // ConvergenceMeasures
+
+#endif // PRECICE_NO_MPI
